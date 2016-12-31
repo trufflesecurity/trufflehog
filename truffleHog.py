2ead44894dd97c874ae8113809a7102c7d05fd5d
flower <flower@flowers-MacBook-Pro.local>
2016-12-30 23:10:07 -0600

    adding initial files

---
+++ truffleHog.py
@@ -0,0 +1,101 @@
+import os, math, string
+import argparse
+from uuid import uuid4
+from git import Repo
+
+BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+HEX_CHARS = "1234567890abcdefABCDEF"
+
+def shannon_entropy(data, iterator):
+    if not data:
+        return 0
+    entropy = 0
+    for x in (ord(c) for c in iterator):
+        p_x = float(data.count(chr(x)))/len(data)
+        if p_x > 0:
+            entropy += - p_x*math.log(p_x, 2)
+    return entropy
+
+
+def get_strings_of_set(word, char_set, threshold=20):
+    count = 0
+    letters = ""
+    strings = []
+    for char in word:
+        if char in char_set:
+            letters += char
+            count += 1
+        else:
+            if count > 20:
+                strings.append(letters)
+            letters = ""
+            count = 0
+    if count > threshold:
+        strings.append(letters)
+    return strings
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def find_strings(git_url):
+    new_project = str(uuid4())
+    project_path = os.path.join("temp", new_project)
+
+    Repo.clone_from(git_url, project_path)
+
+    repo = Repo(project_path)
+
+
+    for i in repo.remotes.origin.fetch():
+        branch_name = str(i).split('/')[1]
+        try:
+            repo.git.checkout(i, b=branch_name)
+        except:
+            pass
+     
+        prev_commit = None
+        for curr_commit in repo.iter_commits():
+            if not prev_commit:
+                pass
+            else:
+                diff = prev_commit.diff(curr_commit, create_patch=True)
+                for i in diff:
+                    #print i.a_blob.data_stream.read()
+                    printableDiff = i.diff
+                    foundSomething = False
+                    lines = i.diff.split("\n")
+                    for line in lines:
+                        for word in line.split():
+                            base64_strings = get_strings_of_set(word, BASE64_CHARS)
+                            hex_strings = get_strings_of_set(word, HEX_CHARS)
+                            for string in base64_strings:
+                                b64Entropy = shannon_entropy(string, BASE64_CHARS)
+                                if b64Entropy > 4.5:
+                                    foundSomething = True
+                                    printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+                            for string in hex_strings:
+                                hexEntropy = shannon_entropy(string, HEX_CHARS)
+                                if hexEntropy > 3:
+                                    foundSomething = True
+                                    printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+                    if foundSomething:
+                        print printableDiff
+
+                    
+            prev_commit = curr_commit
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Find secrets hidden in the depths of git.')
+    parser.add_argument('git_url', type=str, help='URL for secret searching')
+
+
+    args = parser.parse_args()
+    find_strings(args.git_url)
+
