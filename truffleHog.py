a4f6d63872ef2dae596a505d887dadb9401382ec
flower <flower@flowers-MacBook-Pro.local>
2017-01-02 11:23:06 -0600

    updating the temp directory generation again

--- truffleHog.py
+++ truffleHog.py
@@ -1,5 +1,4 @@
-import shutil, sys, os, math, string, datetime, argparse, tempfile
-from uuid import uuid4
+import shutil, sys, math, string, datetime, argparse, tempfile
 from git import Repo
 
 reload(sys)  
@@ -47,8 +46,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def find_strings(git_url):
-    new_project = str(uuid4())
-    project_path = os.path.join(tempfile.gettempdir(), new_project)
+    project_path = tempfile.mkdtemp()
 
     Repo.clone_from(git_url, project_path)
 
