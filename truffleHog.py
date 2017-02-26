a2a5008207cbd92f9edc43d8643edc647246185d
Dylan <dxa4481@rit.edu>
2017-02-25 16:39:14 -0800

    fixing up json output

--- truffleHog.py
+++ truffleHog.py
@@ -62,15 +62,11 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def find_strings(git_url, output):
+def find_strings(git_url, printJson=False):
     project_path = tempfile.mkdtemp()
-
     Repo.clone_from(git_url, project_path)
-
+    output = {"entropicDiffs": []}
     repo = Repo(project_path)
-
-    jsonOutput = output
-
     already_searched = set()
     for remote_branch in repo.remotes.origin.fetch():
         branch_name = str(remote_branch).split('/')[1]
@@ -97,7 +93,7 @@ def find_strings(git_url, output):
                     printableDiff = blob.diff.decode()
                     if printableDiff.startswith("Binary files"):
                         continue
-                    foundSomething = False
+                    stringsFound = []
                     lines = blob.diff.decode().split("\n")
                     for line in lines:
                         for word in line.split():
@@ -106,29 +102,24 @@ def find_strings(git_url, output):
                             for string in base64_strings:
                                 b64Entropy = shannon_entropy(string, BASE64_CHARS)
                                 if b64Entropy > 4.5:
-                                    foundSomething = True
-                                    if jsonOutput:
-                                        stringDiff = string
-                                    else:
-                                        printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+                                    stringsFound.append(string)
+                                    printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
                             for string in hex_strings:
                                 hexEntropy = shannon_entropy(string, HEX_CHARS)
                                 if hexEntropy > 3:
-                                    foundSomething = True
-                                    if jsonOutput:
-                                        stringDiff = string
-                                    else:
-                                        printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
-                    if foundSomething:
+                                    stringsFound.append(string)
+                                    printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+                    if len(stringsFound) > 0:
                         commit_time =  datetime.datetime.fromtimestamp(prev_commit.committed_date).strftime('%Y-%m-%d %H:%M:%S')
-                        if jsonOutput:
-                            output = {}
-                            output['date'] = commit_time
-                            output['branch'] = branch_name
-                            output['commit'] = prev_commit.message
-                            output['diff'] = printableDiff
-                            output['string'] = stringDiff
-                            print json.dumps(output)
+                        entropicDiff = {}
+                        entropicDiff['date'] = commit_time
+                        entropicDiff['branch'] = branch_name
+                        entropicDiff['commit'] = prev_commit.message
+                        entropicDiff['diff'] = blob.diff.decode() 
+                        entropicDiff['stringsFound'] = stringsFound
+                        output["entropicDiffs"].append(entropicDiff)
+                        if printJson:
+                            print(json.dumps(output, sort_keys=True, indent=4))
                         else:
                             print(bcolors.OKGREEN + "Date: " + commit_time + bcolors.ENDC)
                             print(bcolors.OKGREEN + "Branch: " + branch_name + bcolors.ENDC)
@@ -136,13 +127,14 @@ def find_strings(git_url, output):
                             print(printableDiff)
 
             prev_commit = curr_commit
-    return project_path
+    output["project_path"] = project_path
+    return output
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find secrets hidden in the depths of git.')
     parser.add_argument('--json', dest="output_json", action="store_true", help="Output in JSON")
     parser.add_argument('git_url', type=str, help='URL for secret searching')
-
     args = parser.parse_args()
-    project_path = find_strings(args.git_url, args.output_json)
+    output = find_strings(args.git_url, args.output_json)
+    project_path = output["project_path"]
     shutil.rmtree(project_path, onerror=del_rw)
