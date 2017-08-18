ff26e47e337d1078a950b39fa1b461523bdcf57a
Clyde Tedrick <iamclyde@gmail.com>
2017-08-18 10:34:37 -0400

    base for commit message check

--- truffleHog/truffleHog.py
+++ truffleHog/truffleHog.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import shutil
-import sys
 import math
 import datetime
 import argparse
@@ -106,25 +105,12 @@ def find_strings(git_url, printJson=False):
 def printEntropyForDiff(diff, branch_name, prev_commit, output, printJson):
     for blob in diff:
         # print i.a_blob.data_stream.read()
-        printableDiff = blob.diff.decode('utf-8', errors='replace')
-        if printableDiff.startswith("Binary files"):
+        printable_diff = blob.diff.decode('utf-8', errors='replace')
+        if printable_diff.startswith("Binary files"):
             continue
-        stringsFound = []
         lines = blob.diff.decode('utf-8', errors='replace').split("\n")
-        for line in lines:
-            for word in line.split():
-                base64_strings = get_strings_of_set(word, BASE64_CHARS)
-                hex_strings = get_strings_of_set(word, HEX_CHARS)
-                for string in base64_strings:
-                    b64Entropy = shannon_entropy(string, BASE64_CHARS)
-                    if b64Entropy > 4.5:
-                        stringsFound.append(string)
-                        printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
-                for string in hex_strings:
-                    hexEntropy = shannon_entropy(string, HEX_CHARS)
-                    if hexEntropy > 3:
-                        stringsFound.append(string)
-                        printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+        [stringsFound, printable_diff] = entropy_in_strings(lines, printable_diff)
+
         if len(stringsFound) > 0:
             commit_time = datetime.datetime.fromtimestamp(prev_commit.committed_date).strftime('%Y-%m-%d %H:%M:%S')
             entropicDiff = {}
@@ -140,7 +126,25 @@ def printEntropyForDiff(diff, branch_name, prev_commit, output, printJson):
                 print(bcolors.OKGREEN + "Date: " + commit_time + bcolors.ENDC)
                 print(bcolors.OKGREEN + "Branch: " + branch_name + bcolors.ENDC)
                 print(bcolors.OKGREEN + "Commit: " + prev_commit.message + bcolors.ENDC)
-                print(printableDiff)
+                print(printable_diff)
+
+def entropy_in_strings(lines, printable_diff):
+    stringsFound = []
+    for line in lines:
+        for word in line.split():
+            base64_strings = get_strings_of_set(word, BASE64_CHARS)
+            hex_strings = get_strings_of_set(word, HEX_CHARS)
+            for string in base64_strings:
+                b64Entropy = shannon_entropy(string, BASE64_CHARS)
+                if b64Entropy > 4.5:
+                    stringsFound.append(string)
+                    printable_diff = printable_diff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+            for string in hex_strings:
+                hexEntropy = shannon_entropy(string, HEX_CHARS)
+                if hexEntropy > 3:
+                    stringsFound.append(string)
+                    printable_diff = printable_diff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+    return [stringsFound, printable_diff]
 
 if __name__ == "__main__":
     main()
