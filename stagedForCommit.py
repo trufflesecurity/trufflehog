38486ec0e16ca20b87762d4a693324c1b6f6e640
halprin <halprin@users.noreply.github.com>
2017-08-17 18:27:44 -0600

    Return non-0 status when entropy is found

--- stagedForCommit.py
+++ stagedForCommit.py
@@ -10,9 +10,17 @@ def main():
     previous_commit = repo.head.commit
     diff = previous_commit.diff(create_patch=True)
     current_branch = repo.active_branch.name
-    output = {"entropicDiffs": []}
+    output = {'entropicDiffs': []}
 
     truffleHog.printEntropyForDiff(diff, current_branch, previous_commit, output, False)
 
-if __name__ == "__main__":
-    main()
+    found_entropy = len(output['entropicDiffs']) > 0
+    if found_entropy:
+        print(truffleHog.bcolors.FAIL + 'There was at least one word in the index that contains too much entropy. '
+                                        'Are there secrets that shouldn\'t be committed?' + truffleHog.bcolors.ENDC)
+
+    return found_entropy
+
+if __name__ == '__main__':
+    failed = main()
+    exit(1 if failed else 0)
