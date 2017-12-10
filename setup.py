8e3a8c499babc75ba816da90ffdb47618ab01d24
Dylan <dxa4481@rit.edu>
2017-12-10 12:57:57 -0800

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='truffleHog',
-    version='2.0.0',
+    version='2.0.1',
     description='Searches through git repositories for high entropy strings, digging deep into commit history.',
     url='https://github.com/dxa4481/truffleHog',
     author='Dylan Ayrey',
