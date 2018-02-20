3830e6f141e5d7082c788781f1c458f85df11571
calve <calvinh34@gmail.com>
2018-02-20 18:19:23 +0100

    Remove trailing whitespace

--- test_all.py
+++ test_all.py
@@ -7,7 +7,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_shannon(self):
         random_stringB64 = "ZWVTjPQSdhwRgl204Hc51YCsritMIzn8B=/p9UyeX7xu6KkAGqfm3FJ+oObLDNEva"
-        random_stringHex = "b3A0a1FDfe86dcCE945B72" 
+        random_stringHex = "b3A0a1FDfe86dcCE945B72"
         self.assertGreater(truffleHog.shannon_entropy(random_stringB64, truffleHog.BASE64_CHARS), 4.5)
         self.assertGreater(truffleHog.shannon_entropy(random_stringHex, truffleHog.HEX_CHARS), 3)
 
