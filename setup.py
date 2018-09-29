2006727cfdc066bbc4dda6a9247a26cd5b754441
Dylan <dxa4481@rit.edu>
2018-09-29 08:05:56 -0700

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='truffleHog',
-    version='2.0.95',
+    version='2.0.97',
     description='Searches through git repositories for high entropy strings, digging deep into commit history.',
     url='https://github.com/dxa4481/truffleHog',
     author='Dylan Ayrey',
