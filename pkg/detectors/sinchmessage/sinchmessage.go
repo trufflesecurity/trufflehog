054a66ebbfc6f1e93e913b6d07ef0fa2ef30054e
marlon pamisa <marlonpamisa@gmail.com>
2022-01-18 22:19:13 -0800

    added sinchmessage detector

---
+++ pkg/detectors/sinchmessage/sinchmessage.go
@@ -0,0 +1,90 @@
+package sinchmessage
+
+import (
+	"context"
+	"fmt"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"sinch"}) + `\b([a-z0-9]{32})\b`)
+	idPat  = regexp.MustCompile(detectors.PrefixRegex([]string{"sinch"}) + `\b([a-z0-9]{32})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"sinch"}
+}
+
+// FromData will find and optionally verify SinchMessage secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	idmatches := idPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+		for _, idmatch := range idmatches {
+			if len(idmatch) != 2 {
+				continue
+			}
+			resIdMatch := strings.TrimSpace(idmatch[1])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_SinchMessage,
+				Raw:          []byte(resMatch),
+			}
+
+			if verify {
+				payload := strings.NewReader(`
+				{
+				"from": "447537454435",
+				"to": [ "639668957581" ],
+				"body": "This is a test message from your Sinch account"
+				}`)
+				req, _ := http.NewRequest("POST", "https://sms.api.sinch.com/xms/v1/"+resIdMatch+"/batches", payload)
+				req.Header.Add("Content-Type", "application/json")
+				req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", resMatch))
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+						if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+			}
+
+			results = append(results, s1)
+		}
+
+	}
+
+	return detectors.CleanResults(results), nil
+}
