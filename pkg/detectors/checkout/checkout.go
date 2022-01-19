88a156f797678d3fcc53fbb7a355b523c92aae3b
Valerie Gale <valeriegaleguimmayen@gmail.com>
2022-01-18 22:19:03 -0800

    added checkout detector

---
+++ pkg/detectors/checkout/checkout.go
@@ -0,0 +1,84 @@
+package checkout
+
+import (
+	"context"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	//Tokens starting with sk_test are used for the app's sandbox environment while tokens starting with sk only are for production environment
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"checkout"}) + `\b((sk_|sk_test_)[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})\b`)
+	idPat  = regexp.MustCompile(detectors.PrefixRegex([]string{"checkout"}) + `\b(cus_[0-9a-zA-Z]{26})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"checkout"}
+}
+
+// FromData will find and optionally verify Checkout secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	idMatches := idPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 3 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		for _, idMatch := range idMatches {
+			if len(idMatch) != 2 {
+				continue
+			}
+			resIdMatch := strings.TrimSpace(idMatch[1])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_Checkout,
+				Raw:          []byte(resMatch),
+			}
+
+			if verify {
+				//Used the app's sandbox environment for this case since I can't create a live account.
+				req, _ := http.NewRequest("GET", "https://api.sandbox.checkout.com/customers/"+resIdMatch, nil)
+				req.Header.Add("Authorization", resMatch)
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+						if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+			}
+
+			results = append(results, s1)
+		}
+	}
+
+	return detectors.CleanResults(results), nil
+}
