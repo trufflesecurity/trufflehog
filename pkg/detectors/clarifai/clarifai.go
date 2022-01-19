1b4394f07ffb9c19d31d88c58778abd7302162fe
Jonee Ryan Ty <jonee316@yahoo.com>
2022-01-18 22:19:03 -0800

    added clarifai detector

---
+++ pkg/detectors/clarifai/clarifai.go
@@ -0,0 +1,87 @@
+package clarifai
+
+import (
+	"context"
+	"fmt"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"clarifai"}) + `\b([a-zA-Z0-9]{32})\b`) // could be an api key tied to an app or a personal access token (pat)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"clarifai"}
+}
+
+// FromData will find and optionally verify Clarifai secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		s1 := detectors.Result{
+			DetectorType: detectorspb.DetectorType_Clarifai,
+			Raw:          []byte(resMatch),
+		}
+
+		if verify {
+			// test for api key
+			req, _ := http.NewRequest("GET", "https://api.clarifai.com/v2/inputs", nil)
+			req.Header.Add("Authorization", fmt.Sprintf("Key %s", resMatch))
+			res, err := client.Do(req)
+			if err == nil {
+				defer res.Body.Close()
+				if res.StatusCode >= 200 && res.StatusCode < 300 {
+					s1.Verified = true
+				}
+			}
+
+			if !s1.Verified {
+				// test for pat
+				req, _ := http.NewRequest("GET", "https://api.clarifai.com/v2/users/me", nil)
+				req.Header.Add("Authorization", fmt.Sprintf("Key %s", resMatch))
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+			}
+
+		}
+
+		results = append(results, s1)
+	}
+
+	return detectors.CleanResults(results), nil
+}
