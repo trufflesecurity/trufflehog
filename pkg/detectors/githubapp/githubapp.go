0368967b2ccd95211d92e38e7099ba513e68cdf4
marlon pamisa <marlonpamisa@gmail.com>
2022-01-18 22:19:07 -0800

    added githubapp detector

---
+++ pkg/detectors/githubapp/githubapp.go
@@ -0,0 +1,104 @@
+package githubapp
+
+import (
+	"context"
+
+	// b64 "encoding/base64"
+	"fmt"
+	"time"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	appPat = regexp.MustCompile(detectors.PrefixRegex([]string{"github"}) + `\b([0-9]{6})\b`)
+
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"github"}) + `(-----BEGIN RSA PRIVATE KEY-----\s[A-Za-z0-9+\/\s]*\s-----END RSA PRIVATE KEY-----)`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"github"}
+}
+
+// FromData will find and optionally verify GitHubApp secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	appMatches := appPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+
+		resMatch := strings.TrimSpace(match[1])
+		for _, appMatch := range appMatches {
+			if len(appMatch) != 2 {
+				continue
+			}
+			appResMatch := strings.TrimSpace(appMatch[1])
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_GitHubApp,
+				Raw:          []byte(resMatch),
+			}
+
+			if verify {
+				signKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(resMatch))
+				if err != nil {
+					continue
+				}
+				//issued at time
+				iat := time.Now().Add(-60 * time.Second)
+				exp := time.Now().Add(9 * 60 * time.Second)
+
+				iss := appResMatch
+				token := jwt.New(jwt.SigningMethodRS256)
+				claims := token.Claims.(jwt.MapClaims)
+				claims["iat"] = iat.Unix()
+				claims["exp"] = exp.Unix()
+				claims["iss"] = iss
+				tokenString, err := token.SignedString(signKey)
+				if err != nil {
+					continue
+				}
+				//end get token
+
+				req, _ := http.NewRequest("GET", "https://api.github.com/app", nil)
+				req.Header.Add("Accept", "application/vnd.github.v3+json")
+				req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", tokenString))
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					}
+				}
+
+			}
+
+			results = append(results, s1)
+		}
+
+	}
+
+	return detectors.CleanResults(results), nil
+}
---
+++ pkg/detectors/githubapp/githubapp_test.go
@@ -0,0 +1,119 @@
+package githubapp
+
+import (
+	"context"
+	b64 "encoding/base64"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/kylelemons/godebug/pretty"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+func TestGitHubApp_FromChunk(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	testSecrets, err := common.GetScannersTestSecretsVersion(ctx, 3)
+	if err != nil {
+		t.Fatalf("could not get test secrets from GCP: %s", err)
+	}
+	secret := testSecrets.MustGetField("GITHUBAPP_PRIVATEKEY")
+	inactiveSecret := testSecrets.MustGetField("GITHUBAPP_PRIVATEKEY_INACTIVE")
+	appKey := testSecrets.MustGetField("GITHUBAPP_APPKEY")
+
+	sDec, _ := b64.StdEncoding.DecodeString(secret)
+	inactiveSDec, _ := b64.StdEncoding.DecodeString(inactiveSecret)
+
+	type args struct {
+		ctx    context.Context
+		data   []byte
+		verify bool
+	}
+	tests := []struct {
+		name    string
+		s       Scanner
+		args    args
+		want    []detectors.Result
+		wantErr bool
+	}{
+		{
+			name: "found, verified",
+			s:    Scanner{},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf("You can find a githubapp secret %s within githubapp %s", sDec, appKey)),
+				verify: true,
+			},
+			want: []detectors.Result{
+				{
+					DetectorType: detectorspb.DetectorType_GitHubApp,
+					Verified:     true,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "found, unverified",
+			s:    Scanner{},
+			args: args{
+				ctx:  context.Background(),
+				data: []byte(fmt.Sprintf("You can find a githubapp secret %s within githubapp %s but not valid", inactiveSDec, appKey)), // the secret would satisfy the regex but not pass validation
+
+				verify: true,
+			},
+			want: []detectors.Result{
+				{
+					DetectorType: detectorspb.DetectorType_GitHubApp,
+					Verified:     false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "not found",
+			s:    Scanner{},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte("You cannot find the secret within"),
+				verify: true,
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Scanner{}
+			got, err := s.FromData(tt.args.ctx, tt.args.verify, tt.args.data)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GitHubApp.FromData() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			for i := range got {
+				if len(got[i].Raw) == 0 {
+					t.Fatalf("no raw secret present: \n %+v", got[i])
+				}
+				got[i].Raw = nil
+			}
+			if diff := pretty.Compare(got, tt.want); diff != "" {
+				t.Errorf("GitHubApp.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
+			}
+		})
+	}
+}
+
+func BenchmarkFromData(benchmark *testing.B) {
+	ctx := context.Background()
+	s := Scanner{}
+	for name, data := range detectors.MustGetBenchmarkData() {
+		benchmark.Run(name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				s.FromData(ctx, false, data)
+			}
+		})
+	}
+}
