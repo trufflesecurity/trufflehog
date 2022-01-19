57070ad37282c2b44444f61380dd82e6fc78f055
marlon pamisa <marlonpamisa@gmail.com>
2022-01-18 22:19:15 -0800

    added twelvedata detector

---
+++ pkg/detectors/twelvedata/twelvedata.go
@@ -0,0 +1,81 @@
+package twelvedata
+
+import (
+	"context"
+	"io/ioutil"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"twelvedata"}) + `\b([a-z0-9]{32})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"twelvedata"}
+}
+
+// FromData will find and optionally verify TwelveData secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		s1 := detectors.Result{
+			DetectorType: detectorspb.DetectorType_TwelveData,
+			Raw:          []byte(resMatch),
+		}
+
+		if verify {
+			req, _ := http.NewRequest("GET", "https://api.twelvedata.com/earliest_timestamp?symbol=AAPL&interval=1day&apikey="+resMatch, nil)
+
+			res, err := client.Do(req)
+			if err == nil {
+				defer res.Body.Close()
+				bodyBytes, _ := ioutil.ReadAll(res.Body)
+				body := string(bodyBytes)
+
+				// if client_id and client_secret is valid -> 403 {"error":"invalid_grant","error_description":"Invalid authorization code"}
+				// if invalid -> 401 {"error":"access_denied","error_description":"Unauthorized"}
+				// ingenious!
+
+				if !strings.Contains(body, "401") {
+					s1.Verified = true
+				} else {
+					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+						continue
+					}
+				}
+
+			}
+		}
+
+		results = append(results, s1)
+	}
+
+	return detectors.CleanResults(results), nil
+}
