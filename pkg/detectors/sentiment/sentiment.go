1d0aae2f88e853614f385beeb2730b4c45fee493
Mildred Tosoc <mildredtosoc@gmail.com>
2022-01-18 22:19:13 -0800

    added sentiment detector

---
+++ pkg/detectors/sentiment/sentiment.go
@@ -0,0 +1,88 @@
+package sentiment
+
+import (
+	"context"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	tokenPat = regexp.MustCompile(detectors.PrefixRegex([]string{"sentiment"}) + `\b([a-zA-Z0-9]{20})\b`)
+	keyPat   = regexp.MustCompile(detectors.PrefixRegex([]string{"sentiment"}) + `\b([0-9]{17})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"sentiment"}
+}
+
+// FromData will find and optionally verify Sentiment secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	tokenMatches := tokenPat.FindAllStringSubmatch(dataStr, -1)
+	keyMatches := keyPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range tokenMatches {
+		if len(match) != 2 {
+			continue
+		}
+
+		tokenMatch := strings.TrimSpace(match[1])
+
+		for _, secret := range keyMatches {
+			if len(secret) != 2 {
+				continue
+			}
+
+			keyMatch := strings.TrimSpace(secret[1])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_Sentiment,
+				Raw:          []byte(tokenMatch),
+			}
+
+			if verify {
+				req, _ := http.NewRequest("GET", "https://api.sentimentinvestor.com/v4/parsed?symbol=AAPL&token="+tokenMatch+"&key="+keyMatch, nil)
+
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+						if detectors.IsKnownFalsePositive(tokenMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+
+						if detectors.IsKnownFalsePositive(keyMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+			}
+
+			results = append(results, s1)
+		}
+	}
+
+	return detectors.CleanResults(results), nil
+}
