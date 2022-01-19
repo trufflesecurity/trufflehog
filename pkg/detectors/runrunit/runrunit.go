74845aa1762c68748ff9a567a09ef5b093f05527
valeriegaleguimmayen <valeriegaleguimmayen@gmail.com>
2022-01-18 22:19:12 -0800

    added runrunit detector

---
+++ pkg/detectors/runrunit/runrunit.go
@@ -0,0 +1,87 @@
+package runrunit
+
+import (
+	"context"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat       = regexp.MustCompile(detectors.PrefixRegex([]string{"runrunit"}) + `\b([0-9a-f]{32})\b`)
+	userTokenPat = regexp.MustCompile(detectors.PrefixRegex([]string{"runrunit"}) + `\b([0-9A-Za-z]{18,20})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"runrunit"}
+}
+
+// FromData will find and optionally verify RunRunIt secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	userTokenMatches := userTokenPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		for _, userTokenMatch := range userTokenMatches {
+			if len(userTokenMatch) != 2 {
+				continue
+			}
+			resUserTokenMatch := strings.TrimSpace(userTokenMatch[1])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_RunRunIt,
+				Raw:          []byte(resMatch),
+			}
+
+			if verify {
+				req, _ := http.NewRequest("GET", "https://runrun.it/api/v1.0/users", nil)
+				req.Header.Add("App-Key", resMatch)
+				req.Header.Add("User-Token", resUserTokenMatch)
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+						if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+
+						if detectors.IsKnownFalsePositive(resUserTokenMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+			}
+
+			results = append(results, s1)
+		}
+	}
+
+	return detectors.CleanResults(results), nil
+}
