08ecf13b258b13a5f45070fee9ae5ad4f31c4f33
marlon pamisa <marlonpamisa@gmail.com>
2022-01-18 22:19:11 -0800

    added pusherchannelkey detector

---
+++ pkg/detectors/pusherchannelkey/pusherchannelkey.go
@@ -0,0 +1,135 @@
+package pusherchannelkey
+
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"strconv"
+	"time"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+	"net/url"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	appIdPat = regexp.MustCompile(detectors.PrefixRegex([]string{"pusher"}) + `\b([0-9]{7})\b`)
+	keyPat   = regexp.MustCompile(detectors.PrefixRegex([]string{"key"}) + `\b([a-z0-9]{20})\b`)
+	// this is currently incorrect, should be a callback from the API
+	secretPat = regexp.MustCompile(detectors.PrefixRegex([]string{"pusher"}) + `\b([a-z0-9]{20})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"pusher"}
+}
+
+const (
+	auth_version = "1.0"
+)
+
+// FromData will find and optionally verify PusherChannelKey secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	keyMatches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	appMatches := appIdPat.FindAllStringSubmatch(dataStr, -1)
+	secretMatches := secretPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, appMatch := range appMatches {
+		if len(appMatch) != 2 {
+			continue
+		}
+		resappMatch := strings.TrimSpace(appMatch[1])
+
+		for _, keyMatch := range keyMatches {
+			if len(keyMatch) != 2 {
+				continue
+			}
+			reskeyMatch := strings.TrimSpace(keyMatch[1])
+
+			for _, secretMatch := range secretMatches {
+				if len(secretMatch) != 2 {
+					continue
+				}
+				ressecretMatch := strings.TrimSpace(secretMatch[1])
+
+				s1 := detectors.Result{
+					DetectorType: detectorspb.DetectorType_PusherChannelKey,
+					Raw:          []byte(resappMatch),
+				}
+
+				if verify {
+
+					method := "POST"
+					path := "/apps/" + resappMatch + "/events"
+
+					stringPayload := `{"channels":["my-channel"],"data":"{\"message\":\"hello world\"}","name":"my_event"}`
+					payload := strings.NewReader(stringPayload)
+					_bodyMD5 := md5.New()
+					_bodyMD5.Write([]byte(stringPayload))
+					md5 := hex.EncodeToString(_bodyMD5.Sum(nil))
+
+					timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+					params := url.Values{
+						"auth_key":       {reskeyMatch},
+						"auth_timestamp": {timestamp},
+						"auth_version":   {auth_version},
+						"body_md5":       {md5},
+					}
+
+					usecd, _ := url.QueryUnescape(params.Encode())
+
+					stringToSign := strings.Join([]string{method, path, usecd}, "\n")
+					signature := hex.EncodeToString(hmacBytes([]byte(stringToSign), []byte(ressecretMatch)))
+
+					url := "http://api-ap1.pusher.com/apps/" + resappMatch + "/events?auth_key=" + reskeyMatch + "&auth_signature=" + signature + "&auth_timestamp=" + timestamp + "&auth_version=1.0&body_md5=" + md5
+
+					req, _ := http.NewRequest(method, url, payload)
+					req.Header.Add("Content-Type", "application/json")
+					res, err := client.Do(req)
+					if err == nil {
+						defer res.Body.Close()
+						if res.StatusCode >= 200 && res.StatusCode < 300 {
+							s1.Verified = true
+						} else {
+							//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+							if detectors.IsKnownFalsePositive(ressecretMatch, detectors.DefaultFalsePositives, true) {
+								continue
+							}
+						}
+					}
+				}
+
+				results = append(results, s1)
+			}
+
+		}
+
+	}
+
+	return detectors.CleanResults(results), nil
+}
+func hmacBytes(toSign, secret []byte) []byte {
+	_authSignature := hmac.New(sha256.New, secret)
+	_authSignature.Write(toSign)
+	return _authSignature.Sum(nil)
+}
---
+++ pkg/detectors/pusherchannelkey/pusherchannelkey_test.go
@@ -0,0 +1,115 @@
+package pusherchannelkey
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/kylelemons/godebug/pretty"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+func TestPusherChannelKey_FromChunk(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	testSecrets, err := common.GetScannersTestSecretsVersion(ctx, 3)
+	if err != nil {
+		t.Fatalf("could not get test secrets from GCP: %s", err)
+	}
+	appId := testSecrets.MustGetField("PUSHER_APPID")
+	key := testSecrets.MustGetField("PUSHER_KEY")
+	secret := testSecrets.MustGetField("PUSHER_SECRET")
+	inactiveSecret := testSecrets.MustGetField("PUSHER_SECRET_INACTIVE")
+
+	type args struct {
+		ctx    context.Context
+		data   []byte
+		verify bool
+	}
+	tests := []struct {
+		name    string
+		s       Scanner
+		args    args
+		want    []detectors.Result
+		wantErr bool
+	}{
+		{
+			name: "found, verified",
+			s:    Scanner{},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf("You can find a pusherSecret secret %s within pusherId %s within pusherKey %s", secret, appId, key)),
+				verify: true,
+			},
+			want: []detectors.Result{
+				{
+					DetectorType: detectorspb.DetectorType_PusherChannelKey,
+					Verified:     true,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "found, unverified",
+			s:    Scanner{},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf("You can find a pusherSecret secret %s within pusherId %s but not pusherKey %s valid", inactiveSecret, appId, key)), // the secret would satisfy the regex but not pass validation
+				verify: true,
+			},
+			want: []detectors.Result{
+				{
+					DetectorType: detectorspb.DetectorType_PusherChannelKey,
+					Verified:     false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "not found",
+			s:    Scanner{},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte("You cannot find the secret within"),
+				verify: true,
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Scanner{}
+			got, err := s.FromData(tt.args.ctx, tt.args.verify, tt.args.data)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PusherChannelKey.FromData() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			for i := range got {
+				if len(got[i].Raw) == 0 {
+					t.Fatalf("no raw secret present: \n %+v", got[i])
+				}
+				got[i].Raw = nil
+			}
+			if diff := pretty.Compare(got, tt.want); diff != "" {
+				t.Errorf("PusherChannelKey.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
+			}
+		})
+	}
+}
+
+func BenchmarkFromData(benchmark *testing.B) {
+	ctx := context.Background()
+	s := Scanner{}
+	for name, data := range detectors.MustGetBenchmarkData() {
+		benchmark.Run(name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				s.FromData(ctx, false, data)
+			}
+		})
+	}
+}
