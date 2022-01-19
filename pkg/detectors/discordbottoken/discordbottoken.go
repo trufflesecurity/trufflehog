1f88a77d15fbf610a80f00067a09105b6ab2f1ab
marlon pamisa <marlonpamisa@gmail.com>
2022-01-18 22:19:05 -0800

    added discordbottoken detector

---
+++ pkg/detectors/discordbottoken/discordbottoken.go
@@ -0,0 +1,81 @@
+package discordbottoken
+
+import (
+	"context"
+	"fmt"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+	idPat  = regexp.MustCompile(detectors.PrefixRegex([]string{"discord"}) + `\b([0-9]{17})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"discord"}) + `\b([A-Za-z0-9_-]{24}\.[A-Za-z0-9_-]{6}\.[A-Za-z0-9_-]{27})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"discord"}
+}
+
+// FromData will find and optionally verify DiscordBotToken secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	idMatch := idPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		for _, idmatch := range idMatch {
+			if len(match) != 2 {
+				continue
+			}
+			resId := strings.TrimSpace(idmatch[1])
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_DiscordBotToken,
+				Redacted:     resId,
+				Raw:          []byte(resMatch),
+			}
+
+			if verify {
+				req, _ := http.NewRequest("GET", "https://discord.com/api/v8/users/"+resId, nil)
+				req.Header.Add("Authorization", fmt.Sprintf("Bot %s", resMatch))
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+
+					}
+				}
+			}
+			results = append(results, s1)
+		}
+
+	}
+
+	return detectors.CleanResults(results), nil
+}
