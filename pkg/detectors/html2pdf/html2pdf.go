3b95d2307b00394bd496e52791904c5de5b42edb
Roxanne Tampus <roxannetampus02@gmail.com>
2022-01-18 22:19:07 -0800

    added html2pdf detector

---
+++ pkg/detectors/html2pdf/html2pdf.go
@@ -0,0 +1,82 @@
+package html2pdf
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+type html2pdfRequest struct {
+	HTML   string `json:"html"`
+	ApiKey string `json:"apiKey"`
+}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"html2pdf"}) + `\b([a-zA-Z0-9]{64})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"html2pdf"}
+}
+
+// FromData will find and optionally verify Html2Pdf secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		s1 := detectors.Result{
+			DetectorType: detectorspb.DetectorType_Html2Pdf,
+			Raw:          []byte(resMatch),
+		}
+
+		if verify {
+			req := html2pdfRequest{
+				HTML:   "Helloworld",
+				ApiKey: resMatch,
+			}
+			reqJson, _ := json.Marshal(&req)
+			reqBuf := bytes.NewReader(reqJson)
+			res, err := http.Post("https://api.html2pdf.app/v1/generate", "application/json", reqBuf)
+
+			if err == nil {
+				defer res.Body.Close()
+				if res.StatusCode >= 200 && res.StatusCode < 300 {
+					s1.Verified = true
+				} else {
+					//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+						continue
+					}
+				}
+			}
+		}
+
+		results = append(results, s1)
+	}
+
+	return detectors.CleanResults(results), nil
+}
