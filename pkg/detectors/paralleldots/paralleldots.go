b484cb61294228fc124f52a2b571b36e9355ebb9
DANEE ARQUERO <danee.arquero@gmail.com>
2022-01-18 22:19:11 -0800

    added paralleldots detector

---
+++ pkg/detectors/paralleldots/paralleldots.go
@@ -0,0 +1,98 @@
+package paralleldots
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"paralleldots"}) + `\b([0-9A-Za-z]{43})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"paralleldots"}
+}
+
+// FromData will find and optionally verify Paralleldots secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		s1 := detectors.Result{
+			DetectorType: detectorspb.DetectorType_ParallelDots,
+			Raw:          []byte(resMatch),
+		}
+
+		if verify {
+			payload := &bytes.Buffer{}
+			writer := multipart.NewWriter(payload)
+			fw, err := writer.CreateFormField("api_key")
+			if err != nil {
+				continue
+			}
+			_, err = io.Copy(fw, strings.NewReader(resMatch))
+			if err != nil {
+				continue
+			}
+			fw, err = writer.CreateFormField("text")
+			if err != nil {
+				continue
+			}
+			_, err = io.Copy(fw, strings.NewReader("sample text"))
+			if err != nil {
+				continue
+			}
+			writer.Close()
+			req, _ := http.NewRequest("POST", "https://apis.paralleldots.com/v4/intent", bytes.NewReader(payload.Bytes()))
+			req.Header.Add("Content-Type", writer.FormDataContentType())
+			res, err := client.Do(req)
+			if err == nil {
+				defer res.Body.Close()
+				bodyBytes, _ := ioutil.ReadAll(res.Body)
+				body := string(bodyBytes)
+				if (res.StatusCode >= 200 && res.StatusCode < 300) && !strings.Contains(body, "Invalid API Key") {
+					s1.Verified = true
+				} else {
+					//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+						continue
+					}
+				}
+			}
+		}
+
+		results = append(results, s1)
+	}
+
+	return detectors.CleanResults(results), nil
+}
