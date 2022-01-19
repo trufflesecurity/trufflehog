e49f0f347171b2e6f636b13a0491c183cb9538c7
Roxanne Tampus <roxannetampus02@gmail.com>
2022-01-18 22:19:07 -0800

    added graphcms detector

---
+++ pkg/detectors/graphcms/graphcms.go
@@ -0,0 +1,88 @@
+package graphcms
+
+import (
+	"context"
+	"fmt"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat = regexp.MustCompile(`\b(ey[a-zA-Z0-9]{73}.ey[a-zA-Z0-9]{365}.[a-zA-Z0-9_-]{683})\b`)
+	idPat  = regexp.MustCompile(detectors.PrefixRegex([]string{"graph"}) + `\b([a-z0-9]{25})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"graphcms"}
+}
+
+// FromData will find and optionally verify GraphCMS secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	idMatches := idPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		for _, idMatch := range idMatches {
+			if len(idMatch) != 2 {
+				continue
+			}
+
+			resIdMatch := strings.TrimSpace(idMatch[1])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_GraphCMS,
+				Raw:          []byte(resMatch),
+			}
+
+			if verify {
+				payload := strings.NewReader(`{users {id name}}`)
+				req, _ := http.NewRequest("POST", "https://api-ap-northeast-1.graphcms.com/v2/"+resIdMatch+"/master", payload)
+				req.Header.Add("Content-Type", "application/graphql")
+				req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", resMatch))
+
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					}
+				}
+			}
+
+			if !s1.Verified {
+				if detectors.IsKnownFalsePositive(string(s1.Raw), detectors.DefaultFalsePositives, true) {
+					continue
+				}
+			}
+
+			results = append(results, s1)
+		}
+	}
+
+	return detectors.CleanResults(results), nil
+}
