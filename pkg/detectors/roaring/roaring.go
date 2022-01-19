889016fbe7a1cb842c84fab901dc9f6e3aca679a
DANEE ARQUERO <danee.arquero@gmail.com>
2022-01-18 22:19:12 -0800

    added roaring detector

---
+++ pkg/detectors/roaring/roaring.go
@@ -0,0 +1,87 @@
+package roaring
+
+import (
+	"context"
+	b64 "encoding/base64"
+	"fmt"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+
+	"github.com/trufflesecurity/trufflehog/pkg/common"
+	"github.com/trufflesecurity/trufflehog/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	clientPat = regexp.MustCompile(detectors.PrefixRegex([]string{"roaring"}) + `\b([0-9A-Za-z_-]{28})\b`)
+	secretPat = regexp.MustCompile(detectors.PrefixRegex([]string{"roaring"}) + `\b([0-9A-Za-z_-]{28})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"roaring"}
+}
+
+// FromData will find and optionally verify Roaring secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	clientMatches := clientPat.FindAllStringSubmatch(dataStr, -1)
+	secretMatches := secretPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, clientMatch := range clientMatches {
+		if len(clientMatch) != 2 {
+			continue
+		}
+		resClient := strings.TrimSpace(clientMatch[1])
+
+		for _, secretMatch := range secretMatches {
+			if len(secretMatch) != 2 {
+				continue
+			}
+			resSecret := strings.TrimSpace(secretMatch[1])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_Roaring,
+				Raw:          []byte(resClient),
+			}
+
+			if verify {
+				data := fmt.Sprintf("%s:%s", resClient, resSecret)
+				sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+				payload := strings.NewReader("grant_type=client_credentials")
+				req, _ := http.NewRequest("POST", "https://api.roaring.io/token", payload)
+				req.Header.Add("Authorization", fmt.Sprintf("Basic %s", sEnc))
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+						if detectors.IsKnownFalsePositive(resSecret, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+			}
+
+			results = append(results, s1)
+		}
+	}
+
+	return detectors.CleanResults(results), nil
+}
