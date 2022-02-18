7f7e2665a0bff455357d1c091ec9386aafaf6a09
Dustin Decker <dustin@trufflesec.com>
2022-02-17 17:24:43 -0800

    Change BasicAuthWithHeader to just Header

--- pkg/pb/sourcespb/sources.pb.validate.go
+++ pkg/pb/sourcespb/sources.pb.validate.go
@@ -3063,14 +3063,14 @@ func (m *Jenkins) validate(all bool) error {
 			}
 		}
 
-	case *Jenkins_BasicAuthWithHeader:
+	case *Jenkins_Header:
 
 		if all {
-			switch v := interface{}(m.GetBasicAuthWithHeader()).(type) {
+			switch v := interface{}(m.GetHeader()).(type) {
 			case interface{ ValidateAll() error }:
 				if err := v.ValidateAll(); err != nil {
 					errors = append(errors, JenkinsValidationError{
-						field:  "BasicAuthWithHeader",
+						field:  "Header",
 						reason: "embedded message failed validation",
 						cause:  err,
 					})
@@ -3078,16 +3078,16 @@ func (m *Jenkins) validate(all bool) error {
 			case interface{ Validate() error }:
 				if err := v.Validate(); err != nil {
 					errors = append(errors, JenkinsValidationError{
-						field:  "BasicAuthWithHeader",
+						field:  "Header",
 						reason: "embedded message failed validation",
 						cause:  err,
 					})
 				}
 			}
-		} else if v, ok := interface{}(m.GetBasicAuthWithHeader()).(interface{ Validate() error }); ok {
+		} else if v, ok := interface{}(m.GetHeader()).(interface{ Validate() error }); ok {
 			if err := v.Validate(); err != nil {
 				return JenkinsValidationError{
-					field:  "BasicAuthWithHeader",
+					field:  "Header",
 					reason: "embedded message failed validation",
 					cause:  err,
 				}
