0e2bebe6c19fa8d96e7899ed390d6bb55cbe998a
Roxanne Tampus <roxannetampus02@gmail.com>
2022-03-09 22:22:27 -0800

    added neutrinoapi detector

--- pkg/detectors/neutrinoapi/neutrinoapi.go
+++ pkg/detectors/neutrinoapi/neutrinoapi.go
@@ -6,12 +6,9 @@ import (
 	"fmt"
 	"io"
 	"mime/multipart"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
