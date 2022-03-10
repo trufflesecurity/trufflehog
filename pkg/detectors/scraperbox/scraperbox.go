56aacc107e446aae3f9626b931d913755846a0c3
Roxanne Tampus <roxannetampus02@gmail.com>
2022-03-09 22:22:30 -0800

    added scraperbox detector

--- pkg/detectors/scraperbox/scraperbox.go
+++ pkg/detectors/scraperbox/scraperbox.go
@@ -3,13 +3,10 @@ package scraperbox
 import (
 	"context"
 	"fmt"
-	"time"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
+	"time"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
