cc1e55bb57c9fd629a00c5d1c41eaf910d0b5ba5
Roxanne Tampus <roxannetampus02@gmail.com>
2022-03-09 22:22:24 -0800

    added intrinio detector

--- pkg/detectors/intrinio/intrinio.go
+++ pkg/detectors/intrinio/intrinio.go
@@ -3,12 +3,9 @@ package intrinio
 import (
 	"context"
 	"fmt"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
