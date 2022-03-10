d3791c4978c80771d57a426703ea8d8a6119dbb2
DANEE ARQUERO <danee.arquero@gmail.com>
2022-03-09 22:22:33 -0800

    added telnyx detector

--- pkg/detectors/telnyx/telnyx.go
+++ pkg/detectors/telnyx/telnyx.go
@@ -3,12 +3,9 @@ package telnyx
 import (
 	"context"
 	"fmt"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
