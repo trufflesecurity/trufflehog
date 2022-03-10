2aa35ce8c340847ed362e911b875f5a8f97f0bd4
marlon pamisa <marlonpamisa@gmail.com>
2022-03-09 22:22:21 -0800

    added everhour detector

--- pkg/detectors/everhour/everhour.go
+++ pkg/detectors/everhour/everhour.go
@@ -3,12 +3,9 @@ package everhour
 import (
 	"context"
 	"fmt"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
