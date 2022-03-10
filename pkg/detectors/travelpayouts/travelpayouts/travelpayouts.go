1269ba714ee7885629c3b0bfb12e8363dc53f895
Roxanne Tampus <roxannetampus02@gmail.com>
2022-03-09 22:22:34 -0800

    added travelpayouts detector

--- pkg/detectors/travelpayouts/travelpayouts/travelpayouts.go
+++ pkg/detectors/travelpayouts/travelpayouts/travelpayouts.go
@@ -48,7 +48,10 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			req, _ := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("https://api.travelpayouts.com/v2/prices/latest?currency=usd&limit=5&token=%s", resMatch), nil)
+			req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("http://api.travelpayouts.com/v2/prices/latest?currency=usd&limit=5&token=%s", resMatch), nil)
+			if err != nil {
+				continue
+			}
 			res, err := client.Do(req)
 			if err == nil {
 				defer res.Body.Close()
