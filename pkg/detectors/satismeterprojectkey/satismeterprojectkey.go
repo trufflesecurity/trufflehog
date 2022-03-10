41d2f50054a466f3037d28e80706a992750836a4
marlon pamisa <marlonpamisa@gmail.com>
2022-03-09 22:22:30 -0800

    added satismeterprojectkey detector

--- pkg/detectors/satismeterprojectkey/satismeterprojectkey.go
+++ pkg/detectors/satismeterprojectkey/satismeterprojectkey.go
@@ -4,12 +4,9 @@ import (
 	"context"
 	b64 "encoding/base64"
 	"fmt"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
