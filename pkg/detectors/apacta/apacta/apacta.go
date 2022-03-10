232fdb713722e23925d0b9ef794a270fb4166bc1
Roxanne Tampus <roxannetampus02@gmail.com>
2022-03-09 22:22:15 -0800

    added apacta detector

--- pkg/detectors/apacta/apacta/apacta.go
+++ pkg/detectors/apacta/apacta/apacta.go
@@ -48,7 +48,10 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			req, _ := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("https://app.apacta.com/api/v1/time_entries?api_key=%s", resMatch), nil)
+			req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("https://app.apacta.com/api/v1/time_entries?api_key=%s", resMatch), nil)
+			if err != nil {
+				continue
+			}
 			res, err := client.Do(req)
 			if err == nil {
 				defer res.Body.Close()
