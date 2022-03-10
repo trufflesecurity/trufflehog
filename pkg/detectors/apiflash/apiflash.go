0dd2d19b3f2c1f31ecbcb3b4638dbec03296a2bb
Roxanne Tampus <roxannetampus02@gmail.com>
2022-03-09 22:22:15 -0800

    added apiflash detector

--- pkg/detectors/apiflash/apiflash.go
+++ pkg/detectors/apiflash/apiflash.go
@@ -3,12 +3,9 @@ package apiflash
 import (
 	"context"
 	"fmt"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
