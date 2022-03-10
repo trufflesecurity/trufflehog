42c7e2c0a28f5ec22ac1d0d0742591f90dca8805
DANEE ARQUERO <danee.arquero@gmail.com>
2022-03-09 22:22:20 -0800

    added dovico detector

--- pkg/detectors/dovico/dovico.go
+++ pkg/detectors/dovico/dovico.go
@@ -3,12 +3,9 @@ package dovico
 import (
 	"context"
 	"fmt"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
