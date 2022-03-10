8e556645ec5e6448ed58a7ce4a65ba0934665705
Valerie Gale <valeriegaleguimmayen@gmail.com>
2022-03-09 22:22:18 -0800

    added checkvist detector

---
+++ pkg/detectors/checkvist/checkvist.go
@@ -0,0 +1,85 @@
+package checkvist
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat   = regexp.MustCompile(detectors.PrefixRegex([]string{"checkvist"}) + `\b([0-9a-zA-Z]{14})\b`)
+	emailPat = regexp.MustCompile(detectors.PrefixRegex([]string{"checkvist"}) + `\b([\w\.-]+@[\w-]+\.[\w\.-]{2,5})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"checkvist"}
+}
+
+// FromData will find and optionally verify Checkvist secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	emailMatches := emailPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, emailMatch := range emailMatches {
+		if len(emailMatch) != 2 {
+			continue
+		}
+		resEmailMatch := strings.TrimSpace(emailMatch[1])
+
+		for _, match := range matches {
+			if len(match) != 2 {
+				continue
+			}
+			resMatch := strings.TrimSpace(match[1])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_Checkvist,
+				Raw:          []byte(resMatch),
+			}
+
+			if verify {
+
+				payload := url.Values{}
+				payload.Add("username", resEmailMatch)
+				payload.Add("remote_key", resMatch)
+
+				req, _ := http.NewRequest("GET", "https://checkvist.com/auth/login.json?version=2", strings.NewReader(payload.Encode()))
+				req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+						if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+			}
+
+			results = append(results, s1)
+		}
+	}
+
+	return detectors.CleanResults(results), nil
+}
