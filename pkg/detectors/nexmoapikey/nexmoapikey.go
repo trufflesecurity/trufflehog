e8145690454d81c56986a0cde28080ae88708403
marlon pamisa <marlonpamisa@gmail.com>
2022-03-09 22:22:27 -0800

    added nexmoapikey detector

--- pkg/detectors/nexmoapikey/nexmoapikey.go
+++ pkg/detectors/nexmoapikey/nexmoapikey.go
@@ -2,13 +2,9 @@ package nexmoapikey
 
 import (
 	"context"
-	// "fmt"
-
-	// "log"
+	"net/http"
 	"regexp"
 	"strings"
-
-	"net/http"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
