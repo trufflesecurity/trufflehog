8104d65e4c1bb6723a26f7c5db003bfc9fb854fb
valeriegaleguimmayen <valeriegaleguimmayen@gmail.com>
2022-03-11 16:56:58 -0800

    added cexio detector

---
+++ pkg/detectors/cexio/cexio.go
@@ -0,0 +1,140 @@
+package cexio
+
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"time"
+
+	// "log"
+	"regexp"
+	"strings"
+
+	"net/http"
+	"net/url"
+
+	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat    = regexp.MustCompile(detectors.PrefixRegex([]string{"cexio", "cex.io"}) + `\b([0-9A-Za-z]{24,27})\b`)
+	secretPat = regexp.MustCompile(detectors.PrefixRegex([]string{"cexio", "cex.io"}) + `\b([0-9A-Za-z]{24,27})\b`)
+	userIdPat = regexp.MustCompile(detectors.PrefixRegex([]string{"cexio", "cex.io"}) + `\b([a-z]{2}[0-9]{9})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"cexio", "cex.io"}
+}
+
+// FromData will find and optionally verify CexIO secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	keyMatches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	secretMatches := secretPat.FindAllStringSubmatch(dataStr, -1)
+	userIdMatches := userIdPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, userIdMatch := range userIdMatches {
+		if len(userIdMatch) != 2 {
+			continue
+		}
+		resUserIdMatch := strings.TrimSpace(userIdMatch[1])
+
+		for _, keyMatch := range keyMatches {
+			if len(keyMatch) != 2 {
+				continue
+			}
+			resKeyMatch := strings.TrimSpace(keyMatch[1])
+
+			for _, secretMatch := range secretMatches {
+				if len(secretMatch) != 2 {
+					continue
+				}
+				resSecretMatch := strings.TrimSpace(secretMatch[1])
+
+				s1 := detectors.Result{
+					DetectorType: detectorspb.DetectorType_CexIO,
+					Raw:          []byte(resKeyMatch),
+				}
+
+				if verify {
+
+					timestamp := strconv.FormatInt(time.Now().Unix()*1000, 10)
+
+					signature := getCexIOPassphrase(resSecretMatch, resKeyMatch, timestamp, resUserIdMatch)
+
+					payload := url.Values{}
+					payload.Add("key", resKeyMatch)
+					payload.Add("signature", signature)
+					payload.Add("nonce", timestamp)
+
+					req, _ := http.NewRequestWithContext(ctx, "POST", "https://cex.io/api/balance/", strings.NewReader(payload.Encode()))
+					req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+					res, err := client.Do(req)
+					if err == nil {
+						defer res.Body.Close()
+
+						body, err := ioutil.ReadAll(res.Body)
+						if err != nil {
+							fmt.Print(err.Error())
+						}
+
+						var responseObject Response
+						json.Unmarshal(body, &responseObject)
+
+						if res.StatusCode >= 200 && res.StatusCode < 300 && responseObject.Error == "" {
+							s1.Verified = true
+						} else {
+							//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+							if detectors.IsKnownFalsePositive(resUserIdMatch, detectors.DefaultFalsePositives, true) {
+								continue
+							}
+
+							if detectors.IsKnownFalsePositive(resKeyMatch, detectors.DefaultFalsePositives, true) {
+								continue
+							}
+
+							if detectors.IsKnownFalsePositive(resSecretMatch, detectors.DefaultFalsePositives, true) {
+								continue
+							}
+						}
+					}
+				}
+
+				results = append(results, s1)
+			}
+		}
+	}
+
+	return detectors.CleanResults(results), nil
+}
+
+type Response struct {
+	Error string `json:"error"`
+}
+
+func getCexIOPassphrase(apiSecret string, apiKey string, nonce string, userId string) string {
+
+	msg := nonce + userId + apiKey
+	mac := hmac.New(sha256.New, []byte(apiSecret))
+	mac.Write([]byte(msg))
+	macsum := mac.Sum(nil)
+	return strings.ToUpper(hex.EncodeToString(macsum))
+}
---
+++ pkg/detectors/cexio/cexio_test.go
@@ -0,0 +1,116 @@
+package cexio
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/kylelemons/godebug/pretty"
+
+	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+)
+
+func TestCexIO_FromChunk(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	testSecrets, err := common.GetSecret(ctx, "trufflehog-testing", "detectors2")
+	if err != nil {
+		t.Fatalf("could not get test secrets from GCP: %s", err)
+	}
+	userId := testSecrets.MustGetField("CEXIO_USERID")
+	key := testSecrets.MustGetField("CEXIO_KEY")
+	inactiveKey := testSecrets.MustGetField("CEXIO_KEY_INACTIVE")
+	secret := testSecrets.MustGetField("CEXIO")
+	inactiveSecret := testSecrets.MustGetField("CEXIO_INACTIVE")
+
+	type args struct {
+		ctx    context.Context
+		data   []byte
+		verify bool
+	}
+	tests := []struct {
+		name    string
+		s       Scanner
+		args    args
+		want    []detectors.Result
+		wantErr bool
+	}{
+		{
+			name: "found, verified",
+			s:    Scanner{},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf("You can find a cexio userId %s with cexio key %s and cexio secret %s within", userId, key, secret)),
+				verify: true,
+			},
+			want: []detectors.Result{
+				{
+					DetectorType: detectorspb.DetectorType_CexIO,
+					Verified:     true,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "found, unverified",
+			s:    Scanner{},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf("You can find a cexio userId %s with cexio key %s and cexio secret %s within but not valid", userId, inactiveKey, inactiveSecret)), // the secret would satisfy the regex but not pass validation
+				verify: true,
+			},
+			want: []detectors.Result{
+				{
+					DetectorType: detectorspb.DetectorType_CexIO,
+					Verified:     false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "not found",
+			s:    Scanner{},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte("You cannot find the secret within"),
+				verify: true,
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Scanner{}
+			got, err := s.FromData(tt.args.ctx, tt.args.verify, tt.args.data)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CexIO.FromData() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			for i := range got {
+				if len(got[i].Raw) == 0 {
+					t.Fatalf("no raw secret present: \n %+v", got[i])
+				}
+				got[i].Raw = nil
+			}
+			if diff := pretty.Compare(got, tt.want); diff != "" {
+				t.Errorf("CexIO.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
+			}
+		})
+	}
+}
+
+func BenchmarkFromData(benchmark *testing.B) {
+	ctx := context.Background()
+	s := Scanner{}
+	for name, data := range detectors.MustGetBenchmarkData() {
+		benchmark.Run(name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				s.FromData(ctx, false, data)
+			}
+		})
+	}
+}
