db8077a069ea6e003b25fe420a2f2d2f789b04f8
Dustin Decker <dustin@trufflesec.com>
2022-03-16 09:07:33 -0700

    Update pattern

--- pkg/detectors/virustotal/virustotal.go
+++ pkg/detectors/virustotal/virustotal.go
@@ -23,7 +23,7 @@ var (
 	client = common.SaneHttpClient()
 
 	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"virustotal"}) + `\b([a-z0-9]{64})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"virustotal"}) + `\b([a-f0-9]{64})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
