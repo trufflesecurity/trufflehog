ad82a8e52c452033cbd854e0687df83d664e575b
ahrav <ahravdutta02@gmail.com>
2022-04-12 10:55:17 -0700

    Update the Teams message proto to include access token auth. (#389)
    * Update the Teams message proto to include access token auth.
    
    * Add team_id field to the Teams message proto.

--- pkg/pb/sourcespb/sources.pb.validate.go
+++ pkg/pb/sourcespb/sources.pb.validate.go
@@ -3232,7 +3232,40 @@ func (m *Teams) validate(all bool) error {
 		errors = append(errors, err)
 	}
 
+	// no validation rules for TeamId
+
 	switch m.Credential.(type) {
+
+	case *Teams_Token:
+
+		if all {
+			switch v := interface{}(m.GetToken()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, TeamsValidationError{
+						field:  "Token",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, TeamsValidationError{
+						field:  "Token",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetToken()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return TeamsValidationError{
+					field:  "Token",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
 
 	case *Teams_Authenticated:
 
