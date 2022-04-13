e683b3053a3c7669fa9238a7a8c34a20569f8748
Bill Rich <bill.rich@trufflesec.com>
2022-04-13 11:33:56 -0700

    only load cert/key with tls

--- pkg/engine/syslog.go
+++ pkg/engine/syslog.go
@@ -12,26 +12,28 @@ import (
 )
 
 func (e *Engine) ScanSyslog(ctx context.Context, address, protocol, certPath, keyPath, format string, concurrency int) error {
-	cert, err := os.ReadFile(certPath)
-	if err != nil {
-		return errors.WrapPrefix(err, "could not open TLS cert file", 0)
-	}
-
-	key, err := os.ReadFile(keyPath)
-	if err != nil {
-		return errors.WrapPrefix(err, "could not open TLS key file", 0)
-	}
-
 	conn := &sourcespb.Syslog{
 		Protocol:      protocol,
 		ListenAddress: address,
-		TlsCert:       string(cert),
-		TlsKey:        string(key),
 		Format:        format,
 	}
 
+	if certPath != "" && keyPath != "" {
+		cert, err := os.ReadFile(certPath)
+		if err != nil {
+			return errors.WrapPrefix(err, "could not open TLS cert file", 0)
+		}
+		conn.TlsCert = string(cert)
+
+		key, err := os.ReadFile(keyPath)
+		if err != nil {
+			return errors.WrapPrefix(err, "could not open TLS key file", 0)
+		}
+		conn.TlsKey = string(key)
+	}
+
 	source := syslog.Source{}
-	err = source.Init(ctx, "trufflehog - syslog", 0, 0, false, nil, concurrency)
+	err := source.Init(ctx, "trufflehog - syslog", 0, 0, false, nil, concurrency)
 	source.InjectConnection(conn)
 	if err != nil {
 		logrus.WithError(err).Error("failed to initialize syslog source")
