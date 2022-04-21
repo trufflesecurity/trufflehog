1540ab9df03be74acc600a453d8757d1d7f941d6
Bill Rich <bill.rich@gmail.com>
2022-04-21 09:08:51 -0800

    Log explanation of non-zero exit with found results (#431)
    * Log explanation of non-zero exit with found results
    
    * Change exit code and add documentation
    
    * Adjust exit code handling
    
    * Make action fail on found results
    
    * Use new overseer
    
    * Improve wording
    
    * Update README.md

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/trufflesecurity/trufflehog/v3
 
 go 1.17
 
-replace github.com/jpillora/overseer => github.com/trufflesecurity/overseer v1.1.7-custom4
+replace github.com/jpillora/overseer => github.com/trufflesecurity/overseer v1.1.7-custom5
 
 replace github.com/zricethezav/gitleaks/v8 => github.com/trufflesecurity/gitleaks/v8 v8.6.1-custom3
 
--- main.go
+++ main.go
@@ -42,6 +42,7 @@ var (
 	// rules = cli.Flag("rules", "Path to file with custom rules.").String()
 	printAvgDetectorTime = cli.Flag("print-avg-detector-time", "Print the average time spent on each detector.").Bool()
 	noUpdate             = cli.Flag("no-update", "Don't check for updates.").Bool()
+	fail                 = cli.Flag("fail", "Exit with code 183 if results are found.").Bool()
 
 	gitScan             = cli.Command("git", "Find credentials in git repositories.")
 	gitScanURI          = gitScan.Arg("uri", "Git repository URL. https:// or file:// schema expected.").Required().String()
@@ -240,8 +241,9 @@ func run(state overseer.State) {
 		printAverageDetectorTime(e)
 	}
 
-	if foundResults {
-		os.Exit(1)
+	if foundResults && *fail {
+		logrus.Debug("exiting with code 183 because results were found")
+		os.Exit(183)
 	}
 }
 
