3eb43c5973db2414b448e829283dd8e7dacb6dda
Roxanne Tampus <roxannetampus02@gmail.com>
2022-04-21 08:23:37 +0800

    enhanced regex

--- pkg/detectors/aha/aha.go
+++ pkg/detectors/aha/aha.go
@@ -21,7 +21,7 @@ var (
 	client = common.SaneHttpClient()
 
 	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"aha"}) + `\b([0-9a-zA-Z]{64})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"aha"}) + `\b([0-9a-f]{64})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
