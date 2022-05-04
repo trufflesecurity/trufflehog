da03974cea0272da71cdcf64853a2543aa4980fc
bill-rich <bill.rich@gmail.com>
2022-05-04 15:43:54 -0700

    Use default case

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -255,10 +255,8 @@ func fragmentFirstLine(chunk *sources.Chunk) (int64, *int64) {
 		fragmentStart = &metadata.Bitbucket.Line
 	case *source_metadatapb.MetaData_Gerrit:
 		fragmentStart = &metadata.Gerrit.Line
+	default:
+		return 0, nil
 	}
-	fragStartVal := int64(0)
-	if fragmentStart != nil {
-		fragStartVal = *fragmentStart
-	}
-	return fragStartVal, fragmentStart
+	return *fragmentStart, fragmentStart
 }
