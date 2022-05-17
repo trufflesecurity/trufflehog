5b3523f1acc6bfe078bd98e749a7f59c02b9ecad
Marlon Pamisa <marlonpamisa@gmail.com>
2022-05-17 15:15:43 +0800

    fix comment

--- pkg/detectors/getresponse/getresponse.go
+++ pkg/detectors/getresponse/getresponse.go
@@ -21,7 +21,7 @@ var (
 	client = common.SaneHttpClient()
 
 	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"getresponse"}) + `\b([a-z0-9]{32})`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"getresponse"}) + `\b([a-z0-9]{32})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
