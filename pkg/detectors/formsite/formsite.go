162b94e6836c3a9d822d7cf4b7127a3d50624569
Marlon Pamisa <marlonpamisa@gmail.com>
2022-05-23 14:00:09 +0800

    fix comment

--- pkg/detectors/formsite/formsite.go
+++ pkg/detectors/formsite/formsite.go
@@ -32,7 +32,7 @@ func (s Scanner) Keywords() []string {
 	return []string{"formsite"}
 }
 
-// FromData will find and optionally verify Formsite secrets in a given set of bytes.
+// FromData will find and optionally verify Formsite secrets in a given set of bytes..
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
 
