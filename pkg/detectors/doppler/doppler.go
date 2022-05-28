76597d4c88502b63bf155b38bd897544ef34945b
galevalerie <galevalerievg@gmail.com>
2022-05-28 10:18:47 +0800

    enhancements for doppler detector

--- pkg/detectors/doppler/doppler.go
+++ pkg/detectors/doppler/doppler.go
@@ -20,20 +20,34 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(`\b(dp\.pt\.[a-zA-Z0-9]{43})\b`)
+	personalTokenPat = regexp.MustCompile(detectors.PrefixRegex([]string{"doppler"}) + `\b(dp\.pt\.[a-zA-Z0-9]{40,44})\b`)
+	serviceTokenPat  = regexp.MustCompile(detectors.PrefixRegex([]string{"doppler"}) + `\b(dp\.st\.(?:[a-z0-9\-_]{2,35}\.)?[a-zA-Z0-9]{40,44})\b`)
+	auditTokenPat    = regexp.MustCompile(detectors.PrefixRegex([]string{"doppler"}) + `\b(dp\.audit\.[a-zA-Z0-9]{40,44})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
 // Use identifiers in the secret preferably, or the provider name.
 func (s Scanner) Keywords() []string {
-	return []string{"dp.pt."}
+	return []string{"doppler"}
 }
 
 // FromData will find and optionally verify Doppler secrets in a given set of bytes.
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
 
-	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	matches := personalTokenPat.FindAllStringSubmatch(dataStr, -1)
+	serviceTokenMatch := serviceTokenPat.FindAllStringSubmatch(dataStr, -1)
+	auditTokenMatch := auditTokenPat.FindAllStringSubmatch(dataStr, -1)
+
+	isServiceToken := false
+
+	//Validate which token is detected. Default will be the Personal Token
+	if len(serviceTokenMatch) > 0 {
+		matches = serviceTokenMatch
+		isServiceToken = true
+	} else if len(auditTokenMatch) > 0 {
+		matches = auditTokenMatch
+	}
 
 	for _, match := range matches {
 		if len(match) != 2 {
@@ -47,7 +61,13 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.doppler.com/v3/workplace", nil)
+			//This url can both be used by Personal Token and Audit Token
+			reqUrl := "https://api.doppler.com/v3/workplace"
+			if isServiceToken {
+				reqUrl = "https://api.doppler.com/v3/projects"
+			}
+
+			req, err := http.NewRequestWithContext(ctx, "GET", reqUrl, nil)
 			if err != nil {
 				continue
 			}
