d4e81a497604a04a50c49de2e271cb9a6932001d
Roxanne Tampus <roxannetampus02@gmail.com>
2022-07-12 10:42:52 +0800

    added api2convert detector

---
+++ pkg/detectors/api2convert/api2convert.go
@@ -0,0 +1,75 @@
+package api2convert
+
+import (
+	"context"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time.
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"api2convert"}) + `\b([0-9a-f]{32})\b`)
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"api2convert"}
+}
+
+// FromData will find and optionally verify Api2Convert secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		s1 := detectors.Result{
+			DetectorType: detectorspb.DetectorType_Api2Convert,
+			Raw:          []byte(resMatch),
+		}
+
+		if verify {
+			payload := strings.NewReader(`{ "input": [{ "type": "remote", "source": "https://example-files.online-convert.com/raster%20image/png/example.png" }], "conversion": [{ "category": "document", "target": "txt", "options": { "ocr": true } }] }`)
+			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.api2convert.com/v2/jobs", payload)
+			if err != nil {
+				continue
+			}
+			req.Header.Add("Accept", "application/vnd.api2convert+json; version=3")
+			req.Header.Add("x-oc-api-key", resMatch)
+			res, err := client.Do(req)
+			if err == nil {
+				defer res.Body.Close()
+				if res.StatusCode >= 200 && res.StatusCode < 300 {
+					s1.Verified = true
+				} else {
+					// This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key.
+					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+						continue
+					}
+				}
+			}
+		}
+
+		results = append(results, s1)
+	}
+
+	return detectors.CleanResults(results), nil
+}
