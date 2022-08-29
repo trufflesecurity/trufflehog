eed9aa8123b3ccd0d22caccab3c1a8503e131132
Dustin Decker <dustin@trufflesec.com>
2022-08-29 11:45:12 -0700

    fix nits

--- pkg/common/recover.go
+++ pkg/common/recover.go
@@ -11,17 +11,15 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
 )
 
-// Recover recovers panics and reports to Sentry before exiting
+// Recover handles panics and reports to Sentry before exiting.
 func Recover(ctx context.Context) {
 	if err := recover(); err != nil {
 		panicStack := string(debug.Stack())
-		eventID := sentry.CurrentHub().Recover(err)
-		if eventID != nil {
+		if eventID := sentry.CurrentHub().Recover(err); eventID != nil {
 			ctx.Logger().Info("panic captured", "event_id", *eventID)
 		}
 		fmt.Fprint(os.Stderr, panicStack)
-		flushed := sentry.Flush(time.Second * 5)
-		if !flushed {
+		if !sentry.Flush(time.Second * 5) {
 			ctx.Logger().Info("sentry flush failed")
 		}
 		os.Exit(1)
