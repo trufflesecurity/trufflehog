626221a59631ceeb497c236ea09328c92cb37363
roxanne-tampus <roxannetampus02@gmail.com>
2022-08-30 15:52:42 +0800

    added page2images detector

---
+++ pkg/detectors/page2images/page2images.go
@@ -0,0 +1,85 @@
+package page2images
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time.
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"page2images"}) + common.BuildRegex(common.HexPattern, "", 16))
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"page2images"}
+}
+
+// FromData will find and optionally verify Page2Images secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
+
+		s1 := detectors.Result{
+			DetectorType: detectorspb.DetectorType_Page2Images,
+			Raw:          []byte(resMatch),
+		}
+
+		if verify {
+			req, err := http.NewRequestWithContext(ctx, "GET", "http://api.page2images.com/restfullink", nil)
+			if err != nil {
+				continue
+			}
+			param := req.URL.Query()
+			param.Add("p2i_url", "google.com")
+			param.Add("p2i_key", resMatch)
+			req.URL.RawQuery = param.Encode()
+			res, err := client.Do(req)
+			if err == nil {
+				bodyBytes, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					continue
+				}
+
+				bodyString := string(bodyBytes)
+				errorResponse := strings.Contains(bodyString, `error`)
+
+				defer res.Body.Close()
+				if res.StatusCode >= 200 && res.StatusCode < 300 && !errorResponse {
+					s1.Verified = true
+				} else {
+					// This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key.
+					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+						continue
+					}
+				}
+			}
+		}
+
+		results = append(results, s1)
+	}
+
+	return detectors.CleanResults(results), nil
+}
