fc404e46e4e40c95d08780a28be7382342ede646
Kevin Stilwell <kevin.stilwell@gmail.com>
2022-10-27 11:52:49 -0400

    Fixes/work based on testing

---
+++ pkg/detectors/shopify/shopify.go
@@ -0,0 +1,84 @@
+package shopify
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+)
+
+type Scanner struct{}
+
+// Ensure the Scanner satisfies the interface at compile time
+var _ detectors.Detector = (*Scanner)(nil)
+
+var (
+	client = common.SaneHttpClient()
+
+	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	keyPat    = regexp.MustCompile(`\b(shppa_|shpat_)([0-9A-Fa-f]{32})\b`)
+	domainPat = regexp.MustCompile((`[a-zA-Z0-9-]+\.myshopify\.com`))
+)
+
+// Keywords are used for efficiently pre-filtering chunks.
+// Use identifiers in the secret preferably, or the provider name.
+func (s Scanner) Keywords() []string {
+	return []string{"shppa_", "shpat_", "X-Shopify-Access-Token"}
+}
+
+// FromData will find and optionally verify Shopify secrets in a given set of bytes.
+func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
+	dataStr := string(data)
+
+	keyMatches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	domainMatches := domainPat.FindAllStringSubmatch(dataStr, -1)
+
+	for _, match := range keyMatches {
+		// if len(match) != 3 {
+		// 	continue
+		// }
+		resMatch := strings.TrimSpace(match[0])
+
+		for _, domainMatch := range domainMatches {
+			// if len(domainMatch) != 2 {
+			// 	continue
+			// }
+			domainRes := strings.TrimSpace(domainMatch[0])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_Shopify,
+				Redacted:     domainRes,
+				Raw:          []byte(resMatch),
+			}
+
+			if verify {
+				req, err := http.NewRequestWithContext(ctx, "GET", "https://"+domainRes+"/admin/api/2022-10/shop.json", nil)
+				if err != nil {
+					continue
+				}
+				req.Header.Add("X-Shopify-Access-Token", fmt.Sprintf("%s", resMatch))
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+						if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+			}
+
+			results = append(results, s1)
+		}
+	}
+
+	return detectors.CleanResults(results), nil
+}
