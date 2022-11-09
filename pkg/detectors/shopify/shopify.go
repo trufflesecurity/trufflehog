8d2c2fcbf6dc120fcc1a83f90dffede7052ce0cf
Ahrav Dutta <ahravdutta02@gmail.com>
2022-11-08 16:18:29 -0800

    Address linter warnings.

--- pkg/detectors/shopify/shopify.go
+++ pkg/detectors/shopify/shopify.go
@@ -3,7 +3,6 @@ package shopify
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"regexp"
 	"strings"
@@ -21,9 +20,9 @@ var _ detectors.Detector = (*Scanner)(nil)
 var (
 	client = common.SaneHttpClient()
 
-	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
+	// Make sure that your group is surrounded in boundry characters such as below to reduce false positives.
 	keyPat    = regexp.MustCompile(`\b(shppa_|shpat_)([0-9A-Fa-f]{32})\b`)
-	domainPat = regexp.MustCompile((`[a-zA-Z0-9-]+\.myshopify\.com`))
+	domainPat = regexp.MustCompile(`[a-zA-Z0-9-]+\.myshopify\.com`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
@@ -56,27 +55,27 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 				if err != nil {
 					continue
 				}
-				req.Header.Add("X-Shopify-Access-Token", fmt.Sprintf("%s", key))
+				req.Header.Add("X-Shopify-Access-Token", key)
 				res, err := client.Do(req)
 				if err == nil {
 					if res.StatusCode >= 200 && res.StatusCode < 300 {
 						shopifyTokenAccessScopes := shopifyTokenAccessScopes{}
 						err := json.NewDecoder(res.Body).Decode(&shopifyTokenAccessScopes)
 						if err == nil {
-							handle_array := []string{}
+							var handleArray []string
 							for _, handle := range shopifyTokenAccessScopes.AccessScopes {
-								handle_array = append(handle_array, handle.Handle)
+								handleArray = append(handleArray, handle.Handle)
 
 							}
 							s1.Verified = true
 							s1.ExtraData = map[string]string{
-								"access_scopes": strings.Join(handle_array, ","),
+								"access_scopes": strings.Join(handleArray, ","),
 							}
 						}
 						res.Body.Close()
 					}
 				} else {
-					//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+					// This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key.
 					if detectors.IsKnownFalsePositive(key, detectors.DefaultFalsePositives, true) {
 						continue
 					}
