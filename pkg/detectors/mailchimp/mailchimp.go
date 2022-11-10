6f73f188d0825d6d46410d9a1f16e1dfacc9461d
ankushgoel27 <ankushgoel27@gmail.com>
2022-11-10 11:07:44 +0530

    fixed mailchimp detector

--- pkg/detectors/mailchimp/mailchimp.go
+++ pkg/detectors/mailchimp/mailchimp.go
@@ -19,7 +19,7 @@ type Scanner struct{}
 var _ detectors.Detector = (*Scanner)(nil)
 
 var (
-	// TODO: Other country patterns?
+	client = common.SaneHttpClient()
 	keyPat = regexp.MustCompile(`[0-9a-f]{32}-us[0-9]{1,2}`)
 )
 
@@ -46,29 +46,29 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		if verify {
 			datacenter := strings.Split(match, "-")[1]
 
-			client := common.SaneHttpClient()
 			// https://mailchimp.com/developer/guides/marketing-api-conventions/
 			req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("https://%s.api.mailchimp.com/3.0/", datacenter), nil)
 			if err != nil {
 				continue
 			}
 			req.SetBasicAuth("anystring", match)
-			res, err := client.Do(req)
+			req.Header.Add("accept", "application/json")
+			res, err := http.DefaultClient.Do(req)
 			if err == nil {
-				res.Body.Close() // The request body is unused.
-
-				if res.StatusCode == 200 {
+				defer res.Body.Close()
+				if res.StatusCode >= 200 && res.StatusCode < 300 {
 					s.Verified = true
+				} else {
+					// This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key.
+					if detectors.IsKnownFalsePositive(match, detectors.DefaultFalsePositives, true) {
+						continue
+					}
 				}
 			}
-		}
-
-		if !s.Verified && detectors.IsKnownFalsePositive(string(s.Raw), detectors.DefaultFalsePositives, true) {
-			continue
 		}
 
 		results = append(results, s)
 	}
 
-	return
+	return results, nil
 }
