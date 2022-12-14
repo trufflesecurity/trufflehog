acbb3393c18d031ffac52dde796e172c33e576d8
Gonçalo Silva <goncalossilva@gmail.com>
2022-12-14 23:43:40 +0000

    Use Todoist's REST API v2
    v1 was deprecated on December 5, 2022.

--- pkg/detectors/todoist/todoist.go
+++ pkg/detectors/todoist/todoist.go
@@ -48,7 +48,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.todoist.com/rest/v1/projects", nil)
+			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.todoist.com/rest/v2/projects", nil)
 			if err != nil {
 				continue
 			}
