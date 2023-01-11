94722d3daa36dbc3fd85a1ad144deb3e517ac5df
Ahrav Dutta <ahravdutta02@gmail.com>
2023-01-11 11:41:40 -0800

    Use miccah's magic script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env ash
+#!/usr/bin/bash
 
-# "$@" should expand all args in an 'array' as they were passed in
-/usr/bin/trufflehog "$@"
+# Parse the last argument into an array of extra_args.
+mapfile -t extra_args < <(bash -c "for arg in ${*: -1}; do echo \$arg; done")
+
+/usr/bin/trufflehog "${@: 1: $#-1}" "${extra_args[@]}"
