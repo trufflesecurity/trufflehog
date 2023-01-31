b5da83f07e0b7158606edbb6e388652c5297df5f
Ahrav Dutta <ahravdutta02@gmail.com>
2023-01-30 20:39:12 -0800

    use sync pool.

--- pkg/sources/chunker.go
+++ pkg/sources/chunker.go
@@ -17,74 +17,13 @@ const (
 	PeekSize = 3 * 1024
 )
 
-// Chunker takes a chunk and splits it into chunks of ChunkSize.
-// func Chunker(originalChunk *Chunk) chan *Chunk {
-// 	chunkChan := make(chan *Chunk)
-// 	go func() {
-// 		defer close(chunkChan)
-// 		if len(originalChunk.Data) <= ChunkSize+PeekSize {
-// 			chunkChan <- originalChunk
-// 			return
-// 		}
-// 		reader := bufio.NewReaderSize(bytes.NewReader(originalChunk.Data), ChunkSize)
-// 		chunkBytes := make([]byte, ChunkSize)
-// 		for {
-// 			n, err := reader.Read(chunkBytes)
-// 			if n == 0 || errors.Is(err, io.EOF) {
-// 				break
-// 			}
-// 			if err != nil {
-// 				logrus.WithError(err).Error("Error chunking reader.")
-// 				break
-// 			}
-// 			peekData, _ := reader.Peek(PeekSize)
-// 			chunk := *originalChunk
-// 			chunk.Data = make([]byte, n+PeekSize)
-// 			copy(chunk.Data, append(chunkBytes[:n], peekData...))
-// 			chunkChan <- &chunk
-// 		}
-// 	}()
-// 	return chunkChan
-// }
-
-// func Chunker(originalChunk *Chunk) chan *Chunk {
-// 	chunkChan := make(chan *Chunk)
-// 	go func() {
-// 		defer close(chunkChan)
-// 		if len(originalChunk.Data) <= ChunkSize+PeekSize {
-// 			chunkChan <- originalChunk
-// 			return
-// 		}
-// 		// r := bytes.NewReader(originalChunk.Data)
-// 		// reader := bufio.NewReaderSize(bufio.NewReader(r), ChunkSize)
-// 		reader := bufio.NewReaderSize(bytes.NewReader(originalChunk.Data), ChunkSize)
-// 		for {
-// 			chunkBytes := make([]byte, ChunkSize)
-// 			chunk := *originalChunk
-// 			n, err := reader.Read(chunkBytes)
-// 			if err != nil && !errors.Is(err, io.EOF) {
-// 				logrus.WithError(err).Error("Error chunking reader.")
-// 				break
-// 			}
-// 			peekData, _ := reader.Peek(PeekSize)
-// 			chunk.Data = append(chunkBytes[:n], peekData...)
-// 			if n > 0 {
-// 				chunkChan <- &chunk
-// 			}
-// 			if errors.Is(err, io.EOF) {
-// 				break
-// 			}
-// 		}
-// 	}()
-// 	return chunkChan
-// }
-
 var chunkBytesPool = sync.Pool{
 	New: func() interface{} {
-		return make([]byte, ChunkSize)
+		return make([]byte, ChunkSize+PeekSize)
 	},
 }
 
+// Chunker takes a chunk and splits it into chunks of ChunkSize.
 func Chunker(originalChunk *Chunk) chan *Chunk {
 	chunkChan := make(chan *Chunk)
 	go func() {
