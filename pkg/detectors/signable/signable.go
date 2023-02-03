394eeabfc0782c008a57190a028238a025e114a0
Marcin Antkiewicz <marcin.antkiewicz@propellerheahtl.com>
2023-02-02 23:11:29 -0600

    add anchor to the signable key pattern

--- pkg/detectors/signable/signable.go
+++ pkg/detectors/signable/signable.go
@@ -28,7 +28,7 @@ var (
 // Keywords are used for efficiently pre-filtering chunks.
 // Use identifiers in the secret preferably, or the provider name.
 func (s Scanner) Keywords() []string {
-	return []string{"signable"}
+	return []string{"\bsignable"}
 }
 
 // FromData will find and optionally verify Signable secrets in a given set of bytes.
