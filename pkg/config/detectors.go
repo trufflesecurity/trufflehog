e3780c941b6707ed814304902edb1fbfe168cbb7
Miccah Castorina <m.castorina93@gmail.com>
2023-02-14 18:14:34 -0600

    Use strconv.ParseInt instead of strconv.Atoi

--- pkg/config/detectors.go
+++ pkg/config/detectors.go
@@ -129,10 +129,7 @@ func asDetectorType(input string) (dpb.DetectorType, error) {
 		return dt, nil
 	}
 	// Check if it's a detector ID.
-	if i, err := strconv.Atoi(input); err == nil {
-		if i > int(maxDetectorType) {
-			return 0, fmt.Errorf("invalid detector ID: %s", input)
-		}
+	if i, err := strconv.ParseInt(input, 10, 32); err == nil {
 		dt := dpb.DetectorType(i)
 		if _, ok := validDetectors[dt]; !ok {
 			return 0, fmt.Errorf("invalid detector ID: %s", input)
