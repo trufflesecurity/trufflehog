ee592bf926003f3870f7f45ae6fdbb3719842273
Ahrav Dutta <ahravdutta02@gmail.com>
2023-02-15 17:15:03 -0800

    udpate comments.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -8,18 +8,28 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/sources"
 )
 
+// DefaultHandlers returns all the default handlers.
 func DefaultHandlers() []Handler {
 	return []Handler{
 		&Archive{},
 	}
 }
 
+// Handler is responsible for extracting data from a file.
 type Handler interface {
+	// FromFile takes in a file and returns a channel of bytes that are
+	// extracted from the file.
 	FromFile(context.Context, io.Reader) chan []byte
+	// IsFiletype takes in a file and returns whether the file is of the
+	// given type.
 	IsFiletype(context.Context, io.Reader) (io.Reader, bool)
+	// New creates a new handler.
 	New()
 }
 
+// HandleFile takes in different file types and extracts the data from them.
+// It sends the chunked data on the chunksChan.
+// It returns true if the file was handled, false otherwise.
 func HandleFile(ctx context.Context, file io.Reader, chunkSkel *sources.Chunk, chunksChan chan *sources.Chunk) bool {
 	// Find a handler for this file.
 	var handler Handler
@@ -44,6 +54,7 @@ func HandleFile(ctx context.Context, file io.Reader, chunkSkel *sources.Chunk, c
 		if common.IsDone(ctx) {
 			return false
 		}
+		// Send data on chunksChan.
 		chunksChan <- &chunk
 	}
 	return true
