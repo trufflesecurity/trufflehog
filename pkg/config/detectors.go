ef5a0504e3f7610f3aaf3b5bfe7367a59ef6f443
steeeve <steve@trufflesec.com>
2023-03-02 15:25:02 -0500

    Adjust types to use DetectorID struct

--- pkg/config/detectors.go
+++ pkg/config/detectors.go
@@ -2,15 +2,15 @@ package config
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 
 	dpb "github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
-	"golang.org/x/exp/slices"
 )
 
 var (
-	specialGroups = map[string][]dpb.DetectorType{
+	specialGroups = map[string][]DetectorID{
 		"all": allDetectors(),
 	}
 
@@ -31,14 +31,19 @@ func init() {
 	}
 }
 
+type DetectorID struct {
+	ID      dpb.DetectorType
+	Version int
+}
+
 // ParseDetectors parses user supplied string into a list of detectors types.
 // "all" will return the list of all available detectors. The input is comma
 // separated and may use the case-insensitive detector name defined in the
 // protobuf, or the protobuf enum number. A range may be used as well in the
 // form "start-end". Order is preserved and duplicates are ignored.
-func ParseDetectors(input string) ([]dpb.DetectorType, error) {
-	var output []dpb.DetectorType
-	seenDetector := map[dpb.DetectorType]struct{}{}
+func ParseDetectors(input string) ([]DetectorID, error) {
+	var output []DetectorID
+	seenDetector := map[DetectorID]struct{}{}
 	for _, item := range strings.Split(input, ",") {
 		item = strings.TrimSpace(item)
 		if item == "" {
@@ -64,23 +69,23 @@ func ParseDetectors(input string) ([]dpb.DetectorType, error) {
 }
 
 // allDetectors reutrns an ordered slice of all detector types.
-func allDetectors() []dpb.DetectorType {
-	all := make([]dpb.DetectorType, 0, len(dpb.DetectorType_name))
+func allDetectors() []DetectorID {
+	all := make([]DetectorID, 0, len(dpb.DetectorType_name))
 	for id := range dpb.DetectorType_name {
-		all = append(all, dpb.DetectorType(id))
+		all = append(all, DetectorID{ID: dpb.DetectorType(id)})
 	}
-	slices.Sort(all)
+	sort.Slice(all, func(i, j int) bool { return all[i].ID < all[j].ID })
 	return all
 }
 
 // asRange converts a single input into a slice of detector types. If the input
 // is not in range format, a slice of length 1 is returned. Unbounded ranges
 // are allowed.
-func asRange(input string) ([]dpb.DetectorType, error) {
+func asRange(input string) ([]DetectorID, error) {
 	// Check if it's a single detector type.
 	dt, err := asDetectorType(input)
 	if err == nil {
-		return []dpb.DetectorType{dt}, nil
+		return []DetectorID{{ID: dt}}, nil
 	}
 
 	// Check if it's a range; if not return the error from above.
@@ -108,14 +113,14 @@ func asRange(input string) ([]dpb.DetectorType, error) {
 	if dtStart > dtEnd {
 		step = -1
 	}
-	var output []dpb.DetectorType
+	var output []DetectorID
 	for dt := dtStart; dt != dtEnd; dt += step {
 		if _, ok := validDetectors[dt]; !ok {
 			continue
 		}
-		output = append(output, dt)
+		output = append(output, DetectorID{ID: dt})
 	}
-	return append(output, dtEnd), nil
+	return append(output, DetectorID{ID: dtEnd}), nil
 }
 
 // asDetectorType converts the case-insensitive input into a detector type.
