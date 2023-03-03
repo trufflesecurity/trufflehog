34ab6ff170795c6e41dab550bae5d524170298a6
Ahrav Dutta <ahravdutta02@gmail.com>
2023-03-03 14:48:30 -0800

    Add tests for chunking objects.

--- pkg/sources/gcs/gcs_test.go
+++ pkg/sources/gcs/gcs_test.go
@@ -2,12 +2,15 @@ package gcs
 
 import (
 	"fmt"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/source_metadatapb"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/sourcespb"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/sources"
 )
@@ -50,36 +53,67 @@ func (m *mockReader) Read(_ []byte) (n int, err error) {
 	return 0, nil
 }
 
-func (m *mockObjectManager) listObjects(context.Context) (chan BinaryReader, error) {
-	ch := make(chan BinaryReader)
-	defer close(ch)
-
+func (m *mockObjectManager) listObjects(context.Context) (chan io.Reader, error) {
 	if m.wantErr {
 		return nil, fmt.Errorf("some error")
 	}
 
-	// Add 5 objects to the channel.
-	for i := 0; i < 5; i++ {
-		ch <- &object{
-			name:        fmt.Sprintf("object%d", i),
-			bucket:      testBucket,
-			contentType: "plain/text",
-			owner:       "testman@test.com",
-			link:        fmt.Sprintf("https://storage.googleapis.com/%s/%s", testBucket, fmt.Sprintf("object%d", i)),
-			acl:         []string{"authenticatedUsers"},
-			size:        42,
-			Reader:      &mockReader{},
+	ch := make(chan io.Reader)
+	go func() {
+		defer close(ch)
+		// Add 5 objects to the channel.
+		for i := 0; i < 5; i++ {
+			ch <- createTestObject(i)
 		}
-	}
+	}()
 
 	return ch, nil
 }
 
+func createTestObject(id int) object {
+	return object{
+		name:        fmt.Sprintf("object%d", id),
+		bucket:      testBucket,
+		contentType: "plain/text",
+		owner:       "testman@test.com",
+		link:        fmt.Sprintf("https://storage.googleapis.com/%s/%s", testBucket, fmt.Sprintf("object%d", id)),
+		// acl:         []string{"authenticatedUsers"},
+		size:   42,
+		Reader: &mockReader{},
+	}
+}
+
+func createTestSourceChunk(id int) *sources.Chunk {
+	return &sources.Chunk{
+		SourceName: "test",
+		SourceType: sourcespb.SourceType_SOURCE_TYPE_GCS,
+		SourceID:   0,
+		Verify:     true,
+		SourceMetadata: &source_metadatapb.MetaData{
+			Data: &source_metadatapb.MetaData_Gcs{
+				Gcs: &source_metadatapb.GCS{
+					Filename: fmt.Sprintf("object%d", id),
+					Bucket:   testBucket,
+					// ContentType: "plain/text",
+					Email: "testman@test.com",
+					Link:  fmt.Sprintf("https://storage.googleapis.com/%s/%s", testBucket, fmt.Sprintf("object%d", id)),
+					// Acl:         []string{"authenticatedUsers"},
+					// Size:        42,
+				},
+			},
+		},
+	}
+}
+
 func TestSourceChunks_ListObjects(t *testing.T) {
 	ctx := context.Background()
-	source := &Source{gcsManager: &mockObjectManager{}}
-
 	chunksCh := make(chan *sources.Chunk, 1)
+
+	source := &Source{
+		gcsManager: &mockObjectManager{},
+		jobPool:    new(errgroup.Group),
+		chunksCh:   chunksCh,
+	}
 
 	go func() {
 		defer close(chunksCh)
@@ -87,16 +121,25 @@ func TestSourceChunks_ListObjects(t *testing.T) {
 		assert.Nil(t, err)
 	}()
 
+	res := make([]*sources.Chunk, 0, 5)
+	for i := 0; i < 5; i++ {
+		res = append(res, createTestSourceChunk(i))
+	}
+
 	// Ensure we get 5 objects back.
 	count := 0
+
 	for ch := range chunksCh {
-		fmt.Printf("chunk: %v", ch)
+		assert.Equal(
+			t, res[count].SourceMetadata.Data.(*source_metadatapb.MetaData_Gcs).Gcs.Filename,
+			ch.SourceMetadata.Data.(*source_metadatapb.MetaData_Gcs).Gcs.Filename,
+		)
 		count++
 	}
 	assert.Equal(t, 5, count)
 }
 
-func TestSourceChunks_ListObjectsError(t *testing.T) {
+func TestSourceChunks_ListObjects_Error(t *testing.T) {
 	ctx := context.Background()
 	source := &Source{gcsManager: &mockObjectManager{}}
 
