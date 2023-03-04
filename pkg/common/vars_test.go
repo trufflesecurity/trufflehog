b1159f3c8e704ad4d7c8c656dea1739fe2c33b23
Ahrav Dutta <ahravdutta02@gmail.com>
2023-03-03 20:51:33 -0800

    Add comment.

--- pkg/common/vars_test.go
+++ pkg/common/vars_test.go
@@ -8,6 +8,7 @@ func TestSkipFile(t *testing.T) {
 		want bool
 	}
 
+	// Add a test case for each ignored extension.
 	testCases := make([]testCase, 0, len(IgnoredExtensions)+1)
 	for _, ext := range IgnoredExtensions {
 		testCases = append(testCases, testCase{
@@ -15,6 +16,8 @@ func TestSkipFile(t *testing.T) {
 			want: true,
 		})
 	}
+
+	// Add a test case for a file that should not be skipped.
 	testCases = append(testCases, testCase{file: "test.txt", want: false})
 
 	for _, tt := range testCases {
