5f96a24ee47ed8c2a717a9b060779b589cf9c1f4
Dustin Decker <dustin@trufflesec.com>
2023-03-16 18:08:41 -0700

    update alchemy

--- pkg/detectors/alchemy/alchemy.go
+++ pkg/detectors/alchemy/alchemy.go
@@ -20,7 +20,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"alchemy"}) + `\b([0-9a-zA-Z]{23}-[0-9a-zA-Z]{8})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"alchemy"}) + `\b([0-9a-zA-Z]{23}_[0-9a-zA-Z]{8})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
