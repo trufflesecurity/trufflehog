021483f20f32ee7605b0b723873dff2f704b8916
Mike Vanbuskirk <mike.vanbuskirk@trufflesec.com>
2023-03-22 12:44:46 -0400

    add new gha feature to action config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 # Parse the last argument into an array of extra_args.
 mapfile -t extra_args < <(bash -c "for arg in ${*: -1}; do echo \$arg; done")
 
-/usr/bin/trufflehog "${@: 1: $#-1}" "${extra_args[@]}"
+results=$(/usr/bin/trufflehog "${@: 1: $#-1}" "${extra_args[@]}")
+echo "results=$results" >> $GITHUB_OUTPUT
