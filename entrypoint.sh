a26c3fef7dc84b7081e8033176eef9f1455e7f7f
Mike Vanbuskirk <mike.vanbuskirk@trufflesec.com>
2023-03-28 14:22:00 -0400

    ensure stdout is still provided

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 # Parse the last argument into an array of extra_args.
 mapfile -t extra_args < <(bash -c "for arg in ${*: -1}; do echo \$arg; done")
 
-results=$(/usr/bin/trufflehog "${@: 1: $#-1}" "${extra_args[@]}")
+results=$(/usr/bin/trufflehog "${@: 1: $#-1}" "${extra_args[@]}" | tee /dev/tty)
 echo "results=$results" >> $GITHUB_OUTPUT
