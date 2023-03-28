4d46c9a9dc644089dec5159167342d80d846dba6
Mike Vanbuskirk <mike.vanbuskirk@trufflesec.com>
2023-03-28 15:42:57 -0400

    revert to original entrypoint config (#1219)
    * revert to original entrypoint config
    
    * remove explicit output definition

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 # Parse the last argument into an array of extra_args.
 mapfile -t extra_args < <(bash -c "for arg in ${*: -1}; do echo \$arg; done")
 
-results=$(/usr/bin/trufflehog "${@: 1: $#-1}" "${extra_args[@]}" | tee /dev/tty)
-echo "results=$results" >> $GITHUB_OUTPUT
+/usr/bin/trufflehog "${@: 1: $#-1}" "${extra_args[@]}" 
