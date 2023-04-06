0a3760dba2a998f1ba8d6e07a7a46a1432a67180
Zubair Khan <zubair.khan@trufflesec.com>
2023-04-06 14:12:54 -0400

    add sharepoint source proto

--- pkg/pb/sourcespb/sources.pb.validate.go
+++ pkg/pb/sourcespb/sources.pb.validate.go
@@ -3977,3 +3977,154 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = SlackRealtimeValidationError{}
+
+// Validate checks the field values on Sharepoint with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *Sharepoint) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on Sharepoint with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in SharepointMultiError, or
+// nil if none found.
+func (m *Sharepoint) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *Sharepoint) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if _, err := url.Parse(m.GetEndpoint()); err != nil {
+		err = SharepointValidationError{
+			field:  "Endpoint",
+			reason: "value must be a valid URI",
+			cause:  err,
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	// no validation rules for SiteUrl
+
+	switch m.Credential.(type) {
+
+	case *Sharepoint_Token:
+		// no validation rules for Token
+
+	case *Sharepoint_Authenticated:
+
+		if all {
+			switch v := interface{}(m.GetAuthenticated()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, SharepointValidationError{
+						field:  "Authenticated",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, SharepointValidationError{
+						field:  "Authenticated",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetAuthenticated()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return SharepointValidationError{
+					field:  "Authenticated",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return SharepointMultiError(errors)
+	}
+
+	return nil
+}
+
+// SharepointMultiError is an error wrapping multiple validation errors
+// returned by Sharepoint.ValidateAll() if the designated constraints aren't met.
+type SharepointMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m SharepointMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m SharepointMultiError) AllErrors() []error { return m }
+
+// SharepointValidationError is the validation error returned by
+// Sharepoint.Validate if the designated constraints aren't met.
+type SharepointValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SharepointValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SharepointValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SharepointValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SharepointValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SharepointValidationError) ErrorName() string { return "SharepointValidationError" }
+
+// Error satisfies the builtin error interface
+func (e SharepointValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSharepoint.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SharepointValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SharepointValidationError{}
