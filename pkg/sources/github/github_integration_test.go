323c093818842d3e05d92d46a3695b651cbf4629
ahrav <ahravdutta02@gmail.com>
2023-05-03 08:35:53 -0700

    Normalize GitHub repos during enumeration (#1269)
    * Normalize repos during enumeration.
    
    * fix test.
    
    * Add benchmark.
    
    * Add benchmark.
    
    * Add more realistic benchmark values.
    
    * add gist mocks.
    
    * Remove old normalize fxn.
    
    * abstract away the repo cache.
    
    * update test.
    
    * increase repo count.
    
    * increase page limnit to 100.
    
    * move callee fxns below caller for Chunks.
    
    * Add context to normalize.
    
    * remove extra logic in normalize repo.
    
    * Delete new.txt
    
    * Delete old.txt
    
    * Handle errors in a thread safe manner.
    
    * fix test.'
    
    * fix test.
    
    * handle repos that are included by users.
    
    * Abstract include ignore logic within repoCache.
    
    * Add better comment around repoCache.
    
    * Rename params.
    
    * remove commented out code.
    
    * use repos instead of items.
    
    * remove commented out code.
    
    * Use ++ instead of atomic increment.
    
    * update to use logger var.
    
    * use cache pkg.
    
    * Address comments.
    
    * fix test.
    
    * make less sucky test.
    
    * Update test.

--- pkg/sources/github/github_integration_test.go
+++ pkg/sources/github/github_integration_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/types/known/anypb"
 
+	"github.com/trufflesecurity/trufflehog/v3/pkg/cache/memory"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/credentialspb"
@@ -52,10 +53,12 @@ func TestSource_Token(t *testing.T) {
 	}
 
 	s := Source{
-		conn:       conn,
-		httpClient: common.SaneHttpClient(),
-		log:        logr.Discard(),
+		conn:        conn,
+		httpClient:  common.SaneHttpClient(),
+		log:         logr.Discard(),
+		memberCache: map[string]struct{}{},
 	}
+	s.filteredRepoCache = s.newFilteredRepoCache(memory.New(), nil, nil)
 
 	installationClient, err := s.enumerateWithApp(ctx, "https://api.github.com", conn.GetGithubApp())
 	assert.NoError(t, err)
@@ -526,7 +529,7 @@ func TestSource_paginateGists(t *testing.T) {
 			}
 			chunksCh := make(chan *sources.Chunk, 5)
 			go func() {
-				s.addGistsByUser(ctx, tt.user)
+				s.addUserGistsToCache(ctx, tt.user)
 				chunksCh <- &sources.Chunk{}
 			}()
 			var wantedRepo string
@@ -542,11 +545,11 @@ func TestSource_paginateGists(t *testing.T) {
 
 func gistsCheckFunc(expected string, minRepos int, s *Source) sources.ChunkFunc {
 	return func(chunk *sources.Chunk) error {
-		if minRepos != 0 && minRepos > len(s.repos) {
+		if minRepos != 0 && minRepos > s.filteredRepoCache.Count() {
 			return fmt.Errorf("didn't find enough repos. expected: %d, got :%d", minRepos, len(s.repos))
 		}
 		if expected != "" {
-			for _, repo := range s.repos {
+			for _, repo := range s.filteredRepoCache.Values() {
 				if repo == expected {
 					return nil
 				}
