9e9324ad8fe3eca27f998054a4ce4ede89e576ce
Ahrav Dutta <ahravdutta02@gmail.com>
2023-05-04 08:45:08 -0700

    Update regex.

--- pkg/detectors/ipstack/ipstack.go
+++ pkg/detectors/ipstack/ipstack.go
@@ -21,7 +21,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"ipstack"}) + `\b([a-fA-f0-9]{32})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"ipstack"}) + `\b([a-fA-F0-9]{32})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
