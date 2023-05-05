f2924f3061a5ffb5f8b0958d457c26c532b082e5
Bill Rich <bill.rich@gmail.com>
2023-05-05 12:51:27 -0700

    Make sure context lines are properly handled (#1331)
    * Make sure context lines are properly handled
    
    * Fix git test to account for context change

--- pkg/gitparse/gitparse.go
+++ pkg/gitparse/gitparse.go
@@ -118,7 +118,7 @@ func (c1 *Commit) Equal(c2 *Commit) bool {
 
 // RepoPath parses the output of the `git log` command for the `source` path.
 func (c *Parser) RepoPath(ctx context.Context, source string, head string, abbreviatedLog bool, excludedGlobs []string) (chan Commit, error) {
-	args := []string{"-C", source, "log", "-p", "-U5", "--full-history", "--date=format:%a %b %d %H:%M:%S %Y %z"}
+	args := []string{"-C", source, "log", "-p", "--full-history", "--date=format:%a %b %d %H:%M:%S %Y %z"}
 	if abbreviatedLog {
 		args = append(args, "--diff-filter=AM")
 	}
@@ -143,7 +143,7 @@ func (c *Parser) RepoPath(ctx context.Context, source string, head string, abbre
 // Unstaged parses the output of the `git diff` command for the `source` path.
 func (c *Parser) Unstaged(ctx context.Context, source string) (chan Commit, error) {
 	// Provide the --cached flag to diff to get the diff of the staged changes.
-	args := []string{"-C", source, "diff", "-p", "-U5", "--cached", "--full-history", "--diff-filter=AM", "--date=format:%a %b %d %H:%M:%S %Y %z", "HEAD"}
+	args := []string{"-C", source, "diff", "-p", "--cached", "--full-history", "--diff-filter=AM", "--date=format:%a %b %d %H:%M:%S %Y %z", "HEAD"}
 
 	cmd := exec.Command("git", args...)
 
@@ -280,10 +280,8 @@ func (c *Parser) FromReader(ctx context.Context, stdOut io.Reader, commitChan ch
 			currentDiff.Content.Write(line[1:])
 		case isMinusDiffLine(line):
 			// NoOp. We only care about additions.
-		case isMessageLine(line):
-			if recentlyPassedAuthor {
-				currentCommit.Message.Write(line[4:])
-			}
+		case (isMessageLine(line) && recentlyPassedAuthor):
+			currentCommit.Message.Write(line[4:])
 		case isContextDiffLine(line):
 			currentDiff.Content.Write([]byte("\n"))
 		case isBinaryLine(line):
