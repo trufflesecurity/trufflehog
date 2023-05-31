15fe4f7860c3f28f34c9cd75d25005713637a17e
Dustin Decker <dustin@trufflesec.com>
2023-05-30 20:01:38 -0700

    fix mockaroo fps

--- pkg/detectors/mockaroo/mockaroo.go
+++ pkg/detectors/mockaroo/mockaroo.go
@@ -2,6 +2,7 @@ package mockaroo
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"regexp"
 	"strings"
@@ -56,7 +57,11 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 			if err == nil {
 				defer res.Body.Close()
 				if res.StatusCode >= 200 && res.StatusCode < 300 {
-					s1.Verified = true
+					var t []typeResponse
+					err = json.NewDecoder(res.Body).Decode(&t)
+					if err == nil && len(t) > 0 {
+						s1.Verified = true
+					}
 				} else {
 					// This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key.
 					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
@@ -74,4 +79,16 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 
 func (s Scanner) Type() detectorspb.DetectorType {
 	return detectorspb.DetectorType_Mockaroo
+}
+
+type typeResponse struct {
+	Name       string      `json:"name"`
+	Parameters []parameter `json:"parameters"`
+}
+
+type parameter struct {
+	Name        string      `json:"name"`
+	Type        string      `json:"type"`
+	Description string      `json:"description"`
+	Default     interface{} `json:"default"`
 }
