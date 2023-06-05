61950828cc33704b1cfb7589f201c583a42ece7a
Miccah Castorina <m.castorina93@gmail.com>
2023-06-05 08:51:07 -0500

    Use heuristic to choose the most likely UTF-16 decoded string

--- pkg/decoders/utf16.go
+++ pkg/decoders/utf16.go
@@ -31,25 +31,44 @@ func utf16ToUTF8(b []byte) ([]byte, error) {
 		return nil, err
 	}
 
-	buf := &bytes.Buffer{}
-	for i := 0; i < len(b); i += 2 {
-		r := rune(endianness.Uint16(b[i:]))
-		if utf8.ValidRune(r) {
-			buf.WriteRune(r)
+	var buf1, buf2 bytes.Buffer
+	var numLines1, numLines2 int
+	for i := 0; i < len(b)-1; i += 2 {
+		if r := rune(endianness.Uint16(b[i:])); utf8.ValidRune(r) {
+			buf1.WriteRune(r)
+			if r == '\n' {
+				numLines1++
+			}
+		}
+		// Guard against index out of bounds for the next check.
+		if i+1 >= len(b)-1 {
+			continue
+		}
+		// Same check but offset by one.
+		if r := rune(endianness.Uint16(b[i+1:])); utf8.ValidRune(r) {
+			buf2.WriteRune(r)
+			if r == '\n' {
+				numLines2++
+			}
 		}
 	}
 
-	return buf.Bytes(), nil
+	// Choose the one that had more newline characters as it's most likely to contain secrets.
+	// This is a heuristic and won't catch everything.
+	if numLines1 >= numLines2 {
+		return buf1.Bytes(), nil
+	}
+	return buf2.Bytes(), nil
 }
 
 func guessUTF16Endianness(b []byte) (binary.ByteOrder, error) {
-	if len(b) < 2 || len(b)%2 != 0 {
-		return nil, fmt.Errorf("input length must be even and at least 2 bytes long")
+	if len(b) < 2 {
+		return nil, fmt.Errorf("input length must at least 2 bytes long")
 	}
 
 	var evenNullBytes, oddNullBytes int
 
-	for i := 0; i < len(b); i += 2 {
+	for i := 0; i < len(b)-1; i += 2 {
 		if b[i] == 0 {
 			oddNullBytes++
 		}
