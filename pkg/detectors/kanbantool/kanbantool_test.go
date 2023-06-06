0221be569bdb14b50c2b47f3eab87f40619aa394
Ahrav Dutta <ahravdutta02@gmail.com>
2023-06-06 13:15:41 -0700

    fix test.

--- pkg/detectors/kanbantool/kanbantool_test.go
+++ pkg/detectors/kanbantool/kanbantool_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/kylelemons/godebug/pretty"
+
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
@@ -41,7 +42,7 @@ func TestKanbantool_FromChunk(t *testing.T) {
 			s:    Scanner{},
 			args: args{
 				ctx:    context.Background(),
-				data:   []byte(fmt.Sprintf("You can find a kanbantool secret %s within kanbantool %s", secret,domain)),
+				data:   []byte(fmt.Sprintf("You can find a kanbantool secret %s within kanbantool %s", secret, domain)),
 				verify: true,
 			},
 			want: []detectors.Result{
@@ -57,7 +58,7 @@ func TestKanbantool_FromChunk(t *testing.T) {
 			s:    Scanner{},
 			args: args{
 				ctx:    context.Background(),
-				data:   []byte(fmt.Sprintf("You can find a kanbantool secret %s within but not valid kanbantool %s", inactiveSecret,domain)), // the secret would satisfy the regex but not pass validation
+				data:   []byte(fmt.Sprintf("You can find a kanbantool secret %s within but not valid kanbantool %s", inactiveSecret, domain)), // the secret would satisfy the regex but not pass validation
 				verify: true,
 			},
 			want: []detectors.Result{
@@ -93,10 +94,12 @@ func TestKanbantool_FromChunk(t *testing.T) {
 					t.Fatalf("no raw secret present: \n %+v", got[i])
 				}
 				got[i].Raw = nil
+
+				if diff := pretty.Compare(got[i], tt.want[0]); diff != "" {
+					t.Errorf("Kanbantool.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
+				}
 			}
-			if diff := pretty.Compare(got, tt.want); diff != "" {
-				t.Errorf("Kanbantool.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
-			}
+
 		})
 	}
 }
