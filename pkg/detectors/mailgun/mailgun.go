0c3410c5cd5d3e3321b977b3acef033cd1cb8516
Zubair Khan <zubair.khan@trufflesec.com>
2023-06-20 19:14:56 -0400

    add new key pat for mailgun detector (#1375)
    * add new detector key pat for mailgun
    
    * resolve mailgun issue
    
    * remove unused tokenPat and commented strings import
    
    * fix closing bracket issue

--- pkg/detectors/mailgun/mailgun.go
+++ pkg/detectors/mailgun/mailgun.go
@@ -20,7 +20,11 @@ var _ detectors.Detector = (*Scanner)(nil)
 var (
 	client = common.SaneHttpClient()
 
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"mailgun"}) + `\b([a-zA-Z-0-9]{72})\b`)
+	tokenPats = map[string]*regexp.Regexp{
+		"Original MailGun Token": regexp.MustCompile(detectors.PrefixRegex([]string{"mailgun"}) + `\b([a-zA-Z-0-9]{72})\b`),
+		"Key-MailGun Token":      regexp.MustCompile(`\b(key-[a-z0-9]{32})\b`),
+		"Hex MailGun Token":      regexp.MustCompile(`\b([a-f0-9]{32}-[a-f0-9]{8}-[a-f0-9]{8})\b`),
+	}
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
@@ -33,39 +37,48 @@ func (s Scanner) Keywords() []string {
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
 
-	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
-	for _, match := range matches {
-		if len(match) != 2 {
-			continue
-		}
-		resMatch := strings.TrimSpace(match[1])
-
-		s1 := detectors.Result{
-			DetectorType: detectorspb.DetectorType_Mailgun,
-			Raw:          []byte(resMatch),
-		}
-
-		if verify {
-			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.mailgun.net/v3/domains", nil)
-			if err != nil {
+	for _, tokenPat := range tokenPats {
+		matches := tokenPat.FindAllStringSubmatch(dataStr, -1)
+		for _, match := range matches {
+			if len(match) != 2 {
 				continue
 			}
-			req.Header.Add("Authorization", fmt.Sprintf("Basic %s", resMatch))
-			res, err := client.Do(req)
-			if err == nil {
-				defer res.Body.Close()
-				if res.StatusCode >= 200 && res.StatusCode < 300 {
-					s1.Verified = true
-				} else {
-					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
-						continue
-					}
-				}
+			resMatch := strings.TrimSpace(match[1])
+
+			s1 := detectors.Result{
+				DetectorType: detectorspb.DetectorType_Mailgun,
+				Raw:          []byte(resMatch),
 			}
 
-		}
+			if verify {
+				req, err := http.NewRequestWithContext(ctx, "GET", "https://api.mailgun.net/v3/domains", nil)
+				if err != nil {
+					continue
+				}
+				
+				// If resMatch has "key" prefix, use it as the username for basic auth.
+				if strings.HasPrefix(resMatch, "key-") {
+					req.SetBasicAuth("api", resMatch)
+				} else {
+					req.Header.Add("Authorization", fmt.Sprintf("Basic %s", resMatch))
+				}
 
-		results = append(results, s1)
+				res, err := client.Do(req)
+				if err == nil {
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else {
+						if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+							continue
+						}
+					}
+				}
+
+			}
+
+			results = append(results, s1)
+		}
 	}
 
 	return results, nil
