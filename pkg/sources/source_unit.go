fb419ba8ab468fb3ea38d3ed36ec1b64b625da2a
Miccah Castorina <m.castorina93@gmail.com>
2023-06-22 11:33:53 -0500

    Implement CommonSourceUnitUnmarshaller

--- pkg/sources/source_unit.go
+++ pkg/sources/source_unit.go
@@ -1,5 +1,10 @@
 package sources
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // CommonSourceUnit is a common implementation of SourceUnit that Sources can
 // use instead of implementing their own types.
 type CommonSourceUnit struct {
@@ -9,4 +14,21 @@ type CommonSourceUnit struct {
 // Implement the SourceUnit interface.
 func (c CommonSourceUnit) SourceUnitID() string {
 	return c.ID
+}
+
+// CommonSourceUnitUnmarshaller is an implementation of SourceUnitUnmarshaller
+// for the CommonSourceUnit. A source can embed this struct to gain the
+// functionality of converting []byte to a CommonSourceUnit.
+type CommonSourceUnitUnmarshaller struct{}
+
+// Implement the SourceUnitUnmarshaller interface.
+func (c CommonSourceUnitUnmarshaller) UnmarshalSourceUnit(data []byte) (SourceUnit, error) {
+	var unit CommonSourceUnit
+	if err := json.Unmarshal(data, &unit); err != nil {
+		return nil, err
+	}
+	if unit.ID == "" {
+		return nil, fmt.Errorf("not a CommonSourceUnit")
+	}
+	return unit, nil
 }
