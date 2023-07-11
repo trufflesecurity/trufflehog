56a97114b5a6b7f8a3152845b2647e6ec68090dd
Cody Rose <rosecodym@gmail.com>
2023-07-11 15:02:58 -0400

    capture unexpected status codes

--- pkg/detectors/aws/aws.go
+++ pkg/detectors/aws/aws.go
@@ -190,6 +190,8 @@ func (s scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 						// This function will check false positives for common test words, but also it will make sure the key appears "random" enough to be a real key.
 						if detectors.IsKnownFalsePositive(resSecretMatch, detectors.DefaultFalsePositives, true) {
 							continue
+						} else {
+							s1.VerificationError = fmt.Errorf("request to %v returned unexpected status %d", res.Request.URL, res.StatusCode)
 						}
 					}
 				} else {
