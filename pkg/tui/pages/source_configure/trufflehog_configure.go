824c4d042c4ecce4c3d9b08cbb7c2a585e3de976
Hon <8292703+hxnyk@users.noreply.github.com>
2023-07-11 21:04:53 -0700

    Build flags based on truffle config inputs

--- pkg/tui/pages/source_configure/trufflehog_configure.go
+++ pkg/tui/pages/source_configure/trufflehog_configure.go
@@ -1,35 +1,43 @@
 package source_configure
 
 import (
-	tea "github.com/charmbracelet/bubbletea"
+	"strings"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/tui/components/textinputs"
 )
 
-func GetTrufflehogConfiguration() tea.Model {
+type truffleCmdModel struct {
+	textinputs.Model
+}
+
+func GetTrufflehogConfiguration() truffleCmdModel {
 	verification := textinputs.InputConfig{
-		Label:       "Verification",
+		Label:       "Skip Verification",
+		Key:         "no-verification",
 		Required:    false,
 		Help:        "Check if a suspected secret is real or not",
-		Placeholder: "true",
+		Placeholder: "false",
 	}
 
 	verifiedResults := textinputs.InputConfig{
 		Label:       "Verified results",
+		Key:         "only-verified",
 		Required:    false,
 		Help:        "Return only verified results",
 		Placeholder: "false",
 	}
 
 	jsonOutput := textinputs.InputConfig{
-		Required:    false,
 		Label:       "JSON output",
+		Key:         "json",
+		Required:    false,
 		Help:        "Output results to JSON",
 		Placeholder: "false",
 	}
 
 	excludeDetectors := textinputs.InputConfig{
 		Label:       "Exclude detectors",
+		Key:         "exclude_detectors",
 		Required:    false,
 		Help:        "Comma separated list of detector types to exclude. Protobuf name or IDs may be used, as well as ranges. IDs defined here take precedence over the include list.",
 		Placeholder: "",
@@ -37,10 +45,47 @@ func GetTrufflehogConfiguration() tea.Model {
 
 	concurrency := textinputs.InputConfig{
 		Label:       "Concurrency",
+		Key:         "concurrency",
 		Required:    false,
 		Help:        "Number of concurrent workers.",
 		Placeholder: "1",
 	}
 
-	return textinputs.New([]textinputs.InputConfig{jsonOutput, verification, verifiedResults, excludeDetectors, concurrency}).SetSkip(true)
+	return truffleCmdModel{textinputs.New([]textinputs.InputConfig{jsonOutput, verification, verifiedResults, excludeDetectors, concurrency}).SetSkip(true)}
+}
+
+func (m truffleCmdModel) GetFlags() string {
+	var command []string
+	inputs := m.GetInputs()
+
+	if isTrue(inputs["json"]) {
+		command = append(command, "--json")
+	}
+
+	if isTrue(inputs["no-verification"]) {
+		command = append(command, "--no-verification")
+	}
+
+	if isTrue(inputs["only-verified"]) {
+		command = append(command, "--only-verified")
+	}
+
+	if inputs["exclude_detectors"] != "" {
+		cmd := "--exclude-detectors=" + strings.ReplaceAll(inputs["exclude_detectors"], " ", "")
+		command = append(command, cmd)
+	}
+
+	if inputs["concurrency"] != "" {
+		command = append(command, "--concurrency="+inputs["concurrency"])
+	}
+
+	return strings.Join(command, " ")
+}
+
+func isTrue(val string) bool {
+	value := strings.ToLower(val)
+	if value == "true" || value == "yes" || value == "y" || value == "1" {
+		return true
+	}
+	return false
 }
