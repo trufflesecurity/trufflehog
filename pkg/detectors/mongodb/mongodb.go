6a0d10dd13b1efaa843e24b3fabcf23a6f6134ca
Cody Rose <rosecodym@gmail.com>
2023-07-31 17:32:54 -0400

    Revert "streamline disconnect"
    This reverts commit 2e3b1d3e65547100ca58fe0fda19691a85ff95f7.

--- pkg/detectors/mongodb/mongodb.go
+++ pkg/detectors/mongodb/mongodb.go
@@ -88,7 +88,9 @@ func verifyUri(uri string, timeout time.Duration) error {
 	if err != nil {
 		return err
 	}
-	defer client.Disconnect(ctx)
+	defer func() {
+		_ = client.Disconnect(ctx)
+	}()
 	return client.Ping(ctx, readpref.Primary())
 }
 
