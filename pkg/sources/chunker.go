63307dd388aea07808bd1e3b546aa7b5a1867b70
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2023-08-01 14:41:11 -0700

    Add common chunker.

--- pkg/sources/chunker.go
+++ pkg/sources/chunker.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"errors"
 	"io"
+
+	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
 )
 
 const (
@@ -25,6 +27,7 @@ func Chunker(originalChunk *Chunk) chan *Chunk {
 			chunkChan <- originalChunk
 			return
 		}
+
 		r := bytes.NewReader(originalChunk.Data)
 		reader := bufio.NewReaderSize(bufio.NewReader(r), ChunkSize)
 		for {
@@ -32,21 +35,109 @@ func Chunker(originalChunk *Chunk) chan *Chunk {
 			chunk := *originalChunk
 			chunkBytes = chunkBytes[:ChunkSize]
 			n, err := reader.Read(chunkBytes)
-			if err != nil && !errors.Is(err, io.EOF) {
+			if n > 0 {
+				peekData, _ := reader.Peek(len(chunkBytes) - n)
+				chunkBytes = append(chunkBytes[:n], peekData...)
+				chunk.Data = chunkBytes
+				chunkChan <- &chunk
+			}
+			if err != nil {
 				break
 			}
-			if n == 0 {
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				continue
-			}
-			peekData, _ := reader.Peek(PeekSize)
-			copy(chunkBytes[n:], peekData)
-			chunk.Data = chunkBytes[:n+len(peekData)]
-
-			chunkChan <- &chunk
 		}
 	}()
 	return chunkChan
+}
+
+type chunkerConfig struct {
+	chunkSize int
+	totalSize int
+	peekSize  int
+}
+
+// ConfigOption is a function that configures a chunker.
+type ConfigOption func(*chunkerConfig)
+
+// WithChunkSize sets the chunk size.
+func WithChunkSize(size int) ConfigOption {
+	return func(c *chunkerConfig) {
+		c.chunkSize = size
+	}
+}
+
+// WithTotalChunkSize sets the total chunk size.
+// This is the chunk size plus the peek size.
+func WithTotalChunkSize(size int) ConfigOption {
+	return func(c *chunkerConfig) {
+		c.totalSize = size
+	}
+}
+
+// WithPeekSize sets the peek size.
+func WithPeekSize(size int) ConfigOption {
+	return func(c *chunkerConfig) {
+		c.peekSize = size
+	}
+}
+
+// ChunkReader reads chunks from a reader and returns a channel of chunks and a channel of errors.
+type ChunkReader func(ctx context.Context, reader io.Reader) (<-chan []byte, <-chan error)
+
+// NewChunkReader returns a ChunkReader with the given options.
+func NewChunkReader(opts ...ConfigOption) ChunkReader {
+	config := applyOptions(opts)
+	return createReaderFn(config)
+}
+
+func applyOptions(opts []ConfigOption) *chunkerConfig {
+	// Set defaults.
+	config := &chunkerConfig{
+		chunkSize: ChunkSize,      // default
+		totalSize: TotalChunkSize, // default
+		peekSize:  PeekSize,       // default
+	}
+
+	for _, opt := range opts {
+		opt(config)
+	}
+
+	return config
+}
+
+func createReaderFn(config *chunkerConfig) ChunkReader {
+	return func(ctx context.Context, reader io.Reader) (<-chan []byte, <-chan error) {
+		return readInChunks(ctx, reader, config)
+	}
+}
+
+func readInChunks(ctx context.Context, reader io.Reader, config *chunkerConfig) (<-chan []byte, <-chan error) {
+	const channelSize = 1
+	chunkReader := bufio.NewReaderSize(reader, config.chunkSize)
+	dataChan := make(chan []byte, channelSize)
+	errChan := make(chan error, channelSize)
+
+	go func() {
+		defer close(dataChan)
+		defer close(errChan)
+
+		for {
+			chunkBytes := make([]byte, config.totalSize)
+			chunkBytes = chunkBytes[:config.chunkSize]
+			n, err := chunkReader.Read(chunkBytes)
+			if n > 0 {
+				peekData, _ := chunkReader.Peek(len(chunkBytes) - n)
+				chunkBytes = append(chunkBytes[:n], peekData...)
+				dataChan <- chunkBytes
+			}
+
+			if err != nil {
+				if !errors.Is(err, io.EOF) {
+					ctx.Logger().Error(err, "error reading chunk")
+					errChan <- err
+				}
+				return
+			}
+		}
+	}()
+	return dataChan, errChan
 }
