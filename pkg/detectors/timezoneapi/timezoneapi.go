c64069403950c35dbd12fb403f19c2bf7eacb7b6
Zubair Khan <zubair.khan@trufflesec.com>
2023-08-01 15:26:39 -0400

    tighten up regex pattern

--- pkg/detectors/timezoneapi/timezoneapi.go
+++ pkg/detectors/timezoneapi/timezoneapi.go
@@ -20,7 +20,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"timezoneapi"}) + `\b([a-zA-Z0-9]{20})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"timezoneapi"}) + `\b([a-zA-Z]{20})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
