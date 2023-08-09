f2a083016375ceac2d6091674707e6c93938d7e6
Cody Rose <rosecodym@gmail.com>
2023-08-09 11:29:15 -0400

    use test scanner in tests

--- pkg/detectors/gitlab/gitlab_test.go
+++ pkg/detectors/gitlab/gitlab_test.go
@@ -134,8 +134,7 @@ func TestGitlab_FromChunk(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := Scanner{}
-			got, err := s.FromData(tt.args.ctx, tt.args.verify, tt.args.data)
+			got, err := tt.s.FromData(tt.args.ctx, tt.args.verify, tt.args.data)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Gitlab.FromData() error = %v, wantErr %v", err, tt.wantErr)
 				return
