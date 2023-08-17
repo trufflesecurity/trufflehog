c1e1726b85bcd98c6f094dd5cebda7dd439eb779
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2023-08-17 09:38:44 -0700

    Correctly reset reader before handling archive chunk.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -68,12 +68,17 @@ func HandleFile(ctx context.Context, file io.Reader, chunkSkel *sources.Chunk, c
 			aCtx.Logger().Error(err, "error resetting re-reader")
 			return false
 		}
-		reReader.Stop()
-		var isType bool
-		if file, isType = h.IsFiletype(aCtx, reReader); isType {
-			return handleChunks(aCtx, h.FromFile(ctx, file), chunkSkel, chunksChan)
+		if _, isType := h.IsFiletype(aCtx, reReader); !isType {
+			continue
 		}
+
+		if err := reReader.Reset(); err != nil {
+			aCtx.Logger().Error(err, "error resetting re-reader")
+			return false
+		}
+		return handleChunks(aCtx, h.FromFile(ctx, reReader), chunkSkel, chunksChan)
 	}
+
 	return false
 }
 
