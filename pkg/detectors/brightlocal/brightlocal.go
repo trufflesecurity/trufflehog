058c1cd5f8dbc72d2de3e5d7906a3abdf449ab4a
Zachary Rice <zachary.rice@trufflesec.com>
2023-09-22 12:33:30 -0500

    Update brightlocal.go

--- pkg/detectors/brightlocal/brightlocal.go
+++ pkg/detectors/brightlocal/brightlocal.go
@@ -2,22 +2,26 @@ package brightlocal
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"regexp"
 	"strings"
-	"fmt"
+	"time"
+
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
 )
 
-type Scanner struct{}
-
 // Ensure the Scanner satisfies the interface at compile time
+type Scanner struct {
+	client *http.Client
+}
+
 var _ detectors.Detector = (*Scanner)(nil)
 
 var (
-	client = common.SaneHttpClient()
+	defaultClient = common.SaneHttpClientTimeOut(5 * time.Second)
 
 	//Make sure that your group is surrounded in boundry characters such as below to reduce false positives
 	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"brightlocal"}) + `\b([a-f0-9]{40})\b`)
@@ -47,28 +51,42 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			payload := strings.NewReader(fmt.Sprintf("api-key=%s",resMatch))
+			client := s.client
+			if client == nil {
+				client = defaultClient
+			}
+			payload := strings.NewReader(fmt.Sprintf("api-key=%s", resMatch))
 			req, err := http.NewRequestWithContext(ctx, "POST", "https://tools.brightlocal.com/seo-tools/api/v4/batch", payload)
 			if err != nil {
-				continue
-			}
-			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-			res, err := client.Do(req)
-			if err == nil {
-				defer res.Body.Close()
-				if res.StatusCode >= 200 && res.StatusCode < 300 {
-					s1.Verified = true
+				s1.VerificationError = err
+			} else {
+				req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+				res, err := client.Do(req)
+				if err != nil {
+					s1.VerificationError = err
 				} else {
-					//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
-					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
-						continue
+					defer res.Body.Close()
+					if res.StatusCode >= 200 && res.StatusCode < 300 {
+						s1.Verified = true
+					} else if res.StatusCode != 401 {
+						s1.VerificationError = fmt.Errorf("unexpected HTTP response status %d", res.StatusCode)
 					}
+
+					//This function will check false positives for common test words, but also it will make sure the key appears 'random' enough to be a real key
+
 				}
 			}
+		}
+		if !s1.Verified && detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
+			continue
 		}
 
 		results = append(results, s1)
 	}
 
 	return detectors.CleanResults(results), nil
+}
+
+func (s Scanner) Type() detectorspb.DetectorType {
+	return detectorspb.DetectorType_Brightlocal
 }
