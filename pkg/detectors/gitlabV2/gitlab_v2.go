6b751f8624cdc91d404431c595bc75afbb49cfb0
ahmed <ahmed.zahran@trufflesec.com>
2023-09-26 14:15:22 -0400

    nit

--- pkg/detectors/gitlabV2/gitlab_v2.go
+++ pkg/detectors/gitlabV2/gitlab_v2.go
@@ -45,6 +45,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		if len(match) != 2 {
 			continue
 		}
+
 		resMatch := strings.TrimSpace(match[1])
 		s1 := detectors.Result{
 			DetectorType: detectorspb.DetectorType_Gitlab,
