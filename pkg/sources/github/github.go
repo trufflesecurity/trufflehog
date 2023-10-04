035b92f0bf3b2dd4a92f47e8a5c03ef7ff7ada00
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2023-10-04 12:37:34 -0700

    use Repositories field from conn.

--- pkg/sources/github/github.go
+++ pkg/sources/github/github.go
@@ -222,7 +222,7 @@ func (s *Source) Init(aCtx context.Context, name string, jobID sources.JobID, so
 	}
 	s.conn = &conn
 
-	s.filteredRepoCache = s.newFilteredRepoCache(memory.New(), s.conn.IncludeRepos, s.conn.IgnoreRepos)
+	s.filteredRepoCache = s.newFilteredRepoCache(memory.New(), s.conn.GetRepositories(), s.conn.GetIgnoreRepos())
 	s.memberCache = make(map[string]struct{})
 
 	s.repoSizes = newRepoSize()
