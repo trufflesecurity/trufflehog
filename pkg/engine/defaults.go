37c233f14cc087b4de1b83a795281646c1053db6
lc <corben@lynxsecurity.io>
2023-10-04 22:54:41 -0500

    Detector-Competition-New: add ramp.com client id & secret detector

--- pkg/engine/defaults.go
+++ pkg/engine/defaults.go
@@ -3,6 +3,7 @@ package engine
 import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/envoyapikey"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/huggingface"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/ramp"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/salesforce"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/snowflake"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/sourcegraph"
@@ -1548,6 +1549,7 @@ func DefaultDetectors() []detectors.Detector {
 		sourcegraph.Scanner{},
 		tailscale.Scanner{},
 		web3storage.Scanner{},
+		&ramp.Scanner{},
 	}
 
 }
