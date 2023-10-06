6595c2c772f089f0b3c62e34bb316ae24500bcd7
za <zakiakhmad@gmail.com>
2023-10-06 10:24:13 +0700

    Single flag only --include-gist
    following current convention of `--include-forks` and
    `--include-members`.
    
    https://github.com/trufflesecurity/trufflehog/pull/1853#discussion_r1344684799

--- main.go
+++ main.go
@@ -87,7 +87,6 @@ var (
 	githubScanIssueComments = githubScan.Flag("issue-comments", "Include issue descriptions and comments in scan.").Bool()
 	githubScanPRComments    = githubScan.Flag("pr-comments", "Include pull request descriptions and comments in scan.").Bool()
 	githubScanGistComments  = githubScan.Flag("gist-comments", "Include gist comments in scan.").Bool()
-	githubScanIgnoreGist    = githubScan.Flag("ignore-gist", "Ignore gist comments in scan.").Bool()
 	githubScanIncludeGist   = githubScan.Flag("include-gist", "Include gist comments in scan.").Bool()
 
 	gitlabScan = cli.Command("gitlab", "Find credentials in GitLab repositories.")
