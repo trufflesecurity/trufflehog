a02ec01c7fbdf59fd2ca08dcf04535057aba272f
Zachary Rice <zachary.rice@trufflesec.com>
2023-10-19 10:40:48 -0500

    Update plaidkey_test.go
    hardcode dev

--- pkg/detectors/plaidkey/plaidkey_test.go
+++ pkg/detectors/plaidkey/plaidkey_test.go
@@ -26,7 +26,8 @@ func TestPlaidKey_FromChunk(t *testing.T) {
 	secret := testSecrets.MustGetField("PLAIDKEY_SECRET")
 	inactiveSecret := testSecrets.MustGetField("PLAIDKEY_SECRET_INACTIVE")
 	id := testSecrets.MustGetField("PLAIDKEY_CLIENTID")
-	env := testSecrets.MustGetField("PLAIDKEY_ENVIRONMENT") // development or production
+	// env := testSecrets.MustGetField("PLAIDKEY_ENVIRONMENT") // development or production
+	env := "development"
 
 	type args struct {
 		ctx    context.Context
