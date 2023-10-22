0b26b9621cccdce8ae71dfa222c04003bdbf1ac3
lc <corben@lynxsecurity.io>
2023-10-22 09:53:14 -0500

    Detector-Competition-Fix: Fix SuperNotes API verification

--- pkg/detectors/supernotesapi/supernotesapi.go
+++ pkg/detectors/supernotesapi/supernotesapi.go
@@ -47,7 +47,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.supernotes.app/v1/user/", nil)
+			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.supernotes.app/v1/user", nil)
 			if err != nil {
 				continue
 			}
