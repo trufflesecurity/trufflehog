f1a75395e868d5ba8348aa7a5557ca32c20c0232
Damanpreet Singh <daman.4880@gmail.com>
2023-10-27 08:16:56 +0530

    Detector-Competition-Feat: Added BetterStack API token detector (#1987)
    Co-authored-by: Ahrav Dutta <ahrav.dutta@trufflesec.com>

--- pkg/engine/defaults.go
+++ pkg/engine/defaults.go
@@ -2,6 +2,7 @@ package engine
 
 import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/anthropic"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/betterstack"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/envoyapikey"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/huggingface"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/ip2location"
@@ -1550,6 +1551,7 @@ func DefaultDetectors() []detectors.Detector {
 		ip2location.Scanner{},
 		vagrantcloudpersonaltoken.Scanner{},
 		openvpn.Scanner{},
+		betterstack.Scanner{},
 	}
 
 }
