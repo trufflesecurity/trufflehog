2567f343adbc0e9f9e00d98436127d92c8d8e53d
ankushgoel27 <ankushgoel27@gmail.com>
2023-10-30 17:41:35 +0530

    fixed verifier code

--- pkg/detectors/hubspotapikey/hubspotapikey.go
+++ pkg/detectors/hubspotapikey/hubspotapikey.go
@@ -53,7 +53,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 			res, err := client.Do(req)
 			if err == nil {
 				defer res.Body.Close()
-				if res.StatusCode >= 200 && res.StatusCode < 300 {
+				if (res.StatusCode >= 200 && res.StatusCode < 300) || res.StatusCode == 403 {
 					s1.Verified = true
 				} else {
 					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
