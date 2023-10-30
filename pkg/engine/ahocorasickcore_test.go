204a0e8659fd8e2677b6d4ba5be2072d14677ade
Cody Rose <rosecodym@gmail.com>
2023-10-30 17:44:45 -0400

    add failing test

--- pkg/engine/ahocorasickcore_test.go
+++ pkg/engine/ahocorasickcore_test.go
@@ -5,7 +5,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/custom_detectors"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/custom_detectorspb"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
 )
 
@@ -53,6 +55,53 @@ var _ detectors.Detector = (*testDetectorV1)(nil)
 var _ detectors.Detector = (*testDetectorV2)(nil)
 var _ detectors.Versioner = (*testDetectorV1)(nil)
 var _ detectors.Versioner = (*testDetectorV2)(nil)
+
+func TestAhoCorasickCore_MultipleCustomDetectorsMatchable(t *testing.T) {
+	customDetector1, err := custom_detectors.NewWebhookCustomRegex(&custom_detectorspb.CustomRegex{
+		Name:     "custom detector 1",
+		Keywords: []string{"a"},
+		Regex:    map[string]string{"": ""},
+	})
+	assert.Nil(t, err)
+
+	customDetector2, err := custom_detectors.NewWebhookCustomRegex(&custom_detectorspb.CustomRegex{
+		Name:     "custom detector 2",
+		Keywords: []string{"b"},
+		Regex:    map[string]string{"": ""},
+	})
+	assert.Nil(t, err)
+
+	testCases := []struct {
+		matchString string
+		detector    detectors.Detector
+	}{
+		{
+			matchString: "a",
+			detector:    customDetector1,
+		},
+		{
+			matchString: "b",
+			detector:    customDetector2,
+		},
+	}
+
+	var allDetectors []detectors.Detector
+	for _, tt := range testCases {
+		allDetectors = append(allDetectors, tt.detector)
+	}
+
+	ac := NewAhoCorasickCore(allDetectors)
+
+	for _, tt := range testCases {
+		matches := ac.MatchString(tt.matchString)
+		assert.Equal(t, 1, len(matches))
+
+		matchingDetectors := make(map[detectorspb.DetectorType]detectors.Detector)
+		ac.PopulateDetectorsByMatch(matches[0], matchingDetectors)
+		assert.Equal(t, 1, len(matchingDetectors))
+		assert.Equal(t, tt.detector, matchingDetectors[detectorspb.DetectorType_CustomRegex])
+	}
+}
 
 func TestAhoCorasickCore_MultipleDetectorVersionsMatchable(t *testing.T) {
 	testCases := []struct {
