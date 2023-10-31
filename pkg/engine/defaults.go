ee7410511814e819a034dd9287eb48f8ab500ed9
ankushgoel27 <ankushgoel27@gmail.com>
2023-10-31 17:40:47 +0530

    added packagename to defaults.go

--- pkg/engine/defaults.go
+++ pkg/engine/defaults.go
@@ -229,6 +229,7 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/etherscan"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/ethplorer"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/etsyapikey"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/eventbrite"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/everhour"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/exchangerateapi"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/exchangeratesapi"
@@ -1569,6 +1570,7 @@ func DefaultDetectors() []detectors.Detector {
 		lemonsqueezy.Scanner{},
 		denodeploy.Scanner{},
 		budibase.Scanner{},
+		eventbrite.Scanner{},
 	}
 
 }
