703e158648c4eaf61de59a70a8f2e81fd7f48e6f
Ankush Goel <ankushgoel27@gmail.com>
2023-11-07 03:11:37 +0530

    Detector-Competition-New : created grafana service account detector (#1960)
    * created grafana service account detector
    
    * add import
    
    ---------
    
    Co-authored-by: Zachary Rice <zachary.rice@trufflesec.com>

--- pkg/engine/defaults.go
+++ pkg/engine/defaults.go
@@ -304,6 +304,7 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/gocardless"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/goodday"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/grafana"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/grafanaserviceaccount"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/graphcms"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/graphhopper"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/groovehq"
@@ -1558,6 +1559,7 @@ func DefaultDetectors() []detectors.Detector {
 		&sourcegraphcody.Scanner{},
 		voiceflow.Scanner{},
 		ip2location.Scanner{},
+		grafanaserviceaccount.Scanner{},
 		vagrantcloudpersonaltoken.Scanner{},
 		openvpn.Scanner{},
 		&metabase.Scanner{},
