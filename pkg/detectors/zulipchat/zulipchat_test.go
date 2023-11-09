75a4a64d99f3c220b4fb690fd6c39cf6a8c1faf0
ahmed <ahmed.zahran@trufflesec.com>
2023-11-09 15:26:26 -0500

    find/replace error

--- pkg/detectors/zulipchat/zulipchat_test.go
+++ pkg/detectors/zulipchat/zulipchat_test.go
@@ -139,7 +139,7 @@ func TestZulipChat_FromChunk(t *testing.T) {
 					t.Fatalf("wantVerificationError = %v, verification error = %v", tt.wantVerificationErr, got[i].VerificationError())
 				}
 			}
-			ignoreOpts := cmpopts.IgnoreFields(detectors.Result{}, "Raw", "RawV2", *_test.go)
+			ignoreOpts := cmpopts.IgnoreFields(detectors.Result{}, "Raw", "RawV2", "verificationError")
 			if diff := cmp.Diff(got, tt.want, ignoreOpts); diff != "" {
 				t.Errorf("Zulipchat.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
 			}
