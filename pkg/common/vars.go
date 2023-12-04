20bb1ae799343d9f10f72753c4aeed571cf995a4
Cody Rose <rosecodym@gmail.com>
2023-12-04 13:02:18 -0500

    reformat

--- pkg/common/vars.go
+++ pkg/common/vars.go
@@ -7,7 +7,27 @@ import (
 
 var (
 	KB, MB, GB, TB, PB = 1e3, 1e6, 1e9, 1e12, 1e15
-	IgnoredExtensions  = []string{"mp4", "avi", "mpeg", "mpg", "mov", "wmv", "m4p", "swf", "mp2", "flv", "vob", "webm", "hdv", "3gp", "ogg", "mp3", "wav", "flac", "webp"}
+	IgnoredExtensions  = []string{
+		"mp4",
+		"avi",
+		"mpeg",
+		"mpg",
+		"mov",
+		"wmv",
+		"m4p",
+		"swf",
+		"mp2",
+		"flv",
+		"vob",
+		"webm",
+		"hdv",
+		"3gp",
+		"ogg",
+		"mp3",
+		"wav",
+		"flac",
+		"webp",
+	}
 )
 
 func SkipFile(filename string) bool {
