1ca6dae78ea334d9f9b3a04e1030a2676b88a0db
Cody Rose <rosecodym@gmail.com>
2023-12-04 13:12:49 -0500

    add more

--- pkg/common/vars.go
+++ pkg/common/vars.go
@@ -8,6 +8,7 @@ import (
 var (
 	KB, MB, GB, TB, PB = 1e3, 1e6, 1e9, 1e12, 1e15
 	IgnoredExtensions  = []string{
+		// multimedia/containers
 		"mp4",
 		"avi",
 		"mpeg",
@@ -27,6 +28,28 @@ var (
 		"wav",
 		"flac",
 		"webp",
+
+		// images
+		"png",
+		"jpg",
+		"jpeg",
+		"gif",
+		"tiff",
+
+		// binaries
+		// These can theoretically contain secrets, but need decoding for users to make sense of them, and we don't have
+		// any such decoders right now.
+		"class",
+		"dll",
+		"xsb",
+		"jdo",
+		"jar",
+		"ear",
+		"war",
+		"jks",
+		"ser",
+		"idx",
+		"hprof",
 	}
 )
 
