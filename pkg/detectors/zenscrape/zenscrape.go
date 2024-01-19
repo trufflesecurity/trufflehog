868f25f55a747324dafcd4eeaa5e288f8a87e0f0
Joe Leon <joe.leon@trufflesec.com>
2024-01-19 09:51:13 -0500

    updating detector logic for zenscrape

--- pkg/detectors/zenscrape/zenscrape.go
+++ pkg/detectors/zenscrape/zenscrape.go
@@ -2,7 +2,6 @@ package zenscrape
 
 import (
 	"context"
-	"io"
 	"net/http"
 	"regexp"
 	"strings"
@@ -56,20 +55,13 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 			res, err := client.Do(req)
 			if err == nil {
 				defer res.Body.Close()
-				bodyBytes, err := io.ReadAll(res.Body)
-				if err != nil {
-					continue
-				}
-				body := string(bodyBytes)
-
-				if !strings.Contains(body, "Not enough requests.") {
+				if (res.StatusCode >= 200 && res.StatusCode < 300) || res.StatusCode == 429 {
 					s1.Verified = true
 				} else {
 					if detectors.IsKnownFalsePositive(resMatch, detectors.DefaultFalsePositives, true) {
 						continue
 					}
 				}
-
 			}
 		}
 
