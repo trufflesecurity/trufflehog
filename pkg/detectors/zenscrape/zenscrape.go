e1bc5593ff638e7644221d945dd59d25e83b4350
Joe Leon <joe.leon@trufflesec.com>
2024-01-22 12:26:18 -0500

    updating to use status endpoint

--- pkg/detectors/zenscrape/zenscrape.go
+++ pkg/detectors/zenscrape/zenscrape.go
@@ -47,7 +47,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			req, err := http.NewRequestWithContext(ctx, "GET", "https://app.zenscrape.com/api/v1/get?url=http://www.google.com", nil)
+			req, err := http.NewRequestWithContext(ctx, "GET", "https://app.zenscrape.com/api/v1/status", nil)
 			if err != nil {
 				continue
 			}
