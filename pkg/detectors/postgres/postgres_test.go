e0a1b11a8e4c29f0748f540c127f0bab0331ebdc
Cody Rose <rosecodym@gmail.com>
2024-01-23 14:42:31 -0500

    Narrow Postgres detector to only look for URIs (#2314)

--- pkg/detectors/postgres/postgres_test.go
+++ pkg/detectors/postgres/postgres_test.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/lib/pq"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
@@ -24,20 +25,35 @@ var postgresDockerHash string
 
 const (
 	postgresUser = "postgres"
-	postgresPass = "23201dabb56ca236f3dc6736c0f9afad"
+	postgresPass = "23201da=b56ca236f3dc6736c0f9afad"
 	postgresHost = "localhost"
-	postgresPort = "5433"
+	postgresPort = "5434" // Do not use 5433, as local dev environments can use it for other things
 
-	inactiveUser = "inactive"
 	inactivePass = "inactive"
-	inactivePort = "61000"
 	inactiveHost = "192.0.2.0"
 )
 
 func TestPostgres_FromChunk(t *testing.T) {
-	startPostgres()
+	if err := startPostgres(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			t.Fatalf("could not start local postgres: %v w/stderr:\n%s", err, string(exitErr.Stderr))
+		} else {
+			t.Fatalf("could not start local postgres: %v", err)
+		}
+	}
 	defer stopPostgres()
 
+	// The detector is written to connect to the database 'postgres' if no explicit database is found in the candidate
+	// secret (because pq uses 'postgres' as a default if no database is specified). If the target cluster doesn't
+	// actually have a database with this name, but our credentials are good, then Postgres will give us a "missing
+	// database" error message instead of an authentication failure.
+	//
+	// Unfortunately, directly validating this in the automated tests is awkward because the docker image's POSTGRES_DB
+	// environment variable doesn't appear to work: The database created is always named 'postgres', no matter what
+	// POSTGRES_DB is set to. This means that we can't replicate a cluster that has no database named 'postgres', so we
+	// can't directly test what happens if we see one. To work around this, all the automated tests try to connect to
+	// the nonexistent database 'postgres2'. In this way, we test the logic of attempting to connect to a non-existent
+	// database, even though the test cases are the inverse of what we'd see in the wild.
 	type args struct {
 		ctx    context.Context
 		data   []byte
@@ -62,177 +78,150 @@ func TestPostgres_FromChunk(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "found with seperated credentials, verified",
-			s:    Scanner{},
+			name: "found connection URI with ssl mode unset, verified",
+			s:    Scanner{detectLoopback: true},
 			args: args{
-				ctx: context.Background(),
-				data: []byte(fmt.Sprintf(`
-					POSTGRES_USER=%s
-					POSTGRES_PASSWORD=%s
-					POSTGRES_ADDRESS=%s
-					POSTGRES_PORT=%s
-					`, postgresUser, postgresPass, postgresHost, postgresPort)),
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2`, postgresUser, postgresPass, postgresHost, postgresPort)),
 				verify: true,
 			},
 			want: []detectors.Result{
 				{
 					DetectorType: detectorspb.DetectorType_Postgres,
 					Verified:     true,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					ExtraData:    map[string]string{"sslmode": "<unset>"},
 				},
 			},
 			wantErr: false,
 		},
 		{
-			name: "found with single line credentials, verified",
-			s:    Scanner{},
+			name: "found connection URI with ssl mode 'prefer', verified",
+			s:    Scanner{detectLoopback: true},
 			args: args{
 				ctx:    context.Background(),
-				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres`, postgresUser, postgresPass, postgresHost, postgresPort)),
+				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2?sslmode=prefer`, postgresUser, postgresPass, postgresHost, postgresPort)),
 				verify: true,
 			},
 			want: []detectors.Result{
 				{
 					DetectorType: detectorspb.DetectorType_Postgres,
 					Verified:     true,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					ExtraData:    map[string]string{"sslmode": "prefer"},
 				},
 			},
 			wantErr: false,
 		},
 		{
-			name: "found with json credentials, verified",
-			s:    Scanner{},
+			name: "found connection URI with ssl mode 'allow', verified",
+			s:    Scanner{detectLoopback: true},
 			args: args{
-				ctx: context.Background(),
-				data: []byte(fmt.Sprintf(
-					`DB_CONFIG={"user": "%s", "password": "%s", "host": "%s", "port": "%s", "database": "postgres"}`, postgresUser, postgresPass, postgresHost, postgresPort)),
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2?sslmode=allow`, postgresUser, postgresPass, postgresHost, postgresPort)),
 				verify: true,
 			},
 			want: []detectors.Result{
 				{
 					DetectorType: detectorspb.DetectorType_Postgres,
 					Verified:     true,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					ExtraData:    map[string]string{"sslmode": "allow"},
 				},
 			},
 			wantErr: false,
 		},
 		{
-			name: "found with seperated credentials, unverified",
-			s:    Scanner{},
+			name: "found connection URI with requiressl=0, verified",
+			s:    Scanner{detectLoopback: true},
 			args: args{
-				ctx: context.Background(),
-				data: []byte(fmt.Sprintf(`
-					POSTGRES_USER=%s
-					POSTGRES_PASSWORD=%s
-					POSTGRES_ADDRESS=%s
-					POSTGRES_PORT=%s
-					`, postgresUser, inactivePass, postgresHost, postgresPort)),
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2?requiressl=0`, postgresUser, postgresPass, postgresHost, postgresPort)),
+				verify: true,
+			},
+			want: []detectors.Result{
+				{
+					DetectorType: detectorspb.DetectorType_Postgres,
+					Verified:     true,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					ExtraData:    map[string]string{"sslmode": "prefer"},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "found connection URI without database, verified",
+			s:    Scanner{detectLoopback: true},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/`, postgresUser, postgresPass, postgresHost, postgresPort)),
+				verify: true,
+			},
+			want: []detectors.Result{
+				{
+					DetectorType: detectorspb.DetectorType_Postgres,
+					Verified:     true,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					ExtraData:    map[string]string{"sslmode": "<unset>"},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "found connection URI, unverified",
+			s:    Scanner{detectLoopback: true},
+			args: args{
+				ctx:    context.Background(),
+				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2`, postgresUser, inactivePass, postgresHost, postgresPort)),
 				verify: true,
 			},
 			want: []detectors.Result{
 				{
 					DetectorType: detectorspb.DetectorType_Postgres,
 					Verified:     false,
+					Raw:          []byte("postgresql://postgres:inactive@localhost:5434"),
+					RawV2:        []byte("postgresql://postgres:inactive@localhost:5434"),
+					ExtraData:    map[string]string{"sslmode": "<unset>"},
 				},
 			},
 			wantErr: false,
 		},
 		{
-			name: "found with seperated credentials - no port, unverified",
-			s:    Scanner{},
-			args: args{
-				ctx: context.Background(),
-				data: []byte(fmt.Sprintf(`
-					POSTGRES_USER=%s
-					POSTGRES_PASSWORD=%s
-					POSTGRES_ADDRESS=%s
-					`, postgresUser, inactivePass, postgresHost)),
-				verify: true,
-			},
-			want: []detectors.Result{
-				{
-					DetectorType: detectorspb.DetectorType_Postgres,
-					Verified:     false,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "found with single line credentials, unverified",
+			name: "ignored localhost",
 			s:    Scanner{},
 			args: args{
 				ctx:    context.Background(),
-				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres`, postgresUser, inactivePass, postgresHost, postgresPort)),
+				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2`, postgresUser, postgresPass, "localhost", postgresPort)),
 				verify: true,
 			},
-			want: []detectors.Result{
-				{
-					DetectorType: detectorspb.DetectorType_Postgres,
-					Verified:     false,
-				},
-			},
+			want:    nil,
 			wantErr: false,
 		},
 		{
-			name: "found with json credentials, unverified - inactive password",
-			s:    Scanner{},
-			args: args{
-				ctx: context.Background(),
-				data: []byte(fmt.Sprintf(
-					`DB_CONFIG={"user": "%s", "password": "%s", "host": "%s", "port": "%s", "database": "postgres"}`, postgresUser, inactivePass, postgresHost, postgresPort)),
-				verify: true,
-			},
-			want: []detectors.Result{
-				{
-					DetectorType: detectorspb.DetectorType_Postgres,
-					Verified:     false,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "found with json credentials, unverified - inactive user",
-			s:    Scanner{},
-			args: args{
-				ctx: context.Background(),
-				data: []byte(fmt.Sprintf(
-					`DB_CONFIG={"user": "%s", "password": "%s", "host": "%s", "port": "%s", "database": "postgres"}`, inactiveUser, postgresPass, postgresHost, postgresPort)),
-				verify: true,
-			},
-			want: []detectors.Result{
-				{
-					DetectorType: detectorspb.DetectorType_Postgres,
-					Verified:     false,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "found, unverified due to error - inactive port",
+			name: "ignored 127.0.0.1",
 			s:    Scanner{},
 			args: args{
 				ctx:    context.Background(),
-				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres`, postgresUser, postgresPass, postgresHost, inactivePort)),
+				data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2`, postgresUser, postgresPass, "127.0.0.1", postgresPort)),
 				verify: true,
 			},
-			want: func() []detectors.Result {
-				r := detectors.Result{
-					DetectorType: detectorspb.DetectorType_Postgres,
-					Verified:     false,
-				}
-				return []detectors.Result{r}
-			}(),
+			want:    nil,
 			wantErr: false,
 		},
-		// This test seems take a long time to run (70s+) even with the timeout set to 1s. It's not clear why.
 		{
-			name: "found, unverified due to error - inactive host",
+			name: "found connection URI, unverified due to error - inactive host",
 			s:    Scanner{},
 			args: func() args {
 				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 				defer cancel()
 				return args{
 					ctx:    ctx,
-					data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres`, postgresUser, postgresPass, inactiveHost, postgresPort)),
+					data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2`, postgresUser, postgresPass, inactiveHost, postgresPort)),
 					verify: true,
 				}
 			}(),
@@ -240,8 +229,86 @@ func TestPostgres_FromChunk(t *testing.T) {
 				r := detectors.Result{
 					DetectorType: detectorspb.DetectorType_Postgres,
 					Verified:     false,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@192.0.2.0:5434"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@192.0.2.0:5434"),
+					ExtraData:    map[string]string{"sslmode": "<unset>"},
 				}
 				r.SetVerificationError(errors.New("i/o timeout"))
+				return []detectors.Result{r}
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "found connection URI, unverified due to error - wrong port",
+			s:    Scanner{detectLoopback: true},
+			args: func() args {
+				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+				defer cancel()
+				return args{
+					ctx:    ctx,
+					data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s/postgres2`, postgresUser, postgresPass, postgresHost)),
+					verify: true,
+				}
+			}(),
+			want: func() []detectors.Result {
+				r := detectors.Result{
+					DetectorType: detectorspb.DetectorType_Postgres,
+					Verified:     false,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5432"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5432"),
+					ExtraData:    map[string]string{"sslmode": "<unset>"},
+				}
+				r.SetVerificationError(errors.New("connection refused"))
+				return []detectors.Result{r}
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "found connection URI, unverified due to error - ssl not supported (using sslmode)",
+			s:    Scanner{detectLoopback: true},
+			args: func() args {
+				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+				defer cancel()
+				return args{
+					ctx:    ctx,
+					data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2?sslmode=require`, postgresUser, postgresPass, postgresHost, postgresPort)),
+					verify: true,
+				}
+			}(),
+			want: func() []detectors.Result {
+				r := detectors.Result{
+					DetectorType: detectorspb.DetectorType_Postgres,
+					Verified:     false,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					ExtraData:    map[string]string{"sslmode": "require"},
+				}
+				r.SetVerificationError(pq.ErrSSLNotSupported)
+				return []detectors.Result{r}
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "found connection URI, unverified due to error - ssl not supported (using requiressl)",
+			s:    Scanner{detectLoopback: true},
+			args: func() args {
+				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+				defer cancel()
+				return args{
+					ctx:    ctx,
+					data:   []byte(fmt.Sprintf(`postgresql://%s:%s@%s:%s/postgres2?requiressl=1`, postgresUser, postgresPass, postgresHost, postgresPort)),
+					verify: true,
+				}
+			}(),
+			want: func() []detectors.Result {
+				r := detectors.Result{
+					DetectorType: detectorspb.DetectorType_Postgres,
+					Verified:     false,
+					Raw:          []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					RawV2:        []byte("postgresql://postgres:23201da=b56ca236f3dc6736c0f9afad@localhost:5434"),
+					ExtraData:    map[string]string{"sslmode": "require"},
+				}
+				r.SetVerificationError(pq.ErrSSLNotSupported)
 				return []detectors.Result{r}
 			}(),
 			wantErr: false,
@@ -249,8 +316,7 @@ func TestPostgres_FromChunk(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := Scanner{}
-			got, err := s.FromData(tt.args.ctx, tt.args.verify, tt.args.data)
+			got, err := tt.s.FromData(tt.args.ctx, tt.args.verify, tt.args.data)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("postgres.FromData() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -271,7 +337,7 @@ func TestPostgres_FromChunk(t *testing.T) {
 					t.Fatalf("wantVerificationError = %v, verification error = %v", tt.want[i].VerificationError(), got[i].VerificationError())
 				}
 			}
-			ignoreOpts := cmpopts.IgnoreFields(detectors.Result{}, "Raw", "RawV2", "verificationError")
+			ignoreOpts := cmpopts.IgnoreFields(detectors.Result{}, "verificationError")
 			if diff := cmp.Diff(got, tt.want, ignoreOpts); diff != "" {
 				t.Errorf("Postgres.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
 			}
