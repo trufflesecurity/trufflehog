ea94996e429ce96a9ff3c601c9e044fc294600ca
Dustin Decker <dustin@trufflesec.com>
2024-01-25 22:37:06 -0800

    Fix test

--- pkg/detectors/yelp/yelp_test.go
+++ pkg/detectors/yelp/yelp_test.go
@@ -134,11 +134,11 @@ func TestYelp_FromChunk(t *testing.T) {
 				}
 				got[i].Raw = nil
 
-				if (got[i].VerificationError != nil) != tt.wantVerificationErr {
-					t.Fatalf("wantVerificationError = %v, verification error = %v", tt.wantVerificationErr, got[i].VerificationError)
+				if (got[i].VerificationError() != nil) != tt.wantVerificationErr {
+					t.Fatalf("wantVerificationError = %v, verification error = %v", tt.wantVerificationErr, got[i].VerificationError())
 				}
 			}
-			ignoreOpts := cmpopts.IgnoreFields(detectors.Result{}, "Raw", "VerificationError")
+			ignoreOpts := cmpopts.IgnoreFields(detectors.Result{}, "Raw", "verificationError")
 			if diff := cmp.Diff(got, tt.want, ignoreOpts); diff != "" {
 				t.Errorf("SlackWebhook.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
 			}
