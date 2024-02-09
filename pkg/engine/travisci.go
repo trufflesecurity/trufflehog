0a4e23176e6392c10cb86cc08673cf18b27697b8
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-02-08 17:42:09 -0800

    add travis

--- pkg/engine/travisci.go
+++ pkg/engine/travisci.go
@@ -1,13 +1,12 @@
 package engine
 
 import (
-	"runtime"
-
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/sourcespb"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/sources"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/sources/travisci"
 )
 
@@ -29,10 +28,28 @@ func (e *Engine) ScanTravisCI(ctx context.Context, token string) error {
 	sourceName := "trufflehog - Travis CI"
 	sourceID, jobID, _ := e.sourceManager.GetIDs(ctx, sourceName, travisci.SourceType)
 
-	travisSource := &travisci.Source{}
-	if err := travisSource.Init(ctx, sourceName, jobID, sourceID, true, &conn, runtime.NumCPU()); err != nil {
+	src := &travisci.Source{}
+	err = src.Init(
+		ctx,
+		sources.NewConfig(
+			&conn,
+			sources.WithName(sourceName),
+			sources.WithSourceID(sourceID),
+			sources.WithJobID(jobID),
+			sources.WithVerify(e.verify),
+			sources.WithConcurrency(int(e.concurrency)),
+		),
+	)
+	if err != nil {
 		return err
 	}
-	_, err = e.sourceManager.Run(ctx, sourceName, travisSource)
+
+	_, err = e.sourceManager.Run(ctx, sourceName, src)
 	return err
+
+	// if err := travisSource.Init(ctx, sourceName, jobID, sourceID, true, &conn, runtime.NumCPU()); err != nil {
+	// 	return err
+	// }
+	// _, err = e.sourceManager.Run(ctx, sourceName, travisSource)
+	// return err
 }
--- pkg/sources/travisci/travisci.go
+++ pkg/sources/travisci/travisci.go
@@ -56,11 +56,11 @@ func (s *Source) JobID() sources.JobID {
 }
 
 // Init returns an initialized TravisCI source.
-func (s *Source) Init(ctx context.Context, name string, jobId sources.JobID, sourceId sources.SourceID, verify bool, connection *anypb.Any, concurrency int) error {
-	s.name = name
-	s.sourceId = sourceId
-	s.jobId = jobId
-	s.verify = verify
+func (s *Source) Init(ctx context.Context, cfg *sources.Config) error {
+	s.name = cfg.Name
+	s.sourceId = cfg.SourceID
+	s.jobId = cfg.JobID
+	s.verify = cfg.Verify
 	s.jobPool = &errgroup.Group{}
 	s.jobPool.SetLimit(concurrency)
 
