e6e8d41922ec59c950b80cc09b9c2f58a1525e97
Mark Steward <marksteward@gmail.com>
2024-02-21 14:16:44 +0000

    Tell git to ignore directory ownership in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 # Parse the last argument into an array of extra_args.
 mapfile -t extra_args < <(bash -c "for arg in ${*: -1}; do echo \$arg; done")
 
+# Directories might be owned by a user other than root
+git config --global --add safe.directory '*'
+
 /usr/bin/trufflehog "${@: 1: $#-1}" "${extra_args[@]}" 
