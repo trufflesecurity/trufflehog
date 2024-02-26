b477ee397a21f3b04fdd3610c1ba3432a2133dae
Richard Gomez <rmgomez368@gmail.com>
2024-02-26 09:26:50 -0500

    wip: handle unicode notation Experimenting with this.. might remove

--- pkg/decoders/escaped_unicode.go
+++ pkg/decoders/escaped_unicode.go
@@ -15,28 +15,86 @@ var _ Decoder = (*EscapedUnicode)(nil)
 
 // It might be advantageous to limit these to a subset of acceptable characters, similar to base64.
 // https://dencode.com/en/string/unicode-escape
-//
-// Another format worth considering is `U+1234`
-var escapePat = regexp.MustCompile(`(?i:\\{1,2}u)([a-fA-F0-9]{4})`)
+var (
+	// Standard Unicode notation.
+	//https://unicode.org/standard/principles.html
+	codePointPat = regexp.MustCompile(`\bU\+([a-fA-F0-9]{4}).?`)
+
+	// Common escape sequence used in programming languages.
+	escapePat = regexp.MustCompile(`(?i:\\{1,2}u)([a-fA-F0-9]{4})`)
+)
 
 func (d *EscapedUnicode) FromChunk(chunk *sources.Chunk) *DecodableChunk {
-	if chunk == nil || len(chunk.Data) == 0 || !escapePat.Match(chunk.Data) {
+	if chunk == nil || len(chunk.Data) == 0 {
 		return nil
 	}
 
-	chunk.Data = decodeUnicode(chunk.Data)
-	decodableChunk := &DecodableChunk{
-		DecoderType: detectorspb.DecoderType_ESCAPED_UNICODE,
-		Chunk:       chunk,
+	matched := false
+	if codePointPat.Match(chunk.Data) {
+		matched = true
+		chunk.Data = decodeCodePoint(chunk.Data)
+	}
+	if escapePat.Match(chunk.Data) {
+		matched = true
+		chunk.Data = decodeEscaped(chunk.Data)
 	}
 
-	return decodableChunk
+	if matched {
+		decodableChunk := &DecodableChunk{
+			DecoderType: detectorspb.DecoderType_ESCAPED_UNICODE,
+			Chunk:       chunk,
+		}
+		return decodableChunk
+	} else {
+		return nil
+	}
 }
 
 // Unicode characters are encoded as 1 to 4 bytes per rune.
 const maxBytesPerRune = 4
+const spaceChar = byte(' ')
 
-func decodeUnicode(input []byte) []byte {
+func decodeCodePoint(input []byte) []byte {
+	// Find all Unicode escape sequences in the input byte slice
+	indices := codePointPat.FindAllSubmatchIndex(input, -1)
+
+	// Iterate over found indices in reverse order to avoid modifying the slice length
+	utf8Bytes := make([]byte, maxBytesPerRune)
+	for i := len(indices) - 1; i >= 0; i-- {
+		matches := indices[i]
+
+		startIndex := matches[0]
+		endIndex := matches[1]
+		hexStartIndex := matches[2]
+		hexEndIndex := matches[3]
+
+		// If the input is like `U+1234 U+5678` we should replace `U+1234 `.
+		// Otherwise, we should only replace `U+1234`.
+		if endIndex != hexEndIndex && input[endIndex-1] != spaceChar {
+			endIndex = endIndex - 1
+		}
+
+		// Extract the hexadecimal value from the escape sequence
+		hexValue := string(input[hexStartIndex:hexEndIndex])
+
+		// Parse the hexadecimal value to an integer
+		unicodeInt, err := strconv.ParseInt(hexValue, 16, 32)
+		if err != nil {
+			// If there's an error, continue to the next escape sequence
+			continue
+		}
+
+		// Convert the Unicode code point to a UTF-8 representation
+		utf8Len := utf8.EncodeRune(utf8Bytes, rune(unicodeInt))
+
+		// Replace the escape sequence with the UTF-8 representation
+		input = append(input[:startIndex], append(utf8Bytes[:utf8Len], input[endIndex:]...)...)
+	}
+
+	return input
+}
+
+func decodeEscaped(input []byte) []byte {
 	// Find all Unicode escape sequences in the input byte slice
 	indices := escapePat.FindAllSubmatchIndex(input, -1)
 
--- pkg/decoders/escaped_unicode_test.go
+++ pkg/decoders/escaped_unicode_test.go
@@ -15,8 +15,19 @@ func TestUnicodeEscape_FromChunk(t *testing.T) {
 		want    *sources.Chunk
 		wantErr bool
 	}{
+		// U+1234
 		{
-			name: "all escaped",
+			name: "[notation] all escaped",
+			chunk: &sources.Chunk{
+				Data: []byte("U+0074 U+006f U+006b U+0065 U+006e U+003a U+0020 U+0022 U+0067 U+0068 U+0070 U+005f U+0049 U+0077 U+0064 U+004d U+0078 U+0039 U+0057 U+0046 U+0057 U+0052 U+0052 U+0066 U+004d U+0068 U+0054 U+0059 U+0069 U+0061 U+0056 U+006a U+005a U+0037 U+0038 U+004a U+0066 U+0075 U+0061 U+006d U+0076 U+006e U+0030 U+0059 U+0057 U+0052 U+004d U+0030 U+0022"),
+			},
+			want: &sources.Chunk{
+				Data: []byte("token: \"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\""),
+			},
+		},
+		// \u1234
+		{
+			name: "[slash] all escaped",
 			chunk: &sources.Chunk{
 				Data: []byte("\\u0074\\u006f\\u006b\\u0065\\u006e\\u003a\\u0020\\u0022\\u0067\\u0068\\u0070\\u005f\\u0049\\u0077\\u0064\\u004d\\u0078\\u0039\\u0057\\u0046\\u0057\\u0052\\u0052\\u0066\\u004d\\u0068\\u0054\\u0059\\u0069\\u0061\\u0056\\u006a\\u005a\\u0037\\u0038\\u004a\\u0066\\u0075\\u0061\\u006d\\u0076\\u006e\\u0030\\u0059\\u0057\\u0052\\u004d\\u0030\\u0022"),
 			},
@@ -25,7 +36,7 @@ func TestUnicodeEscape_FromChunk(t *testing.T) {
 			},
 		},
 		{
-			name: "mixed content",
+			name: "[slash] mixed content",
 			chunk: &sources.Chunk{
 				Data: []byte("npm config set @trufflesec:registry=https://npm.pkg.github.com\nnpm config set //npm.pkg.github.com:_authToken=$'\\u0067hp_9ovSHEBCq0drG42yjoam76iNybtqLN25CgSf'"),
 			},
@@ -34,7 +45,7 @@ func TestUnicodeEscape_FromChunk(t *testing.T) {
 			},
 		},
 		{
-			name: "multiple slashes",
+			name: "[slash] multiple slashes",
 			chunk: &sources.Chunk{
 				Data: []byte(`SameValue("hello","\\u0068el\\u006co");          // true`),
 			},
@@ -42,6 +53,8 @@ func TestUnicodeEscape_FromChunk(t *testing.T) {
 				Data: []byte(`SameValue("hello","hello");          // true`),
 			},
 		},
+
+		// nothing
 		{
 			name: "no escaped",
 			chunk: &sources.Chunk{
