b69d2acb58cc30b1391c099ba409f9e24b150fb1
Fatih Aktas <akfatih3@gmail.com>
2024-03-14 15:33:21 -0400

    MaxMind detector uses the right endpoint
    The endpoint that the current detector uses fails in validating the license key as some license keys do not have permissions to the geoip API. This commit is to make the detector use the right endpoint https://dev.maxmind.com/license-key-validation-api

--- pkg/detectors/maxmindlicense/v2/maxmindlicense_v2.go
+++ pkg/detectors/maxmindlicense/v2/maxmindlicense_v2.go
@@ -20,7 +20,6 @@ var _ detectors.Detector = (*Scanner)(nil)
 var (
 	client = common.SaneHttpClient()
 
-	idPat  = regexp.MustCompile(detectors.PrefixRegex([]string{"maxmind", "geoip"}) + `\b([0-9]{2,7})\b`)
 	keyPat = regexp.MustCompile(`\b([0-9A-Za-z]{6}_[0-9A-Za-z]{29}_mmk)\b`)
 )
 
@@ -37,49 +36,44 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 	dataStr := string(data)
 
 	keyMatches := keyPat.FindAllStringSubmatch(dataStr, -1)
-	idMatches := idPat.FindAllStringSubmatch(dataStr, -1)
 
 	for _, keyMatch := range keyMatches {
 		keyRes := strings.TrimSpace(keyMatch[1])
 
-		for _, idMatch := range idMatches {
-			if len(idMatch) != 2 {
+		r := detectors.Result{
+			DetectorType: detectorspb.DetectorType_MaxMindLicense,
+			Redacted:     keyRes,
+			Raw:          []byte(keyRes),
+			RawV2:        []byte(keyRes),
+			ExtraData: map[string]string{
+				"rotation_guide": "https://howtorotate.com/docs/tutorials/maxmind/",
+				"version":        fmt.Sprintf("%d", s.Version()),
+			},
+		}
+
+		if verify {
+			req, err := http.NewRequestWithContext(ctx, "POST", "https://secret-scanning.maxmind.com/secrets/validate-license-key", nil)
+			if err != nil {
 				continue
 			}
-			idRes := strings.TrimSpace(idMatch[1])
 
-			s := detectors.Result{
-				DetectorType: detectorspb.DetectorType_MaxMindLicense,
-				Redacted:     idRes,
-				Raw:          []byte(keyRes),
-				RawV2:        []byte(keyRes + idRes),
-				ExtraData: map[string]string{
-					"rotation_guide": "https://howtorotate.com/docs/tutorials/maxmind/",
-					"version":        fmt.Sprintf("%d", s.Version()),
-				},
-			}
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			req.Form.Add("license_key", keyRes)
 
-			if verify {
-				req, err := http.NewRequestWithContext(ctx, "GET", "https://geoip.maxmind.com/geoip/v2.1/country/8.8.8.8", nil)
-				if err != nil {
-					continue
-				}
-				req.SetBasicAuth(idRes, keyRes)
-				res, err := client.Do(req)
-				if err == nil {
-					defer res.Body.Close()
-					if res.StatusCode >= 200 && res.StatusCode < 300 {
-						s.Verified = true
-					} else {
-						if detectors.IsKnownFalsePositive(keyRes, detectors.DefaultFalsePositives, true) {
-							continue
-						}
+			res, err := client.Do(req)
+			if err == nil {
+				defer res.Body.Close()
+				if res.StatusCode >= 200 && res.StatusCode < 300 {
+					r.Verified = true
+				} else {
+					if detectors.IsKnownFalsePositive(keyRes, detectors.DefaultFalsePositives, true) {
+						continue
 					}
 				}
 			}
-
-			results = append(results, s)
 		}
+
+		results = append(results, r)
 	}
 
 	return results, nil
