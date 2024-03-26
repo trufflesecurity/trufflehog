9da396e23752b8748d182dd792cf1f5f5001bb7a
fml09 <91314791+fml09@users.noreply.github.com>
2024-03-27 03:32:14 +0900

    Fix incorrect regular expression with missing closing bracket (#2616)

--- pkg/detectors/atera/atera.go
+++ pkg/detectors/atera/atera.go
@@ -20,7 +20,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"atera"}) + `\b([[0-9a-z]{32})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"atera"}) + `\b([0-9a-z]{32})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
