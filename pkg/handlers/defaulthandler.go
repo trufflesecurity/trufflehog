8c8b3028c8bc90b276566c1f2de1a04cd448689f
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-04-23 12:25:33 -0700

    fix

--- pkg/handlers/defaulthandler.go
+++ pkg/handlers/defaulthandler.go
@@ -156,7 +156,7 @@ func (h *defaultHandler) openArchive(ctx logContext.Context, depth int, reader i
 		}
 		defer compReader.Close()
 
-		rdr, err := readers.NewBufferedFileReader(ctx, reader)
+		rdr, err := readers.NewBufferedFileReader(ctx, compReader)
 		if err != nil {
 			return fmt.Errorf("error creating random access reader: %w", err)
 		}
