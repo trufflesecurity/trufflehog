65864387aee3919d9ae828410a3535ff1f446ee1
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-04-23 11:31:27 -0700

    defer the return

--- pkg/writers/buffered_file_writer/bufferedfilewriter.go
+++ pkg/writers/buffered_file_writer/bufferedfilewriter.go
@@ -172,9 +172,11 @@ func (w *BufferedFileWriter) CloseForWriting() error {
 		return nil
 	}
 
-	// Return the buffer to the pool since the contents have been written to the file and
-	// the writer is transitioning to read-only mode.
-	w.bufPool.Put(w.buf)
+	defer func() {
+		// Return the buffer to the pool since the contents have been written to the file and
+		// the writer is transitioning to read-only mode.
+		w.bufPool.Put(w.buf)
+	}()
 
 	if w.buf.Len() > 0 {
 		_, err := w.buf.WriteTo(w.file)
