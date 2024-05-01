7b5db838cf9b64acc12cc5bd581c82548e288281
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-05-01 09:24:09 -0700

    update imports

--- pkg/detectors/googleoauth2/googleoauth2_access_token.go
+++ pkg/detectors/googleoauth2/googleoauth2_access_token.go
@@ -6,9 +6,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"regexp"
 	"strconv"
 	"time"
+
+	regexp "github.com/wasilibs/go-re2"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
