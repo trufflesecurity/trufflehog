10316dab6f519be3e720668c751b4b9fb829bd15
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-06-05 09:41:39 -0700

    update detector

--- pkg/detectors/microsoftteamswebhook/microsoftteamswebhook.go
+++ pkg/detectors/microsoftteamswebhook/microsoftteamswebhook.go
@@ -17,7 +17,7 @@ import (
 
 type Scanner struct {
 	client *http.Client
-	detectors.MultiPartCredentialProvider
+	detectors.DefaultMultiPartCredentialProvider
 }
 
 // Ensure the Scanner satisfies the interface at compile time.
