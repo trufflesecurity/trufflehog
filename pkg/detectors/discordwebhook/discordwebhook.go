fcfd9c1d82b44ddd939d3ca911b1875c9727b0cb
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-06-10 10:10:44 -0700

    use stdlib var

--- pkg/detectors/discordwebhook/discordwebhook.go
+++ pkg/detectors/discordwebhook/discordwebhook.go
@@ -45,7 +45,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			req, err := http.NewRequestWithContext(ctx, "GET", resMatch, nil)
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, resMatch, nil)
 			if err != nil {
 				continue
 			}
