35ba05f8723b88b5240c802d05682d58b7a4e9b5
Abdul Basit <basit.mussani@gmail.com>
2024-06-13 16:28:33 +0500

    reverted the file to avoid conflicts

--- pkg/detectors/twitter/twitter.go
+++ pkg/detectors/twitter/twitter.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
-
-	regexp "github.com/wasilibs/go-re2"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
@@ -14,7 +13,6 @@ import (
 )
 
 type Scanner struct {
-	client *http.Client
 	detectors.DefaultMultiPartCredentialProvider
 }
 
@@ -22,10 +20,10 @@ type Scanner struct {
 var _ detectors.Detector = (*Scanner)(nil)
 
 var (
-	defaultClient = common.SaneHttpClient()
+	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	bearerTokenPat = regexp.MustCompile(`\b([a-zA-Z0-9]{20,59}%([a-zA-Z0-9]{3,26}%){0,4}[a-zA-Z0-9]{52})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"twitter"}) + `\b([A-Z]{22}%[a-zA-Z-0-9]{23}%[a-zA-Z-0-9]{6}%[a-zA-Z-0-9]{3}%[a-zA-Z-0-9]{9}%[a-zA-Z-0-9]{52})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
@@ -34,17 +32,17 @@ func (s Scanner) Keywords() []string {
 	return []string{"twitter"}
 }
 
-// FromData will find and optionally verify Twitter bearer token in a given set of bytes.
+// FromData will find and optionally verify Twitter secrets in a given set of bytes.
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
 
-	tokenMatches := make(map[string]struct{})
-	for _, match := range bearerTokenPat.FindAllStringSubmatch(dataStr, -1) {
-		tokenMatches[match[1]] = struct{}{}
-	}
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
 
-	for match := range tokenMatches {
-		resMatch := strings.TrimSpace(match)
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[1])
 
 		s1 := detectors.Result{
 			DetectorType: detectorspb.DetectorType_Twitter,
@@ -52,45 +50,26 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			client := s.client
-			if client == nil {
-				client = defaultClient
-			}
-			isVerified, err := verifyBearerToken(ctx, client, resMatch)
-			s1.Verified = isVerified
+			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.twitter.com/2/tweets/20", nil)
 			if err != nil {
-				s1.SetVerificationError(err, resMatch)
+				continue
+			}
+			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", resMatch))
+			res, err := client.Do(req)
+			if err == nil {
+				defer res.Body.Close()
+				if res.StatusCode >= 200 && res.StatusCode < 300 {
+					s1.Verified = true
+				}
 			}
 		}
 
 		results = append(results, s1)
 	}
+
 	return results, nil
 }
 
 func (s Scanner) Type() detectorspb.DetectorType {
 	return detectorspb.DetectorType_Twitter
-}
-
-func verifyBearerToken(ctx context.Context, client *http.Client, token string) (bool, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://api.twitter.com/2/tweets/20", nil)
-	if err != nil {
-		return false, err
-	}
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
-	res, err := client.Do(req)
-	if err == nil {
-		defer res.Body.Close()
-		switch res.StatusCode {
-		case http.StatusOK, http.StatusForbidden:
-			// 403 indicates lack of permission, but valid token (could be due to twitter free tier)
-			return true, nil
-		case http.StatusUnauthorized:
-			return false, nil
-		default:
-			return false, fmt.Errorf("unexpected HTTP response status %d", res.StatusCode)
-		}
-	}
-
-	return false, err
 }
