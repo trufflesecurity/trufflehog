4addd81e29c1daebe0046bc6a7fcd4bc079f70b1
Richard Gomez <32133502+rgmz@users.noreply.github.com>
2024-06-13 11:22:25 -0400

    test: fix compile errors (#2964)

--- pkg/detectors/groq/groq_test.go
+++ pkg/detectors/groq/groq_test.go
@@ -35,9 +35,8 @@ func TestGroq_Pattern(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			chunkSpecificDetectors := make(map[ahocorasick.DetectorKey]detectors.Detector, 2)
-			ahoCorasickCore.PopulateMatchingDetectors(test.input, chunkSpecificDetectors)
-			if len(chunkSpecificDetectors) == 0 {
+			detectorMatches := ahoCorasickCore.FindDetectorMatches([]byte(test.input))
+			if len(detectorMatches) == 0 {
 				t.Errorf("keywords '%v' not matched by: %s", d.Keywords(), test.input)
 				return
 			}
