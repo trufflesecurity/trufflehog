0e90e5c631dfcfc7990eaeebb72a30ffe3ced4f2
Cody Rose <rosecodym@gmail.com>
2024-07-03 17:38:25 -0400

    fix deps???

--- pkg/sources/gitlab/gitlab.go
+++ pkg/sources/gitlab/gitlab.go
@@ -21,6 +21,7 @@ import (
 
 	gogit "github.com/go-git/go-git/v5"
 	"github.com/gobwas/glob"
+	"github.com/xanzy/go-gitlab"
 	"golang.org/x/exp/slices"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
