4bb377022de8bb0221032903d930f05ec8e6b4b0
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-08-02 14:39:28 -0700

    return errors during private key verification

--- pkg/detectors/privatekey/privatekey.go
+++ pkg/detectors/privatekey/privatekey.go
@@ -3,13 +3,13 @@ package privatekey
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/errors"
 	regexp "github.com/wasilibs/go-re2"
 	"golang.org/x/crypto/ssh"
 
@@ -48,6 +48,7 @@ func (s Scanner) MaxSecretSize() int64 { return maxPrivateKeySize }
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
 
+	var verifcationErrs error
 	matches := keyPat.FindAllString(dataStr, -1)
 	for _, match := range matches {
 		token := normalize(match)
@@ -89,6 +90,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 				wg                 sync.WaitGroup
 				extraData          = newExtraData()
 				verificationErrors = newVerificationErrors()
+				errChan            = make(chan error, 3)
 			)
 
 			// Look up certificate information.
@@ -132,6 +134,12 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 			}()
 
 			wg.Wait()
+			close(errChan)
+
+			for err := range errChan {
+				verifcationErrs = errors.Join(verifcationErrs, err)
+			}
+
 			if len(extraData.data) > 0 {
 				s1.Verified = true
 				for k, v := range extraData.data {
@@ -148,7 +156,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		results = append(results, s1)
 	}
 
-	return results, nil
+	return results, verifcationErrs
 }
 
 func (s Scanner) IsFalsePositive(_ detectors.Result) (bool, string) {
