bf3a0224895051f9dd217275161d6bb9c9fa3b6a
Cody Rose <rosecodym@gmail.com>
2024-08-19 17:12:10 -0400

    write results_cleaner test

--- pkg/detectors/results_cleaner_test.go
+++ pkg/detectors/results_cleaner_test.go
@@ -2,8 +2,65 @@ package detectors
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDefaultCleanResults(t *testing.T) {
-	t.Fail()
+	testCases := []struct {
+		name                string
+		resultsToClean      []Result
+		wantVerifiedCount   int
+		wantUnverifiedCount int
+	}{
+		{
+			name: "all unverified",
+			resultsToClean: []Result{
+				{Redacted: "abc", Verified: false},
+				{Redacted: "def", Verified: false},
+			},
+			wantVerifiedCount:   0,
+			wantUnverifiedCount: 1,
+		},
+		{
+			name: "all verified",
+			resultsToClean: []Result{
+				{Redacted: "abc", Verified: true},
+				{Redacted: "def", Verified: true},
+			},
+			wantVerifiedCount:   2,
+			wantUnverifiedCount: 0,
+		},
+		{
+			name: "mixed verified/unverified",
+			resultsToClean: []Result{
+				{Redacted: "abc", Verified: true},
+				{Redacted: "def", Verified: false},
+				{Redacted: "ghi", Verified: true},
+			},
+			wantVerifiedCount:   2,
+			wantUnverifiedCount: 0,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			cleaner := DefaultResultsCleaner{}
+
+			cleaned := cleaner.CleanResults(tt.resultsToClean)
+
+			gotVerifiedCount := 0
+			gotUnverifiedCount := 0
+			for _, r := range cleaned {
+				if r.Verified {
+					gotVerifiedCount++
+				} else {
+					gotUnverifiedCount++
+				}
+			}
+
+			assert.Equal(t, tt.wantVerifiedCount, gotVerifiedCount)
+			assert.Equal(t, tt.wantUnverifiedCount, gotUnverifiedCount)
+		})
+	}
 }
