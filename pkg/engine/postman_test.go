5edab541b4dd705b9296a4fb2fe5113009eb21d0
nash <nash.catami@trufflesec.com>
2024-08-26 13:44:03 -0400

    Fixed the checks for local exported data

---
+++ pkg/engine/postman_test.go
@@ -0,0 +1,89 @@
+package engine
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/decoders"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/sources"
+)
+
+func TestPostmanEngine(t *testing.T) {
+	tests := []struct {
+		name          string
+		postmanConfig sources.PostmanConfig
+		wantErr       bool
+		errMsg        string
+	}{
+		{
+			name: "scanned Postman with a token",
+			postmanConfig: sources.PostmanConfig{
+				Token: "PMAK-66c885ef8263e50001469b8e-30f557a09f1a6f95aea0a29f5c72957a04",
+			},
+		},
+		{
+			name: "scanned Postman with workspacePath",
+			postmanConfig: sources.PostmanConfig{
+				WorkspacePaths: []string{"Downloads/Test API.postman_collection.json"},
+			},
+		},
+		{
+			name: "scanned Postman with environmentPath",
+			postmanConfig: sources.PostmanConfig{
+				EnvironmentPaths: []string{"Downloads/Mobile - Points Unlock Redeemables.postman_environment.json"},
+			},
+		},
+		{
+			name: "no token or file path provided",
+			// postmanConfig: sources.PostmanConfig{},
+			wantErr: true,
+			errMsg:  "no path to locally exported data or API token provided",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.TODO())
+			defer cancel()
+
+			const defaultOutputBufferSize = 64
+			opts := []func(*sources.SourceManager){
+				sources.WithSourceUnits(),
+				sources.WithBufferedOutput(defaultOutputBufferSize),
+			}
+
+			sourceManager := sources.NewManager(opts...)
+
+			conf := Config{
+				Concurrency:   1,
+				Decoders:      decoders.DefaultDecoders(),
+				Detectors:     DefaultDetectors(),
+				Verify:        false,
+				SourceManager: sourceManager,
+				Dispatcher:    NewPrinterDispatcher(new(discardPrinter)),
+			}
+
+			e, err := NewEngine(ctx, &conf)
+			assert.NoError(t, err)
+
+			e.Start(ctx)
+
+			go func() {
+				resultCount := 0
+				for range e.ResultsChan() {
+					resultCount++
+				}
+			}()
+
+			err = e.ScanPostman(ctx, test.postmanConfig)
+			if err != nil && !test.wantErr && err.Error() != test.errMsg {
+				t.Errorf("ScanPostman() got: %v, want: %v", err, nil)
+				return
+			}
+			if err == nil && test.wantErr {
+				t.Errorf("ScanPostman() got: %v, want: %v", err, "error")
+			}
+		})
+	}
+}
