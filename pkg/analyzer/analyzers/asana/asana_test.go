c1e3c4efa233c4e683c2790edac9658bf948c7e4
Abdul Basit <basit.mussani@gmail.com>
2024-08-27 17:14:06 +0500

    to make test cleaner moved want json in external file. Moreover without sorting test will not be able to compare.

--- pkg/analyzer/analyzers/asana/asana_test.go
+++ pkg/analyzer/analyzers/asana/asana_test.go
@@ -1,7 +1,9 @@
 package asana
 
 import (
+	_ "embed"
 	"encoding/json"
+	"sort"
 	"testing"
 	"time"
 
@@ -10,6 +12,9 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
 )
+
+//go:embed expected_output.json
+var expectedOutput []byte
 
 func TestAnalyzer_Analyze(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
@@ -26,38 +31,9 @@ func TestAnalyzer_Analyze(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "valid Asana OAUTH Token",
-			key:  testSecrets.MustGetField("ASANAOAUTH_TOKEN"),
-			want: `{"AnalyzerType":0,"Bindings":[{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"projects:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"sections:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"tags:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"events:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"users:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"memberships:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"project_memberships:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"projects:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"project_memberships:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"tasks:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"memberships:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"custom_field_settings:write","Parent":null}},
-			{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"custom_fields:write","Parent":null}},
-			{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"jobs:write","Parent":null}},
-			{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"user_task_lists:write","Parent":null}},
-			{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"custom_fields:read","Parent":null}},
-			{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"attachments:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"batch_api:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"portfolios:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"teams:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"rules:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"rules:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"allocations:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},
-			"Parent":null},"Permission":{"Value":"goals:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"jobs:read","Parent":null}},
-			{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"tags:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"tasks:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},
-			"Permission":{"Value":"autdit_logs:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"batch_api:write","Parent":null}},{"Resource":{"Name":"John Done",
-			"FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"events:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"portfolios:write","Parent":null}},
-			{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"sections:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"teams:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"user_task_lists:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"allocations:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"autdit_logs:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"custom_field_settings:read","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"goals:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user",
-			"Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"users:write","Parent":null}},{"Resource":{"Name":"John Done","FullyQualifiedName":"1200089577600156","Type":"user","Metadata":{"email":"away45846@gmail.com","type":"user"},"Parent":null},"Permission":{"Value":"attachments:read","Parent":null}}],
-			"UnboundedResources":[{"Name":"Engineering","FullyQualifiedName":"1200089577216305","Type":"workspace","Metadata":null,"Parent":null}],"Metadata":null}`,
+			name:    "valid Asana OAUTH Token",
+			key:     testSecrets.MustGetField("ASANAOAUTH_TOKEN"),
+			want:    string(expectedOutput),
 			wantErr: false,
 		},
 	}
@@ -71,6 +47,9 @@ func TestAnalyzer_Analyze(t *testing.T) {
 				return
 			}
 
+			// bindings need to be in the same order to be comparable
+			sortBindings(got.Bindings)
+
 			// Marshal the actual result to JSON
 			gotJSON, err := json.Marshal(got)
 			if err != nil {
@@ -82,6 +61,9 @@ func TestAnalyzer_Analyze(t *testing.T) {
 			if err := json.Unmarshal([]byte(tt.want), &wantObj); err != nil {
 				t.Fatalf("could not unmarshal want JSON string: %s", err)
 			}
+
+			// bindings need to be in the same order to be comparable
+			sortBindings(wantObj.Bindings)
 
 			// Marshal the expected result to JSON (to normalize)
 			wantJSON, err := json.Marshal(wantObj)
@@ -105,4 +87,14 @@ func TestAnalyzer_Analyze(t *testing.T) {
 			}
 		})
 	}
+}
+
+// Helper function to sort bindings
+func sortBindings(bindings []analyzers.Binding) {
+	sort.SliceStable(bindings, func(i, j int) bool {
+		if bindings[i].Resource.Name == bindings[j].Resource.Name {
+			return bindings[i].Permission.Value < bindings[j].Permission.Value
+		}
+		return bindings[i].Resource.Name < bindings[j].Resource.Name
+	})
 }
