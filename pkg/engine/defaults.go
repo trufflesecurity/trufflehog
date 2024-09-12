d201e543053f24f8edbc1caab9eb63f57d95714d
dylanTruffle <52866392+dylanTruffle@users.noreply.github.com>
2024-09-11 19:17:17 -0700

    adding pypi detector (#3287)
    * adding pypi detector
    
    * update test and use helper
    
    ---------
    
    Co-authored-by: Dylan Ayrey <dxa4481@rit.edu>
    Co-authored-by: Dustin Decker <dustin@trufflesec.com>

--- pkg/engine/defaults.go
+++ pkg/engine/defaults.go
@@ -551,6 +551,7 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/purestake"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/pushbulletapikey"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/pusherchannelkey"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/pypi"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/qase"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/qualaroo"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/qubole"
@@ -806,6 +807,7 @@ import (
 func DefaultDetectors() []detectors.Detector {
 	return []detectors.Detector{
 		&heroku.Scanner{},
+		&pypi.Scanner{},
 		&linearapi.Scanner{},
 		&alibaba.Scanner{},
 		aws.New(),
