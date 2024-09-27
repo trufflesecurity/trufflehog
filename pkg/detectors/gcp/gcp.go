cdca3601d198649c6cdcb9ed8d68968f3f4997af
kashif khan <kashifk@folio3.com>
2024-09-27 12:26:00 +0500

    improved gcp detector

--- pkg/detectors/gcp/gcp.go
+++ pkg/detectors/gcp/gcp.go
@@ -1,20 +1,25 @@
 package gcp
 
 import (
-	"bytes"
 	"context"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"net/http"
 	"strings"
 
 	regexp "github.com/wasilibs/go-re2"
 
-	"golang.org/x/oauth2/google"
-
+	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
 )
 
-type Scanner struct{}
+type Scanner struct {
+	client *http.Client
+}
 
 // Ensure the Scanner satisfies the interface at compile time.
 var _ detectors.Detector = (*Scanner)(nil)
@@ -23,26 +28,19 @@ var _ detectors.MaxSecretSizeProvider = (*Scanner)(nil)
 var _ detectors.StartOffsetProvider = (*Scanner)(nil)
 
 var (
-	keyPat = regexp.MustCompile(`\{[^{]+auth_provider_x509_cert_url[^}]+\}`)
+	defaultClient = common.SaneHttpClient()
+
+	keyPat        = regexp.MustCompile(`\{[^{]+auth_provider_x509_cert_url[^}]+\}`)
+	privateKeyPat = regexp.MustCompile(`-----BEGIN PRIVATE KEY-----[a-zA-Z0-9+/=\\s]+-----END PRIVATE KEY-----`)
+	x509UrlPat    = regexp.MustCompile(`https://www\.googleapis\.com/robot/v1/metadata/x509/[^"]+`)
 )
 
-type gcpKey struct {
-	Type                    string `json:"type"`
-	ProjectID               string `json:"project_id"`
-	PrivateKeyID            string `json:"private_key_id"`
-	PrivateKey              string `json:"private_key"`
-	ClientEmail             string `json:"client_email"`
-	ClientID                string `json:"client_id"`
-	AuthURI                 string `json:"auth_uri"`
-	TokenURI                string `json:"token_uri"`
-	AuthProviderX509CertURL string `json:"auth_provider_x509_cert_url"`
-	ClientX509CertURL       string `json:"client_x509_cert_url"`
-}
+func (s Scanner) getClient() *http.Client {
+	if s.client != nil {
+		return s.client
+	}
 
-func trimCarrots(s string) string {
-	s = strings.TrimPrefix(s, "<")
-	s = strings.TrimSuffix(s, ">")
-	return s
+	return defaultClient
 }
 
 // Keywords are used for efficiently pre-filtering chunks.
@@ -65,72 +63,31 @@ func (Scanner) StartOffset() int64 { return startOffset }
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
 
-	matches := keyPat.FindAllString(dataStr, -1)
+	keys := keyPat.FindAllString(dataStr, -1)
 
-	for _, match := range matches {
-		key := match
+	for _, key := range keys {
+		// find private key and x509 url from the key
+		privateKeyPEM := privateKeyPat.FindString(key)
+		x509URL := x509UrlPat.FindString(key)
 
-		key = strings.ReplaceAll(key, `,\\n`, `\n`)
-		key = strings.ReplaceAll(key, `\"\\n`, `\n`)
-		key = strings.ReplaceAll(key, `\\"`, `"`)
-
-		creds := gcpKey{}
-		err := json.Unmarshal([]byte(key), &creds)
-		if err != nil {
-			continue
-		}
-
-		// for Slack mangling (mailto scheme and hyperlinks)
-		if strings.Contains(creds.ClientEmail, `<mailto:`) {
-			creds.ClientEmail = strings.Split(strings.Split(creds.ClientEmail, `<mailto:`)[1], `|`)[0]
-		}
-		creds.AuthProviderX509CertURL = trimCarrots(creds.AuthProviderX509CertURL)
-		creds.AuthURI = trimCarrots(creds.AuthURI)
-		creds.ClientX509CertURL = trimCarrots(creds.ClientX509CertURL)
-		creds.TokenURI = trimCarrots(creds.TokenURI)
-
-		// Not sure why this might happen, but we've observed this with a verified cred
-		raw := []byte(creds.ClientEmail)
-		if len(raw) == 0 {
-			raw = []byte(key)
-		}
-		// This is an unprivileged service account used in Kubernetes' tests. It is intentionally public.
-		// https://github.com/kubernetes/kubernetes/blob/10a06602223eab17e02e197d1da591727c756d32/test/e2e_node/runtime_conformance_test.go#L50
-		if bytes.Equal(raw, []byte("image-pulling@authenticated-image-pulling.iam.gserviceaccount.com")) {
-			continue
-		}
-
-		credBytes, _ := json.Marshal(creds)
-
-		result := detectors.Result{
+		s1 := detectors.Result{
 			DetectorType: detectorspb.DetectorType_GCP,
-			Raw:          raw,
-			RawV2:        credBytes,
-			Redacted:     creds.ClientEmail,
-		}
-		// Set the RotationGuideURL in the ExtraData
-		result.ExtraData = map[string]string{
-			"rotation_guide": "https://howtorotate.com/docs/tutorials/gcp/",
-			"project":        creds.ProjectID,
+			Raw:          []byte(x509URL),
+			ExtraData: map[string]string{
+				"rotation_guide": "https://howtorotate.com/docs/tutorials/gcp/",
+			},
 		}
 
 		if verify {
-			credentials, err := google.CredentialsFromJSON(ctx, credBytes, "https://www.googleapis.com/auth/cloud-platform")
-			if err != nil {
-				continue
-			}
-			if credentials != nil {
-				_, err = credentials.TokenSource.Token()
-				if err == nil {
-					result.Verified = true
-				}
-			}
+			isVerified, verificationErr := s.verifyGCP(ctx, privateKeyPEM, x509URL)
+			s1.Verified = isVerified
+			s1.SetVerificationError(verificationErr)
 		}
 
-		results = append(results, result)
+		results = append(results, s1)
 	}
 
-	return
+	return results, nil
 }
 
 func (s Scanner) IsFalsePositive(_ detectors.Result) (bool, string) {
@@ -143,4 +100,113 @@ func (s Scanner) Type() detectorspb.DetectorType {
 
 func (s Scanner) Description() string {
 	return "GCP (Google Cloud Platform) is a suite of cloud computing services that runs on the same infrastructure that Google uses internally for its end-user products. GCP keys can be used to access and manage these services."
+}
+
+// getPublicKeys make a GET call to the provided x509Url and get the signed public keys
+func getPublicKeys(ctx context.Context, client *http.Client, x509Url string) (map[string]string, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, x509Url, http.NoBody)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		// decode the JSON response
+		var publicKeys map[string]string
+
+		if err = json.NewDecoder(resp.Body).Decode(&publicKeys); err != nil {
+			return nil, fmt.Errorf("error decoding JSON response: %v", err)
+		}
+
+		return publicKeys, nil
+	case http.StatusNotFound:
+		return nil, fmt.Errorf("no public keys found for x509 URL: %s", x509Url)
+	default:
+		return map[string]string{}, fmt.Errorf("unexpected HTTP response status %d", resp.StatusCode)
+	}
+}
+
+func (s Scanner) verifyGCP(ctx context.Context, privateKeyPEM, x509URL string) (bool, error) {
+	if privateKeyPEM == "" || x509URL == "" {
+		return false, nil // skip this key if private key or x509 URL is missing
+	}
+
+	// make a call to x509 URL to get the signed public keys
+	publicKeysMap, err := getPublicKeys(ctx, s.getClient(), x509URL)
+	if err != nil {
+		return false, err
+	}
+
+	// parse the private key
+	privateKey, err := parseRSAPrivateKey(privateKeyPEM)
+	if err != nil {
+		return false, err
+	}
+
+	// loop over all public keys and compare them with private key
+	for _, publicKeyPEM := range publicKeysMap {
+		publicKey, err := parseRSAPublicKey(publicKeyPEM)
+		if err != nil {
+			continue
+		}
+
+		// if any keys match, return verified as true
+		if compareKeys(privateKey, publicKey) {
+			return true, nil
+		}
+	}
+
+	return false, fmt.Errorf("private key didn't match with any public keys from client_x509_cert_url: %s", x509URL)
+}
+
+// parseRSAPrivateKey parse RSA private key from PEM
+func parseRSAPrivateKey(privateKeyPEM string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(privateKeyPEM))
+	if block == nil || !strings.Contains(block.Type, "PRIVATE KEY") {
+		return nil, fmt.Errorf("failed to decode PEM block containing private key")
+	}
+
+	// decode the private key (PKCS#1 or PKCS#8)
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		privateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse private key: %v", err)
+		}
+	}
+
+	return privateKey.(*rsa.PrivateKey), nil
+}
+
+// parseRSAPublicKey parse RSA public key from x509 certificate (PEM encoded)
+func parseRSAPublicKey(publicKeyPEM string) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode([]byte(publicKeyPEM))
+	if block == nil || !strings.Contains(block.Type, "CERTIFICATE") {
+		return nil, fmt.Errorf("failed to decode PEM block containing certificate")
+	}
+
+	// parse the x509 certificate
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse x509 certificate: %v", err)
+	}
+
+	// extract and return the public key as *rsa.PublicKey
+	publicKey, ok := cert.PublicKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("not an RSA public key")
+	}
+	return publicKey, nil
+}
+
+// compareKeys compare RSA private key and public key
+func compareKeys(privateKey *rsa.PrivateKey, publicKey *rsa.PublicKey) bool {
+	// compare the modulus (N) of the private key and public key
+	return privateKey.PublicKey.N.Cmp(publicKey.N) == 0
 }
