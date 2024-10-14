b7e74bcc4af1256ff8ef8ef90b048a679263b535
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-10-14 10:25:29 -0700

    Fix dropbox detector

--- pkg/detectors/dropbox/dropbox.go
+++ pkg/detectors/dropbox/dropbox.go
@@ -3,71 +3,89 @@ package dropbox
 import (
 	"context"
 	"fmt"
-	regexp "github.com/wasilibs/go-re2"
+	"io"
 	"net/http"
+
+	regexp "github.com/wasilibs/go-re2"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
 )
 
-type Scanner struct{}
+type Scanner struct{ client *http.Client }
 
 // Ensure the Scanner satisfies the interface at compile time.
 var _ detectors.Detector = (*Scanner)(nil)
 
 var (
-	keyPat = regexp.MustCompile(`\b(sl\.[A-Za-z0-9\-\_]{130,140})\b`)
+	defaultClient = common.SaneHttpClient()
+	keyPat        = regexp.MustCompile(`sl\.u\.[0-9a-zA-Z_-]+`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
 // Use identifiers in the secret preferably, or the provider name.
-func (s Scanner) Keywords() []string {
-	return []string{"sl."}
-}
+func (s Scanner) Keywords() []string { return []string{"sl.u."} }
 
 // FromData will find and optionally verify Dropbox secrets in a given set of bytes.
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
-
 	dataStr := string(data)
 
-	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
+	uniqueMatches := make(map[string]struct{})
+	for _, match := range keyPat.FindAllStringSubmatch(dataStr, -1) {
+		uniqueMatches[match[0]] = struct{}{}
+	}
 
-	for _, match := range matches {
-
-		result := detectors.Result{
+	for match := range uniqueMatches {
+		s1 := detectors.Result{
 			DetectorType: detectorspb.DetectorType_Dropbox,
-			Raw:          []byte(match[1]),
+			Raw:          []byte(match),
 		}
 
 		if verify {
-
-			baseURL := "https://api.dropboxapi.com/2/users/get_current_account"
-
-			client := common.SaneHttpClient()
-
-			req, err := http.NewRequestWithContext(ctx, "POST", baseURL, nil)
-			if err != nil {
-				continue
+			client := s.client
+			if client == nil {
+				client = defaultClient
 			}
-			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", match[1]))
-			res, err := client.Do(req)
-			if err == nil {
-				res.Body.Close() // The request body is unused.
 
-				// 200 means good key for get current user
-				// 400 is bad (malformed)
-				// 403 bad scope
-				if res.StatusCode == http.StatusOK {
-					result.Verified = true
-				}
-			}
+			isVerified, verificationErr := verifyMatch(ctx, client, match)
+			s1.Verified = isVerified
+			s1.SetVerificationError(verificationErr, match)
 		}
 
-		results = append(results, result)
+		results = append(results, s1)
 	}
 
 	return
+}
+
+func verifyMatch(ctx context.Context, client *http.Client, token string) (bool, error) {
+	const baseURL = "https://api.dropboxapi.com/2/check/user"
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, baseURL, nil)
+	if err != nil {
+		return false, nil
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer func() {
+		_, _ = io.Copy(io.Discard, res.Body)
+		_ = res.Body.Close()
+	}()
+
+	switch res.StatusCode {
+	case http.StatusOK:
+		return true, nil
+	case http.StatusUnauthorized:
+		// 401 access token not found
+		// The secret is determinately not verified (nothing to do)
+		return false, nil
+	default:
+		return false, fmt.Errorf("unexpected HTTP response status %d", res.StatusCode)
+	}
 }
 
 func (s Scanner) Type() detectorspb.DetectorType {
--- pkg/detectors/dropbox/dropbox_test.go
+++ pkg/detectors/dropbox/dropbox_test.go
@@ -5,110 +5,67 @@ package dropbox
 
 import (
 	"context"
-	"fmt"
 	"testing"
-	"time"
 
-	"github.com/kylelemons/godebug/pretty"
-	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
+	"github.com/google/go-cmp/cmp"
+
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
-	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/engine/ahocorasick"
 )
 
-func TestDropbox_FromChunk(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-	testSecrets, err := common.GetSecret(ctx, "trufflehog-testing", "detectors2")
-	if err != nil {
-		t.Fatalf("could not get test secrets from GCP: %s", err)
-	}
-	secret := testSecrets.MustGetField("DROPBOX")
-	secretInactive := testSecrets.MustGetField("DROPBOX_INACTIVE")
-	type args struct {
-		ctx    context.Context
-		data   []byte
-		verify bool
-	}
+func TestDropbox_Pattern(t *testing.T) {
+	d := Scanner{}
+	ahoCorasickCore := ahocorasick.NewAhoCorasickCore([]detectors.Detector{d})
 	tests := []struct {
-		name    string
-		s       Scanner
-		args    args
-		want    []detectors.Result
-		wantErr bool
+		name  string
+		input string
+		want  []string
 	}{
 		{
-			name: "found, verified",
-			s:    Scanner{},
-			args: args{
-				ctx:    context.Background(),
-				data:   []byte(fmt.Sprintf("You can find a dropbox secret %s within", secret)),
-				verify: true,
-			},
-			want: []detectors.Result{
-				{
-					DetectorType: detectorspb.DetectorType_Dropbox,
-					Verified:     true,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "found, unverified",
-			s:    Scanner{},
-			args: args{
-				ctx:    context.Background(),
-				data:   []byte(fmt.Sprintf("You can find a dropbox secret %s within", secretInactive)),
-				verify: true,
-			},
-			want: []detectors.Result{
-				{
-					DetectorType: detectorspb.DetectorType_Dropbox,
-					Verified:     false,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "not found",
-			s:    Scanner{},
-			args: args{
-				ctx:    context.Background(),
-				data:   []byte("You cannot find the secret within"),
-				verify: true,
-			},
-			want:    nil,
-			wantErr: false,
+			name:  "typical pattern",
+			input: "dropbox_access_token = 'sl.u.AFTdsdpAiWY_giz5n5am9X7M01K4MeXwxOxhJAJGilJ4bj4uFlUxrA4XV96JOnBXf92I_rjBbpmM6kNELLAfe1Mte1IHkYYIPOyqbciOJECMUoyFOBl9GxjJN9_FjTjjBIBQR_lPskThzIfI0IXSIKSLc7fH9hP-2jwbxYdyZTBOwPJ9y3cuClJbbR-A3rejwAL3on0RSTxoSYcDezHlW0SvR7TAwEcuqqIqpJA6_cxyhG-jfMAh4kr4AS_kghKHrzDR_WeyoC5Ju8ev4o6Y9I_rcj787hS_JuBGZcEZ9yI9mYb4l0jwU84tb3HF7aza4XqhSTEhgfBNrTvPJ-YNhrS59KZKG7vVkTOqpyQBiaGHXrA4SFUA_PhbLA9y9r8f1XRR-VLwYavzMb6TtZjSRh5TdPO1g70u7SEzoAz5uWwXX6y-L5HocLCprsbzGypmSYT0lO3N4r2aqsjCuzxsEODOXBex2qofkk5fOSuu5XewElxQxacYZYyB3xBKC_TJ-EWhm44xDsmE1u-Ymeo3CQVJZFbqD_csb5SaUpuRv1-Jn2lkiNirq9jyTZyfnJbruMpdsVzfDxh84ArR-prFtOKR04GV_R6izGdkXZM47WVKJn8TaCJlmfLwc68NW6VgiR_EVxBTQ5Mou0OLo61KTj3PqoObhrRIz5C4-GDbWY4AFW3Ht_RlEtuinyDFNu1i_0dxWW_DMt9u0iwNeRXD5YPBOAD9uia90G-ohlbwtzPSsvnFcebE_6tEIuTosoKtMd8F88ZVwViAPkaV_hVAohOWbAvgCAoS_kfWWqY4kLRojor2DRDLgR3JcuRsapgyqNuT7hbbxFMYPYCnb9GHz3teyolFL0bIfHPgHA_y7AEQknXJskl2farR_waPI3i3l2Xsn7Pm7sPplSrBbuiRj6ZFWGk3heZ05InkgfXoRDal018XWGlUMV6tf7GYbGXccRhqMbsRUZmDcJfffRPZI0Vxxohcl6LHF3ydGGEOb-wsdyAU1IgKt6t1Fq3PUljJg0c0sFIiztffjuOQY5igJC1ts6S5_W34v4u95ydRMnCj08MCviUQUYAXp4ykVoiHydJVOq2LkOcjcTxgPuLyghGLoBYYA9Yb4XpgWyfB0XhCbeDHuoQhaaHToHTBzvsm1cXwZFQDKb69Xdq3yIHyFI6GUyoEZXEok0clHbM1TCS9cacoM4Go2Pw9cWpUgTSq3q_7-9bClx9ky8m-74Vi_dzUhu9c_MIuETCy4SQEffYxvFNVAdbuCeGVYngOQ5DyXCJq1V-SB8v52eNG1KLIfHtn'",
+			want:  []string{"sl.u.AFTdsdpAiWY_giz5n5am9X7M01K4MeXwxOxhJAJGilJ4bj4uFlUxrA4XV96JOnBXf92I_rjBbpmM6kNELLAfe1Mte1IHkYYIPOyqbciOJECMUoyFOBl9GxjJN9_FjTjjBIBQR_lPskThzIfI0IXSIKSLc7fH9hP-2jwbxYdyZTBOwPJ9y3cuClJbbR-A3rejwAL3on0RSTxoSYcDezHlW0SvR7TAwEcuqqIqpJA6_cxyhG-jfMAh4kr4AS_kghKHrzDR_WeyoC5Ju8ev4o6Y9I_rcj787hS_JuBGZcEZ9yI9mYb4l0jwU84tb3HF7aza4XqhSTEhgfBNrTvPJ-YNhrS59KZKG7vVkTOqpyQBiaGHXrA4SFUA_PhbLA9y9r8f1XRR-VLwYavzMb6TtZjSRh5TdPO1g70u7SEzoAz5uWwXX6y-L5HocLCprsbzGypmSYT0lO3N4r2aqsjCuzxsEODOXBex2qofkk5fOSuu5XewElxQxacYZYyB3xBKC_TJ-EWhm44xDsmE1u-Ymeo3CQVJZFbqD_csb5SaUpuRv1-Jn2lkiNirq9jyTZyfnJbruMpdsVzfDxh84ArR-prFtOKR04GV_R6izGdkXZM47WVKJn8TaCJlmfLwc68NW6VgiR_EVxBTQ5Mou0OLo61KTj3PqoObhrRIz5C4-GDbWY4AFW3Ht_RlEtuinyDFNu1i_0dxWW_DMt9u0iwNeRXD5YPBOAD9uia90G-ohlbwtzPSsvnFcebE_6tEIuTosoKtMd8F88ZVwViAPkaV_hVAohOWbAvgCAoS_kfWWqY4kLRojor2DRDLgR3JcuRsapgyqNuT7hbbxFMYPYCnb9GHz3teyolFL0bIfHPgHA_y7AEQknXJskl2farR_waPI3i3l2Xsn7Pm7sPplSrBbuiRj6ZFWGk3heZ05InkgfXoRDal018XWGlUMV6tf7GYbGXccRhqMbsRUZmDcJfffRPZI0Vxxohcl6LHF3ydGGEOb-wsdyAU1IgKt6t1Fq3PUljJg0c0sFIiztffjuOQY5igJC1ts6S5_W34v4u95ydRMnCj08MCviUQUYAXp4ykVoiHydJVOq2LkOcjcTxgPuLyghGLoBYYA9Yb4XpgWyfB0XhCbeDHuoQhaaHToHTBzvsm1cXwZFQDKb69Xdq3yIHyFI6GUyoEZXEok0clHbM1TCS9cacoM4Go2Pw9cWpUgTSq3q_7-9bClx9ky8m-74Vi_dzUhu9c_MIuETCy4SQEffYxvFNVAdbuCeGVYngOQ5DyXCJq1V-SB8v52eNG1KLIfHtn"},
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := Scanner{}
-			got, err := s.FromData(tt.args.ctx, tt.args.verify, tt.args.data)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Dropbox.FromData() error = %v, wantErr %v", err, tt.wantErr)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			matchedDetectors := ahoCorasickCore.FindDetectorMatches([]byte(test.input))
+			if len(matchedDetectors) == 0 {
+				t.Errorf("keywords '%v' not matched by: %s", d.Keywords(), test.input)
 				return
 			}
-			for i := range got {
-				got[i].Raw = nil
-			}
-			if diff := pretty.Compare(got, tt.want); diff != "" {
-				t.Errorf("Dropbox.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
-			}
-		})
-	}
-}
 
-func BenchmarkFromData(benchmark *testing.B) {
-	ctx := context.Background()
-	s := Scanner{}
-	for name, data := range detectors.MustGetBenchmarkData() {
-		benchmark.Run(name, func(b *testing.B) {
-			b.ResetTimer()
-			for n := 0; n < b.N; n++ {
-				_, err := s.FromData(ctx, false, data)
-				if err != nil {
-					b.Fatal(err)
+			results, err := d.FromData(context.Background(), false, []byte(test.input))
+			if err != nil {
+				t.Errorf("error = %v", err)
+				return
+			}
+
+			if len(results) != len(test.want) {
+				if len(results) == 0 {
+					t.Errorf("did not receive result")
+				} else {
+					t.Errorf("expected %d results, only received %d", len(test.want), len(results))
 				}
+				return
+			}
+
+			actual := make(map[string]struct{}, len(results))
+			for _, r := range results {
+				if len(r.RawV2) > 0 {
+					actual[string(r.RawV2)] = struct{}{}
+				} else {
+					actual[string(r.Raw)] = struct{}{}
+				}
+			}
+			expected := make(map[string]struct{}, len(test.want))
+			for _, v := range test.want {
+				expected[v] = struct{}{}
+			}
+
+			if diff := cmp.Diff(expected, actual); diff != "" {
+				t.Errorf("%s diff: (-want +got)\n%s", test.name, diff)
 			}
 		})
 	}
