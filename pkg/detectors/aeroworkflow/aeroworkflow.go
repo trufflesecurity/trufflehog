3dd41adf596f120868d8aa15c4afb15c4a269870
Sahil Silare <sahilsilare@gmail.com>
2024-10-16 22:31:25 +0530

    fix: fixed pattern detection logic in aeroworkflow

--- pkg/detectors/aeroworkflow/aeroworkflow.go
+++ pkg/detectors/aeroworkflow/aeroworkflow.go
@@ -27,7 +27,7 @@ var (
 	defaultClient = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"aeroworkflow"}) + `([a-zA-Z0-9^!?#:*;]{20})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"aeroworkflow"}) + `\b([a-zA-Z0-9^!?#:*;]{20})`)
 	idPat  = regexp.MustCompile(detectors.PrefixRegex([]string{"aeroworkflow"}) + `\b([0-9]{1,})\b`)
 )
 
