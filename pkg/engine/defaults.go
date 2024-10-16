f0dbe7db27e3b4e175f89586e97b26dcaa575234
Sahil Silare <sahilsilare@gmail.com>
2024-10-16 11:01:32 +0530

    feat: added onepassword

--- pkg/engine/defaults.go
+++ pkg/engine/defaults.go
@@ -483,6 +483,7 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/onedesk"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/onelogin"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/onepagecrm"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/onepassword"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/onesignal"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/onfleet"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors/oopspam"
@@ -1642,6 +1643,7 @@ func DefaultDetectors() []detectors.Detector {
 		meraki.Scanner{},
 		saladcloudapikey.Scanner{},
 		boxoauth.Scanner{},
+		onepassword.Scanner{},
 	}
 
 	// Automatically initialize all detectors that implement
