a606644adef0c09605c19b278af7a46e8ecd306d
Sahil Silare <sahilsilare@gmail.com>
2024-10-17 03:30:57 +0530

    fix: added correct validation check for apiscience

--- pkg/detectors/apiscience/apiscience.go
+++ pkg/detectors/apiscience/apiscience.go
@@ -22,7 +22,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"apiscience"}) + `\b([a-bA-Z0-9\S]{22})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"apiscience"}) + `\b([a-bA-Z0-9\S]{32})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
