841b6e3e8f94d2507cc262a7ebec6431a3b8554c
Sahil Silare <sahilsilare@gmail.com>
2024-10-16 10:09:16 +0530

    feat: added correct rootly detector

--- pkg/detectors/rootly/rootly.go
+++ pkg/detectors/rootly/rootly.go
@@ -2,9 +2,8 @@ package rootly
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"net/http"
+	"strings"
 
 	regexp "github.com/wasilibs/go-re2"
 
@@ -13,83 +12,60 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
 )
 
-type Scanner struct {
-	client *http.Client
-}
+type Scanner struct{}
 
 // Ensure the Scanner satisfies the interface at compile time.
 var _ detectors.Detector = (*Scanner)(nil)
 
 var (
-	defaultClient = common.SaneHttpClient()
-	keyPat        = regexp.MustCompile(detectors.PrefixRegex([]string{"rootly"}) + `\b(rootly_[a-f0-9]{64})\b`)
+	client = common.SaneHttpClient()
+
+	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
+	keyPat = regexp.MustCompile(`\brootly_[a-f0-9]{64}\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
 // Use identifiers in the secret preferably, or the provider name.
 func (s Scanner) Keywords() []string {
-	return []string{"rootly_"}
+	return []string{"rootly"}
 }
 
 // FromData will find and optionally verify Rootly secrets in a given set of bytes.
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
 
-	uniqueMatches := make(map[string]struct{})
-	for _, match := range keyPat.FindAllStringSubmatch(dataStr, -1) {
-		uniqueMatches[match[1]] = struct{}{}
-	}
+	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
 
-	for match := range uniqueMatches {
+	for _, match := range matches {
+		if len(match) != 1 {
+			continue
+		}
+		resMatch := strings.TrimSpace(match[0])
+
 		s1 := detectors.Result{
 			DetectorType: detectorspb.DetectorType_Rootly,
-			Raw:          []byte(match),
+			Raw:          []byte(resMatch),
 		}
 
 		if verify {
-			client := s.client
-			if client == nil {
-				client = defaultClient
+			req, err := http.NewRequestWithContext(ctx, "GET", "https://api.rootly.com/v1/users/me", nil)
+			if err != nil {
+				continue
 			}
-
-			isVerified, extraData, verificationErr := verifyMatch(ctx, client, match)
-			s1.Verified = isVerified
-			s1.ExtraData = extraData
-			s1.SetVerificationError(verificationErr, match)
+			req.Header.Add("Authorization", "Bearer "+resMatch)
+			res, err := client.Do(req)
+			if err == nil {
+				defer res.Body.Close()
+				if res.StatusCode >= 200 && res.StatusCode < 300 {
+					s1.Verified = true
+				}
+			}
 		}
 
 		results = append(results, s1)
 	}
 
-	return
-}
-
-func verifyMatch(ctx context.Context, client *http.Client, token string) (bool, map[string]string, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://api.rootly.com/v1/users/me", nil)
-	if err != nil {
-		return false, nil, nil
-	}
-	req.Header.Add("Authorization", "Bearer "+token)
-
-	res, err := client.Do(req)
-	if err != nil {
-		return false, nil, err
-	}
-	defer func() {
-		_, _ = io.Copy(io.Discard, res.Body)
-		_ = res.Body.Close()
-	}()
-
-	switch res.StatusCode {
-	case http.StatusOK:
-		// If the endpoint returns useful information, we can return it as a map.
-		return true, nil, nil
-	case http.StatusUnauthorized:
-		// The secret is determinately not verified (nothing to do)
-		return false, nil, nil
-	default:
-		return false, nil, fmt.Errorf("unexpected HTTP response status %d", res.StatusCode)
-	}
+	return results, nil
 }
 
 func (s Scanner) Type() detectorspb.DetectorType {
