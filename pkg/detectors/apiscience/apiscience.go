169948af69107fbbc77df147b9de1d6be43895a1
Sahil Silare <sahilsilare@gmail.com>
2024-10-17 03:30:57 +0530

    fix: added correct validation check for apiscience

--- pkg/detectors/apiscience/apiscience.go
+++ pkg/detectors/apiscience/apiscience.go
@@ -3,9 +3,10 @@ package apiscience
 import (
 	"context"
 	"fmt"
-	regexp "github.com/wasilibs/go-re2"
 	"net/http"
 	"strings"
+
+	regexp "github.com/wasilibs/go-re2"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
@@ -21,7 +22,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"apiscience"}) + `\b([a-bA-Z0-9\S]{22})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"apiscience"}) + `\b([a-bA-Z0-9\S]{32})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
