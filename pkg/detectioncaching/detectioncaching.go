6f54591520e3d2668401a7039188631fbfb03340
Cody Rose <webmaster@codyro.se>
2024-10-17 12:54:02 -0400

    initial sketch

---
+++ pkg/detectioncaching/detectioncaching.go
@@ -0,0 +1,54 @@
+package detectioncaching
+
+import (
+	"github.com/trufflesecurity/trufflehog/v3/pkg/cache"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
+)
+
+func FromDataCached(
+	ctx context.Context,
+	cache cache.Cache[*detectors.Result],
+	detector detectors.Detector,
+	verify bool,
+	data []byte,
+) ([]detectors.Result, error) {
+	withoutVerification, err := detector.FromData(ctx, false, data)
+	if err != nil {
+		return nil, err
+	}
+
+	if !verify {
+		return withoutVerification, nil
+	}
+
+	everythingCached := false
+	var cachedResults []detectors.Result
+	for _, r := range withoutVerification {
+		if cacheHit, ok := cache.Get(cacheKey(r)); ok {
+			cachedResults = append(cachedResults, *cacheHit)
+		} else {
+			everythingCached = false
+			break
+		}
+	}
+
+	if everythingCached {
+		return cachedResults, nil
+	}
+
+	results, err := detector.FromData(ctx, true, data)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, r := range results {
+		cache.Set(cacheKey(r), &r)
+	}
+
+	return results, nil
+}
+
+func cacheKey(result detectors.Result) string {
+	return string(result.Raw) + string(result.RawV2)
+}
