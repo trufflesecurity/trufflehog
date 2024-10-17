38dfcd26ea68f3e520c8e43bd0d2b3c539b04138
Sahil Silare <sahilsilare@gmail.com>
2024-10-17 15:13:13 +0530

    fix: changed verification endpoint for ayrshare

--- pkg/detectors/ayrshare/ayrshare.go
+++ pkg/detectors/ayrshare/ayrshare.go
@@ -49,7 +49,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 		}
 
 		if verify {
-			req, err := http.NewRequestWithContext(ctx, "GET", "https://app.ayrshare.com/api/analytics/links", nil)
+			req, err := http.NewRequestWithContext(ctx, "GET", "https://app.ayrshare.com/api/user", nil)
 			if err != nil {
 				continue
 			}
