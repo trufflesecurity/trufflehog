b0d9e54820a735461cf6be2422d8ac0d16e6c85a
Sahil Silare <sahilsilare@gmail.com>
2024-10-17 15:07:56 +0530

    fix: fixed verification endpoint for AyrShare

--- pkg/detectors/ayrshare/ayrshare.go
+++ pkg/detectors/ayrshare/ayrshare.go
@@ -3,9 +3,10 @@ package ayrshare
 import (
 	"context"
 	"fmt"
-	regexp "github.com/wasilibs/go-re2"
 	"net/http"
 	"strings"
+
+	regexp "github.com/wasilibs/go-re2"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
@@ -21,7 +22,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"ayrshare"}) + `\b([A-Z]{7}-[A-Z0-9]{7}-[A-Z0-9]{7}-[A-Z0-9]{7})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"ayrshare"}) + `\b([A-Z0-9]{8}-[A-Z0-9]{8}-[A-Z0-9]{8}-[A-Z0-9]{8})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
