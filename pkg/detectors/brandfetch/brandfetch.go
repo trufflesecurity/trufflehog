f02e22bf55b8a501b3964911df9f07f9b4d749e5
Sahil Silare <sahilsilare@gmail.com>
2024-10-18 10:29:21 +0530

    fix: fixed verification endpoint and verification logic

--- pkg/detectors/brandfetch/brandfetch.go
+++ pkg/detectors/brandfetch/brandfetch.go
@@ -23,7 +23,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"brandfetch"}) + `\b([0-9A-Za-z]{40})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"brandfetch"}) + `\b([a-zA-Z0-9=]{44})`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
