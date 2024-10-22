2d1dc73c9eb29d1a533fec59687df1ddcfc8a4c5
ahrav <ahravdutta02@gmail.com>
2024-10-21 19:54:06 -0700

    [fix] - Inadvertent s3 body close (#3491)
    * fix object retrival
    
    * update comment

--- pkg/sources/s3/s3_integration_test.go
+++ pkg/sources/s3/s3_integration_test.go
@@ -10,9 +10,10 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/anypb"
+
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/credentialspb"
-	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/sourcespb"
@@ -48,6 +49,37 @@ func TestSource_ChunksCount(t *testing.T) {
 		got++
 	}
 	assert.Greater(t, got, wantChunkCount)
+}
+
+func TestSource_ChunksLarge(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	s := Source{}
+	connection := &sourcespb.S3{
+		Credential: &sourcespb.S3_Unauthenticated{},
+		Buckets:    []string{"trufflesec-ahrav-test"},
+	}
+	conn, err := anypb.New(connection)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = s.Init(ctx, "test name", 0, 0, false, conn, 1)
+	chunksCh := make(chan *sources.Chunk)
+	go func() {
+		defer close(chunksCh)
+		err = s.Chunks(ctx, chunksCh)
+		assert.Nil(t, err)
+	}()
+
+	wantChunkCount := 9637
+	got := 0
+
+	for range chunksCh {
+		got++
+	}
+	assert.Equal(t, got, wantChunkCount)
 }
 
 func TestSource_Validate(t *testing.T) {
