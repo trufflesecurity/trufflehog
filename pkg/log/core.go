c6f4a0a47d645f9e6f210814e2f8ed60fd0542ae
Cody Rose <webmaster@codyro.se>
2024-10-24 11:36:47 -0400

    apply patch

--- pkg/log/core.go
+++ pkg/log/core.go
@@ -1,41 +1,66 @@
 package log
 
 import (
+	"strings"
+
 	"go.uber.org/zap/zapcore"
 )
 
-type levelFilterCore struct {
-	core  zapcore.Core
-	level zapcore.LevelEnabler
+// redactionCore wraps a zapcore.Core to perform redaction of log messages in
+// the message and field values.
+type redactionCore struct {
+	zapcore.Core
+	replacer *strings.Replacer
 }
 
-// NewLevelCore creates a core that can be used to independently control the
-// level of an existing Core. This is essentially a filter that will only log
-// if both the parent and the wrapper cores are enabled.
-func NewLevelCore(core zapcore.Core, level zapcore.LevelEnabler) zapcore.Core {
-	return &levelFilterCore{core, level}
+// NewRedactionCore creates a zapcore.Core that performs redaction of logs in
+// the message and field values.
+func NewRedactionCore(core zapcore.Core, denyList []string) zapcore.Core {
+	denyList = stableDedupe(denyList)
+	if len(denyList) == 0 {
+		return core
+	}
+	replaceList := make([]string, 0, len(denyList)*2)
+	for _, target := range denyList {
+		replaceList = append(replaceList, target, "******")
+	}
+	return &redactionCore{core, strings.NewReplacer(replaceList...)}
 }
 
-func (c *levelFilterCore) Enabled(lvl zapcore.Level) bool {
-	return c.level.Enabled(lvl)
+// Check overrides the embedded zapcore.Core Check() method to add the
+// redactionCore to the zapcore.CheckedEntry.
+func (c *redactionCore) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	if c.Enabled(ent.Level) {
+		return ce.AddCore(ent, c)
+	}
+	return ce
 }
 
-func (c *levelFilterCore) With(fields []zapcore.Field) zapcore.Core {
-	return &levelFilterCore{c.core.With(fields), c.level}
+// Write overrides the embedded zapcore.Core Write() method to redact the
+// message and fields before passing them to be written.
+func (c *redactionCore) Write(ent zapcore.Entry, fields []zapcore.Field) error {
+	ent.Message = c.replacer.Replace(ent.Message)
+	for i := range fields {
+		fields[i].String = c.replacer.Replace(fields[i].String)
+	}
+	return c.Core.Write(ent, fields)
 }
 
-func (c *levelFilterCore) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
-	if !c.Enabled(ent.Level) {
-		return ce
+// stableDedupe is a helper function to deduplicate a slice of elements while
+// maintaining their original order.
+func stableDedupe(vals []string) []string {
+	set := make(map[string]struct{}, len(vals))
+	output := make([]string, 0, len(vals))
+	for _, val := range vals {
+		if val == "" {
+			continue
+		}
+		if _, ok := set[val]; ok {
+			continue
+		}
+		set[val] = struct{}{}
+		output = append(output, val)
 	}
 
-	return c.core.Check(ent, ce)
-}
-
-func (c *levelFilterCore) Write(ent zapcore.Entry, fields []zapcore.Field) error {
-	return c.core.Write(ent, fields)
-}
-
-func (c *levelFilterCore) Sync() error {
-	return c.core.Sync()
+	return output
 }
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -83,9 +83,10 @@ func WithSentry(opts sentry.ClientOptions, tags map[string]string) logConfig {
 }
 
 type sinkConfig struct {
-	encoder zapcore.Encoder
-	sink    zapcore.WriteSyncer
-	level   levelSetter
+	encoder       zapcore.Encoder
+	sink          zapcore.WriteSyncer
+	level         levelSetter
+	redactionList []string
 }
 
 // WithJSONSink adds a JSON encoded output to the logger.
@@ -176,6 +177,13 @@ func WithLeveler(leveler levelSetter) func(*sinkConfig) {
 	}
 }
 
+// WithRedaction adds values to be redacted from logs.
+func WithRedaction(targets ...string) func(*sinkConfig) {
+	return func(conf *sinkConfig) {
+		conf.redactionList = append(conf.redactionList, targets...)
+	}
+}
+
 // firstErrorFunc is a helper function that returns a function that executes
 // all provided args and returns the first error, if any.
 func firstErrorFunc(fs ...func() error) func() error {
@@ -209,11 +217,11 @@ func newCoreConfig(
 	for _, f := range opts {
 		f(&conf)
 	}
-	return logConfig{
-		core: zapcore.NewCore(
-			conf.encoder,
-			conf.sink,
-			conf.level,
-		),
-	}
+	core := zapcore.NewCore(
+		conf.encoder,
+		conf.sink,
+		conf.level,
+	)
+
+	return logConfig{core: NewRedactionCore(core, conf.redactionList)}
 }
