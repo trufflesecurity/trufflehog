6c823e1d60bdd6c79b39ba801711d6a3fe70b79c
Richard Gomez <32133502+rgmz@users.noreply.github.com>
2024-11-07 11:27:46 -0500

    Add UUIDs to false positive checker (#2976)
    * feat(detectors): add uuid false positives
    
    * feat(detectors): use map for false positives

--- pkg/detectors/falsepositives.go
+++ pkg/detectors/falsepositives.go
@@ -13,7 +13,12 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
 )
 
-var DefaultFalsePositives = []FalsePositive{"example", "xxxxxx", "aaaaaa", "abcde", "00000", "sample", "*****"}
+var (
+	DefaultFalsePositives = map[FalsePositive]struct{}{
+		"example": {}, "xxxxxx": {}, "aaaaaa": {}, "abcde": {}, "00000": {}, "sample": {}, "*****": {},
+	}
+	UuidFalsePositives map[FalsePositive]struct{}
+)
 
 type FalsePositive string
 
@@ -24,18 +29,21 @@ type CustomFalsePositiveChecker interface {
 	IsFalsePositive(result Result) (bool, string)
 }
 
-//go:embed "badlist.txt"
-var badList []byte
+var (
+	filter *ahocorasick.Trie
 
-//go:embed "words.txt"
-var wordList []byte
-
-//go:embed "programmingbooks.txt"
-var programmingBookWords []byte
-
-var filter *ahocorasick.Trie
+	//go:embed "fp_badlist.txt"
+	badList []byte
+	//go:embed "fp_words.txt"
+	wordList []byte
+	//go:embed "fp_programmingbooks.txt"
+	programmingBookWords []byte
+	//go:embed "fp_uuids.txt"
+	uuidList []byte
+)
 
 func init() {
+	// Populate trie.
 	builder := ahocorasick.NewTrieBuilder()
 
 	wordList := bytesToCleanWordList(wordList)
@@ -47,7 +55,16 @@ func init() {
 	programmingBookWords := bytesToCleanWordList(programmingBookWords)
 	builder.AddStrings(programmingBookWords)
 
+	uuidList := bytesToCleanWordList(uuidList)
+	builder.AddStrings(uuidList)
+
 	filter = builder.Build()
+
+	// Populate custom FalsePositive list
+	UuidFalsePositives = make(map[FalsePositive]struct{}, len(uuidList))
+	for _, uuid := range uuidList {
+		UuidFalsePositives[FalsePositive(uuid)] = struct{}{}
+	}
 }
 
 func GetFalsePositiveCheck(detector Detector) func(Result) (bool, string) {
@@ -65,15 +82,20 @@ func GetFalsePositiveCheck(detector Detector) func(Result) (bool, string) {
 //
 // Currently, this includes: english word in key or matches common example patterns.
 // Only the secret key material should be passed into this function
-func IsKnownFalsePositive(match string, falsePositives []FalsePositive, wordCheck bool) (bool, string) {
+func IsKnownFalsePositive(match string, falsePositives map[FalsePositive]struct{}, wordCheck bool) (bool, string) {
 	if !utf8.ValidString(match) {
 		return true, "invalid utf8"
 	}
 	lower := strings.ToLower(match)
-	for _, fp := range falsePositives {
+
+	if _, exists := falsePositives[FalsePositive(lower)]; exists {
+		return true, "matches term: " + lower
+	}
+
+	for fp := range falsePositives {
 		fps := string(fp)
 		if strings.Contains(lower, fps) {
-			return true, "matches term: " + fps
+			return true, "contains term: " + fps
 		}
 	}
 
