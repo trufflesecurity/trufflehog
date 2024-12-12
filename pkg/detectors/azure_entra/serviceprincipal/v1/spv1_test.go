f388e6c6cf38616c14fed6f0d50b22f62f0cdc37
Richard Gomez <rmgomez368@gmail.com>
2024-12-12 17:18:54 -0500

    chore: run unit tests

--- pkg/detectors/azure_entra/serviceprincipal/v1/spv1_test.go
+++ pkg/detectors/azure_entra/serviceprincipal/v1/spv1_test.go
@@ -1,93 +1,10 @@
 package v1
 
 import (
-	"context"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-
-	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
-	"github.com/trufflesecurity/trufflehog/v3/pkg/engine/ahocorasick"
 )
-
-var (
-	validPattern = `
-	azure credentials:
-		azureClientID: clientid9304d5df4-aac1-6117-552c-7f70c89a40d9
-		azureTenant: tenant_idid9304d5df4-aac1-6117-552c-7f70c89a40d9
-		azureClientSecret: clientsecretY_0w|[cGpan41k6ng.ol414sp4ccw2v_rkfmbs537i
-	`
-	invalidPattern = `
-	azure credentials:
-		azureClientID: 9304d5df4-aac1-6117-552c-7f70c89a
-		azureTenant: id9304d5df4-aac1-6117-55-7f70c89a40d9
-		azureClientSecret: Y_0w|[cGpan41k6ng.
-	`
-)
-
-func TestAzure_Pattern(t *testing.T) {
-	d := Scanner{}
-	ahoCorasickCore := ahocorasick.NewAhoCorasickCore([]detectors.Detector{d})
-
-	tests := []struct {
-		name  string
-		input string
-		want  []string
-	}{
-		{
-			name:  "valid pattern",
-			input: validPattern,
-			want:  []string{"304d5df4-aac1-6117-552c-7f70c89a40d9cGpan41k6ng.ol414sp4ccw2v_rkfmbs53304d5df4-aac1-6117-552c-7f70c89a40d9"},
-		},
-		{
-			name:  "invalid pattern",
-			input: invalidPattern,
-			want:  nil,
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			matchedDetectors := ahoCorasickCore.FindDetectorMatches([]byte(test.input))
-			if len(matchedDetectors) == 0 {
-				t.Errorf("keywords '%v' not matched by: %s", d.Keywords(), test.input)
-				return
-			}
-
-			results, err := d.FromData(context.Background(), false, []byte(test.input))
-			if err != nil {
-				t.Errorf("error = %v", err)
-				return
-			}
-
-			if len(results) != len(test.want) {
-				if len(results) == 0 {
-					t.Errorf("did not receive result")
-				} else {
-					t.Errorf("expected %d results, only received %d", len(test.want), len(results))
-				}
-				return
-			}
-
-			actual := make(map[string]struct{}, len(results))
-			for _, r := range results {
-				if len(r.RawV2) > 0 {
-					actual[string(r.RawV2)] = struct{}{}
-				} else {
-					actual[string(r.Raw)] = struct{}{}
-				}
-			}
-			expected := make(map[string]struct{}, len(test.want))
-			for _, v := range test.want {
-				expected[v] = struct{}{}
-			}
-
-			if diff := cmp.Diff(expected, actual); diff != "" {
-				t.Errorf("%s diff: (-want +got)\n%s", test.name, diff)
-			}
-		})
-	}
-}
 
 type testCase struct {
 	Input    string
@@ -155,10 +72,10 @@ configs = {"fs.azure.account.auth.type": "OAuth"`,
 			Input:    ` "AZUREAD-AKS-APPID-SECRET": "8w__IGsaY.6g6jUxb1.pPGK262._pgX.q-",`,
 			Expected: map[string]struct{}{"8w__IGsaY.6g6jUxb1.pPGK262._pgX.q-": {}},
 		},
-		//"client_secret6": {
+		// "client_secret6": {
 		//	Input:    ``,
 		//	Expected: map[string]struct{}{"": {}},
-		//},
+		// },
 
 		"password": {
 			Input: `# Login using Service Principal
@@ -175,12 +92,12 @@ $Credential = New-Object -TypeName System.Management.Automation.PSCredential -Ar
 az login --service-principal --username {{http://azure-cli-service-principal}} --password {{secret}} --tenant {{someone.onmicrosoft.com}}`,
 			Expected: nil,
 		},
-		//"client_secret3": {
+		// "client_secret3": {
 		//	Input: ``,
 		//	Expected: map[string]struct{}{
 		//		"": {},
 		//	},
-		//},
+		// },
 	}
 
 	for name, test := range cases {
