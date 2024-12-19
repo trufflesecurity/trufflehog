11e7906eb68785aaec845bc32b2414a3fa189ff4
Cody Rose <webmaster@codyro.se>
2024-12-19 13:32:13 -0800

    add comments

--- pkg/verificationcaching/metrics_reporter.go
+++ pkg/verificationcaching/metrics_reporter.go
@@ -1,10 +1,25 @@
 package verificationcaching
 
-// MetricsReporter is an interface used by a verification cache to report various metrics on its operation.
+// MetricsReporter is an interface used by a verification cache to report various metrics related to its operation.
 type MetricsReporter interface {
+	// AddCredentialVerificationsSaved records "saved" verification attempts, which is when credential verification
+	// status is loaded from the cache instead of retrieved from a remote verification endpoint. This number might not
+	// match the number of cache hits due to hit "wasting"; see AddResultCacheHitsWasted for more information.
 	AddCredentialVerificationsSaved(count int)
+
+	// AddFromDataVerifyTimeSpent records wall time spent in calls to FromData with verify = true.
 	AddFromDataVerifyTimeSpent(ms int64)
+
+	// AddResultCacheHits records result cache hits. Not all cache hits result in elided remote verification requests
+	// due to cache hit "wasting"; see AddResultCacheHitsWasted for more information.
 	AddResultCacheHits(count int)
+
+	// AddResultCacheMisses records result cache misses.
 	AddResultCacheMisses(count int)
+
+	// AddResultCacheHitsWasted records "wasted" result cache hits. A "wasted" result cache hit is a result cache hit
+	// that does not elide a remote verification request because there are other secret findings in the relevant chunk
+	// that are not cached. When this happens, the detector's FromData method must be called anyway, so the cache hit
+	// doesn't save any remote requests.
 	AddResultCacheHitsWasted(count int)
 }
