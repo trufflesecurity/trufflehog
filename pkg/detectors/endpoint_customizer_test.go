3ce9973c48217cc13012d5a83e43d91d7e731360
kashif khan <kashif.khan@trufflesec.com>
2024-12-30 13:10:22 +0500

    added negative test cases

--- pkg/detectors/endpoint_customizer_test.go
+++ pkg/detectors/endpoint_customizer_test.go
@@ -9,19 +9,28 @@ import (
 func TestEmbeddedEndpointSetter(t *testing.T) {
 	type Scanner struct{ EndpointSetter }
 	var s Scanner
-	// use foundendpoint to set the "baz" in endpoints
+	// set useFoundEndpoints to set the "baz" in endpoints
 	s.useFoundEndpoints = true
 	assert.Equal(t, []string{"baz"}, s.Endpoints("baz"))
 	// setting "foo" and "bar" as configured endpoint
 	assert.NoError(t, s.SetConfiguredEndpoints("foo", "bar"))
 	// return error as no endpoints are passed
 	assert.Error(t, s.SetConfiguredEndpoints())
-	// as use foundendpoint is true, "baz" will be added in the endpoints list
+	// as useFoundEndpoints is true, "baz" will be added in the endpoints list
 	assert.Equal(t, []string{"foo", "bar", "baz"}, s.Endpoints("baz"))
-	// setting cloudendpoints
+	// setting cloudEndpoint along with setting useCloudEndpoint as true
 	s.useCloudEndpoint = true
-	// cloudendpoint must be set in order to be added in the list along with useCloudEndpoint as true
+	// cloudEndpoint must be set in order to be added in the list along with useCloudEndpoint as true
 	s.cloudEndpoint = "test"
 	// reason for "foo" and "bar" here is that they are already configured
 	assert.Equal(t, []string{"foo", "bar", "test"}, s.Endpoints())
+	// set useFoundEndpoints and useCloudEndpoint to false
+	s.useFoundEndpoints = false
+	s.useCloudEndpoint = false
+	// as both useFoundEndpoints and useCloudEndpoint are set to false, passing any endpoint to Endpoints() will not be added
+	assert.Equal(t, []string{"foo", "bar"}, s.Endpoints("test"))
+	// set any new endpoint as cloudEndpoint
+	s.cloudEndpoint = "new"
+	// this time it will not be added in list because useCloudEndpoint is set to false
+	assert.Equal(t, []string{"foo", "bar"}, s.Endpoints())
 }
