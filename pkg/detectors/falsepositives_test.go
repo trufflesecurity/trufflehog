d0f3f32f19ff2e81cf466ae1a7d495d4c0640a03
kashif khan <kashif.khan@trufflesec.com>
2024-12-31 11:42:12 +0500

    ahrav is legend

--- pkg/detectors/falsepositives_test.go
+++ pkg/detectors/falsepositives_test.go
@@ -3,7 +3,6 @@ package detectors
 import (
 	"context"
 	_ "embed"
-	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -174,9 +173,11 @@ func TestStringShannonEntropy(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := roundToTwoDigits(StringShannonEntropy(tt.args.input))
-			if got != tt.want {
-				t.Errorf("StringShannonEntropy() = %v, want %v", got, tt.want)
+			got := StringShannonEntropy(tt.args.input)
+			if len(tt.args.input) > 0 && tt.want != 0 {
+				assert.InEpsilon(t, tt.want, got, 0.1)
+			} else {
+				assert.Equal(t, tt.want, got)
 			}
 		})
 	}
@@ -187,9 +188,4 @@ func BenchmarkDefaultIsKnownFalsePositive(b *testing.B) {
 		// Use a string that won't be found in any dictionary for the worst case check.
 		IsKnownFalsePositive("aoeuaoeuaoeuaoeuaoeuaoeu", DefaultFalsePositives, true)
 	}
-}
-
-func roundToTwoDigits(number float64) float64 {
-	scale := math.Pow(10, float64(2))
-	return math.Round(number*scale) / scale
 }
