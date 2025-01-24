70fa772bd3b4d84066004b6e524a1d659ed9f14f
Abdul Basit <basit.mussani@gmail.com>
2025-01-24 11:50:29 +0500

    Impl analyzePermission method for PrivateKey analyzer. export few private functions for reusability in analyzers

--- pkg/analyzer/analyzers/privatekey/privatekey.go
+++ pkg/analyzer/analyzers/privatekey/privatekey.go
@@ -3,6 +3,8 @@ package privatekey
 import (
 	"errors"
 	"fmt"
+	"strings"
+	"sync"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/analyzer/analyzers"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/analyzer/config"
@@ -33,14 +35,34 @@ func (a Analyzer) Analyze(ctx context.Context, credInfo map[string]string) (*ana
 	return secretInfoToAnalyzerResult(info), nil
 }
 
-type SecretInfo struct{}
+type SecretInfo struct {
+	TLSCertificateResult *privatekey.DriftwoodResult
+	GithubUsername       *string
+	GitlabUsername       *string
+}
 
 func AnalyzePermissions(ctx context.Context, cfg *config.Config, key string) (*SecretInfo, error) {
-	key = privatekey.Normalize(key)
+	token := privatekey.Normalize(key)
+	if len(token) < 64 {
+		return nil, fmt.Errorf("invalid token")
+	}
 
-	parsedKey, err := ssh.ParseRawPrivateKey([]byte(key))
-	if err != nil {
-		// TODO: check if the key is encrypted
+	var (
+		wg                 sync.WaitGroup
+		parsedKey          any
+		err                error
+		verificationErrors = privatekey.NewVerificationErrors(3)
+		info               = &SecretInfo{}
+	)
+
+	parsedKey, err = ssh.ParseRawPrivateKey([]byte(token))
+	if err != nil && strings.Contains(err.Error(), "private key is passphrase protected") {
+		// key is password protected
+		parsedKey, _, err = privatekey.Crack([]byte(token))
+		if err != nil {
+			return nil, err
+		}
+	} else if err != nil {
 		return nil, err
 	}
 
@@ -49,14 +71,48 @@ func AnalyzePermissions(ctx context.Context, cfg *config.Config, key string) (*S
 		return nil, err
 	}
 
-	result, err := privatekey.LookupFingerprint(ctx, fingerprint, true)
-	if err != nil {
-		return nil, err
+	// Look up certificate information.
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		data, err := privatekey.LookupFingerprint(ctx, fingerprint)
+		if err != nil {
+			verificationErrors.Add(err)
+		} else {
+			info.TLSCertificateResult = data
+		}
+	}()
+
+	// Test SSH key against github.com
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		user, err := privatekey.VerifyGitHubUser(ctx, parsedKey)
+		if err != nil {
+			verificationErrors.Add(err)
+		} else if user != nil {
+			info.GithubUsername = user
+		}
+	}()
+
+	// Test SSH key against gitlab.com
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		user, err := privatekey.VerifyGitLabUser(ctx, parsedKey)
+		if err != nil {
+			verificationErrors.Add(err)
+		} else if user != nil {
+			info.GitlabUsername = user
+		}
+	}()
+	wg.Wait()
+
+	if len(verificationErrors.Errors) > 0 {
+		return nil, fmt.Errorf("verification failures: %s", strings.Join(verificationErrors.Errors, ", "))
 	}
 
-	fmt.Printf("%v\n", result)
-
-	return nil, nil
+	return info, nil
 }
 
 func AnalyzeAndPrintPermissions(cfg *config.Config, key string) {
