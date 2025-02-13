0bec4f2ccd8524e0804070ca38c86cc7df59baf1
Cody Rose <webmaster@codyro.se>
2025-02-13 16:18:17 -0500

    add protos for github realtime

--- pkg/pb/sourcespb/sources.pb.validate.go
+++ pkg/pb/sourcespb/sources.pb.validate.go
@@ -2890,6 +2890,110 @@ var _ interface {
 	ErrorName() string
 } = GitHubExperimentalValidationError{}
 
+// Validate checks the field values on GitHubRealtime with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *GitHubRealtime) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on GitHubRealtime with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in GitHubRealtimeMultiError,
+// or nil if none found.
+func (m *GitHubRealtime) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *GitHubRealtime) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for ListenerPort
+
+	// no validation rules for WebhookSecret
+
+	if len(errors) > 0 {
+		return GitHubRealtimeMultiError(errors)
+	}
+
+	return nil
+}
+
+// GitHubRealtimeMultiError is an error wrapping multiple validation errors
+// returned by GitHubRealtime.ValidateAll() if the designated constraints
+// aren't met.
+type GitHubRealtimeMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m GitHubRealtimeMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m GitHubRealtimeMultiError) AllErrors() []error { return m }
+
+// GitHubRealtimeValidationError is the validation error returned by
+// GitHubRealtime.Validate if the designated constraints aren't met.
+type GitHubRealtimeValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e GitHubRealtimeValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e GitHubRealtimeValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e GitHubRealtimeValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e GitHubRealtimeValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e GitHubRealtimeValidationError) ErrorName() string { return "GitHubRealtimeValidationError" }
+
+// Error satisfies the builtin error interface
+func (e GitHubRealtimeValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sGitHubRealtime.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = GitHubRealtimeValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = GitHubRealtimeValidationError{}
+
 // Validate checks the field values on GoogleDrive with the rules defined in
 // the proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
