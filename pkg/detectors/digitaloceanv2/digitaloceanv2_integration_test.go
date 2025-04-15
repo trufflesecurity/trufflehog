85dd3bb3357eedae370dff82d847af6e6033d852
Nabeel <nabeelalam811@gmail.com>
2025-04-15 19:02:53 +0500

    updated unexpected error test case name

--- pkg/detectors/digitaloceanv2/digitaloceanv2_integration_test.go
+++ pkg/detectors/digitaloceanv2/digitaloceanv2_integration_test.go
@@ -84,7 +84,7 @@ func TestDigitalOceanV2_FromChunk(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "found, verified but unexpected api surface",
+			name: "found verifiable secret, verification failed due to unexpected API response",
 			s:    Scanner{client: common.ConstantResponseHttpClient(404, "")},
 			args: args{
 				ctx:    context.Background(),
