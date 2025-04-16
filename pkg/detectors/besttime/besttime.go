c40787fa159cebd4ec8df742cbc48e4d09d3082e
Kashif Khan <70996046+kashifkhan0771@users.noreply.github.com>
2025-04-16 13:55:13 +0500

    Added boundary character

--- pkg/detectors/besttime/besttime.go
+++ pkg/detectors/besttime/besttime.go
@@ -22,7 +22,7 @@ var (
 	client = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
-	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"besttime"}) + `(pri_[a-f0-9]{32})\b`)
+	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"besttime"}) + `\b(pri_[a-f0-9]{32})\b`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
