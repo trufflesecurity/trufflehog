1255b52eac6e8c4d1baa14a18efc76bce73f9631
kashif khan <kashif.khan@trufflesec.com>
2025-05-12 13:39:01 +0500

    removed print statement

--- pkg/analyzer/analyzers/databricks/databricks_test.go
+++ pkg/analyzer/analyzers/databricks/databricks_test.go
@@ -3,7 +3,6 @@ package databricks
 import (
 	_ "embed"
 	"encoding/json"
-	"fmt"
 	"sort"
 	"testing"
 	"time"
@@ -61,8 +60,6 @@ func TestAnalyzer_Analyze(t *testing.T) {
 			if err != nil {
 				t.Fatalf("could not marshal got to JSON: %s", err)
 			}
-
-			fmt.Println(string(gotJSON))
 
 			// Parse the expected JSON string
 			var wantObj analyzers.AnalyzerResult
