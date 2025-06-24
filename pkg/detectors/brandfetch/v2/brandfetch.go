228a92a10562c09d4e3dd1cf55f92a67a9c5a898
Sahil Silare <sahilsilare@gmail.com>
2025-06-24 23:45:24 +0530

    refactor: added abstraction
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

--- pkg/detectors/brandfetch/v2/brandfetch.go
+++ pkg/detectors/brandfetch/v2/brandfetch.go
@@ -3,7 +3,6 @@ package brandfetch
 import (
 	"context"
 	"fmt"
-	"io"
 	"net/http"
 	"strings"
 
@@ -14,16 +13,19 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
 )
 
-type Scanner struct{}
+type Scanner struct {
+	client *http.Client // Optional: allow custom client
+}
+
+var defaultClient = common.SaneHttpClient()
 
 func (s Scanner) Version() int { return 2 }
 
 // Ensure the Scanner satisfies the interface at compile time.
 var _ detectors.Detector = (*Scanner)(nil)
 var _ detectors.Versioner = (*Scanner)(nil)
-var (
-	client = common.SaneHttpClient()
 
+var (
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
 	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"brandfetch"}) + `\b([a-zA-Z0-9=+/\-_!@#$%^&*()]{44})`)
 )
@@ -37,21 +39,28 @@ func (s Scanner) Keywords() []string {
 // FromData will find and optionally verify Brandfetch secrets in a given set of bytes.
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
+	uniqueMatches := make(map[string]struct{})
+	for _, match := range keyPat.FindAllStringSubmatch(dataStr, -1) {
+		if len(match) == 2 {
+			uniqueMatches[strings.TrimSpace(match[1])] = struct{}{}
+		}
+	}
 
-	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
-
-	for _, match := range matches {
-		resMatch := strings.TrimSpace(match[1])
-
+	for match := range uniqueMatches {
 		s1 := detectors.Result{
 			DetectorType: detectorspb.DetectorType_Brandfetch,
-			Raw:          []byte(resMatch),
+			Raw:          []byte(match),
 		}
 
 		if verify {
-			isVerified, verificationErr := verifyBrandFetch(ctx, client, resMatch)
+			client := s.client
+			if client == nil {
+				client = defaultClient
+			}
+			isVerified, extraData, verificationErr := verifyMatch(ctx, client, match)
 			s1.Verified = isVerified
-			s1.SetVerificationError(verificationErr)
+			s1.ExtraData = extraData
+			s1.SetVerificationError(verificationErr, match)
 		}
 
 		results = append(results, s1)
@@ -60,44 +69,33 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 	return results, nil
 }
 
+func verifyMatch(ctx context.Context, client *http.Client, token string) (bool, map[string]string, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://api.brandfetch.io/v2/brands/google.com", nil)
+	if err != nil {
+		return false, nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
+	res, err := client.Do(req)
+	if err != nil {
+		return false, nil, err
+	}
+	defer res.Body.Close()
+
+	switch res.StatusCode {
+	case http.StatusOK:
+		return true, nil, nil
+	case http.StatusUnauthorized:
+		return false, nil, nil
+	default:
+		return false, nil, fmt.Errorf("unexpected HTTP response status %d", res.StatusCode)
+	}
+}
+
 func (s Scanner) Type() detectorspb.DetectorType {
 	return detectorspb.DetectorType_Brandfetch
 }
 
 func (s Scanner) Description() string {
 	return "Brandfetch is a service that provides brand data, including logos, colors, fonts, and more. Brandfetch API keys can be used to access this data."
-}
-
-// docs: https://docs.brandfetch.com/docs/brand-api#overview
-func verifyBrandFetch(ctx context.Context, client *http.Client, key string) (bool, error) {
-	payload := strings.NewReader(`{
-		"domain": "www.example.com"
-		}`)
-
-	req, err := http.NewRequestWithContext(ctx, "POST", "https://api.brandfetch.io/v1/color", payload)
-	if err != nil {
-		return false, err
-	}
-
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("x-api-key", key)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return false, err
-	}
-
-	defer func() {
-		_, _ = io.Copy(io.Discard, resp.Body)
-		_ = resp.Body.Close()
-	}()
-
-	switch resp.StatusCode {
-	case http.StatusOK:
-		return true, nil
-	case http.StatusUnauthorized, http.StatusForbidden:
-		return false, nil
-	default:
-		return false, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-	}
 }
