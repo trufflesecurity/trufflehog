80189845c0e991038435b18820c02bb7b514de00
kashif khan <kashif.khan@trufflesec.com>
2025-07-17 12:38:46 +0500

    updated test cases

--- pkg/sources/circleci/circleci_test.go
+++ pkg/sources/circleci/circleci_test.go
@@ -15,7 +15,7 @@ import (
 )
 
 func TestSource_Scan(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10) // Increased timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
 	secret, err := common.GetSecret(ctx, "trufflehog-testing", "test")
