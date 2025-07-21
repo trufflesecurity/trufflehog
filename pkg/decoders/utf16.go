dc23e7155de1d481df4f3d2cd89b5d76af8f76f1
Joe Leon <joe.leon@trufflesec.com>
2025-07-21 13:58:25 -0400

    added UTF-16 BOM support

--- pkg/decoders/utf16.go
+++ pkg/decoders/utf16.go
@@ -34,8 +34,20 @@ func (d *UTF16) FromChunk(chunk *sources.Chunk) *DecodableChunk {
 
 // utf16ToUTF8 converts a byte slice containing UTF-16 encoded data to a UTF-8 encoded byte slice.
 func utf16ToUTF8(b []byte) ([]byte, error) {
+
+	if len(b) < 2 {
+		return []byte{}, nil
+	}
+
+	start := 0
+	if b[0] == 0xFF && b[1] == 0xFE {
+		start = 2
+	} else if b[0] == 0xFE && b[1] == 0xFF {
+		start = 2
+	}
+
 	var bufBE, bufLE bytes.Buffer
-	for i := 0; i < len(b)-1; i += 2 {
+	for i := start; i < len(b)-1; i += 2 {
 		if r := rune(binary.BigEndian.Uint16(b[i:])); b[i] == 0 && utf8.ValidRune(r) {
 			if isPrintableByte(byte(r)) {
 				bufBE.WriteRune(r)
