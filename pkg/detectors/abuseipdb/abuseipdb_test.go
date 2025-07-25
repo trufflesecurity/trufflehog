95a8e095b7725620cf2fd756fe89ac2f40b2269b
kashif khan <kashif.khan@trufflesec.com>
2025-07-25 16:32:23 +0500

    first commit

--- pkg/detectors/abuseipdb/abuseipdb_test.go
+++ pkg/detectors/abuseipdb/abuseipdb_test.go
@@ -2,18 +2,13 @@ package abuseipdb
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/engine/ahocorasick"
-)
-
-var (
-	validPattern   = "123abcdef456ghijkl789mnopqr012stuvwx3455123abcdef456ghijkl789mnopqr012stuvwx3455"
-	invalidPattern = "123abcdef456Ghijkl789mnopqr012stuvwx3455123abcdef456ghijkl789mnopqr012stuvwX"
 )
 
 func TestAbuseipdb_Pattern(t *testing.T) {
@@ -26,19 +21,32 @@ func TestAbuseipdb_Pattern(t *testing.T) {
 		want  []string
 	}{
 		{
-			name:  "valid pattern",
-			input: fmt.Sprintf("abuseipdb token = '%s'", validPattern),
-			want:  []string{validPattern},
+			name: "valid pattern",
+			input: `
+				[INFO] Sending request to abuseipdb API
+				[DEBUG] Using API_KEY=o8oqti3tghu2xic76ii4t7jb9bxuzd4200j1yrkdjl6s8834hx4dgz1wwo90diqraakjd13sljcjkfnf
+				[INFO] Response received: 200 OK
+			`,
+			want: []string{"o8oqti3tghu2xic76ii4t7jb9bxuzd4200j1yrkdjl6s8834hx4dgz1wwo90diqraakjd13sljcjkfnf"},
 		},
 		{
-			name:  "valid pattern - out of prefix range",
-			input: fmt.Sprintf("abuseipdb token keyword is not close to the real token = '%s'", validPattern),
-			want:  nil,
+			name: "valid pattern - out of prefix range",
+			input: `
+				[INFO] Sending request to abuseipdb API
+				[INFO] Processing request
+				[Info] Response received: 200 OK
+				[DEBUG] Used API_KEY=o8oqti3tghu2xic76ii4t7jb9bxuzd4200j1yrkdjl6s8834hx4dgz1wwo90diqraakjd13sljcjkfnf
+			`,
+			want: nil,
 		},
 		{
-			name:  "invalid pattern",
-			input: fmt.Sprintf("abuseipdb = '%s'", invalidPattern),
-			want:  nil,
+			name: "invalid pattern",
+			input: `
+				[INFO] Sending request to abuseipdb API
+				[DEBUG] Using API_KEY=7e4abcdef456Ghijkl789mnopqr012stuvwx3455123abcdef456ghijkl789mnopqr012stuvwX
+				[ERROR] Response received: 400 BadRequest
+			`,
+			want: nil,
 		},
 	}
 
@@ -46,22 +54,15 @@ func TestAbuseipdb_Pattern(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			matchedDetectors := ahoCorasickCore.FindDetectorMatches([]byte(test.input))
 			if len(matchedDetectors) == 0 {
-				t.Errorf("keywords '%v' not matched by: %s", d.Keywords(), test.input)
+				t.Errorf("test %q failed: expected keywords %v to be found in the input", test.name, d.Keywords())
 				return
 			}
 
 			results, err := d.FromData(context.Background(), false, []byte(test.input))
-			if err != nil {
-				t.Errorf("error = %v", err)
-				return
-			}
+			require.NoError(t, err)
 
 			if len(results) != len(test.want) {
-				if len(results) == 0 {
-					t.Errorf("did not receive result")
-				} else {
-					t.Errorf("expected %d results, only received %d", len(test.want), len(results))
-				}
+				t.Errorf("mismatch in result count: expected %d, got %d", len(test.want), len(results))
 				return
 			}
 
@@ -73,6 +74,7 @@ func TestAbuseipdb_Pattern(t *testing.T) {
 					actual[string(r.Raw)] = struct{}{}
 				}
 			}
+
 			expected := make(map[string]struct{}, len(test.want))
 			for _, v := range test.want {
 				expected[v] = struct{}{}
--- pkg/detectors/aha/aha_test.go
+++ pkg/detectors/aha/aha_test.go
@@ -2,27 +2,18 @@ package aha
 
 import (
 	"context"
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/engine/ahocorasick"
 )
 
-var (
-	validPattern   = "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff/example.aha.io"
-	invalidPattern = "00112233445566778899aabbCC$%eeff00112233445566778899aabbccddeeff/example.fake.io"
-)
-
 func TestAha_Pattern(t *testing.T) {
 	d := Scanner{}
 	ahoCorasickCore := ahocorasick.NewAhoCorasickCore([]detectors.Detector{d})
-
-	key := strings.Split(validPattern, "/")[0]
-	url := strings.Split(validPattern, "/")[1]
 
 	tests := []struct {
 		name  string
@@ -30,34 +21,51 @@ func TestAha_Pattern(t *testing.T) {
 		want  []string
 	}{
 		{
-			name:  "valid pattern",
-			input: fmt.Sprintf("aha.io = '%s'", validPattern),
-			want:  []string{key},
+			name: "valid pattern",
+			input: `
+				[INFO] Sending request to the truffle-security.aha.io API
+				[DEBUG] Using Key=b2fb683b321da0f90aa23decc45c74a8578f2eb4390af155d3277e53362a8b45
+				[INFO] Response received: 200 OK
+			`,
+			want: []string{"b2fb683b321da0f90aa23decc45c74a8578f2eb4390af155d3277e53362a8b45"},
 		},
 		{
-			name:  "valid pattern - detect URL far away from keyword",
-			input: fmt.Sprintf("aha.io = '%s\n URL is not close to the keyword but should be detected %s'", key, url),
-			want:  []string{key},
+			name: "valid pattern",
+			input: `
+				[DEBUG] Using truffle-security.aha.io domain
+				[INFO] Sending request to the API
+				[DEBUG] Using Key=b2fb683b321da0f90aa23decc45c74a8578f2eb4390af155d3277e53362a8b45
+				[INFO] Response received: 200 OK
+			`,
+			want: nil,
 		},
 		{
-			name:  "valid pattern - key out of prefix range",
-			input: fmt.Sprintf("aha.io keyword is not close to the real key and secret = '%s'", validPattern),
-			want:  nil,
+			name: "valid pattern - only key",
+			input: `
+				[INFO] Sending request to the aha.io API
+				[DEBUG] Using Key=b2fb683b321da0f90aa23decc45c74a8578f2eb4390af155d3277e53362a8b45
+				[INFO] Response received: 200 OK
+			`,
+			want: []string{"b2fb683b321da0f90aa23decc45c74a8578f2eb4390af155d3277e53362a8b45"},
 		},
 		{
-			name:  "valid pattern - only key",
-			input: fmt.Sprintf("aha.io %s", key),
-			want:  []string{key},
+			name: "valid pattern - only URL",
+			input: `
+				[DEBUG] Using truffle-security.aha.io domain
+				[INFO] Sending request to the API
+				[INFO] Response received: 200 OK
+			`,
+			want: nil,
 		},
 		{
-			name:  "valid pattern - only URL",
-			input: fmt.Sprintf("aha.io %s", url),
-			want:  nil,
-		},
-		{
-			name:  "invalid pattern",
-			input: fmt.Sprintf("aha.io %s", invalidPattern),
-			want:  nil,
+			name: "invalid pattern",
+			input: `
+				[DEBUG] Using truffle-security.aha.io domain
+				[INFO] Sending request to the API
+				[DEBUG] Using Key=b2fb683b321da0f90aa23decc4ic74a8578f2eb4390af155d3277e53362a8b45
+				[ERROR] Response received: 400 BadRequest
+			`,
+			want: nil,
 		},
 	}
 
@@ -65,22 +73,15 @@ func TestAha_Pattern(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			matchedDetectors := ahoCorasickCore.FindDetectorMatches([]byte(test.input))
 			if len(matchedDetectors) == 0 {
-				t.Errorf("keywords '%v' not matched by: %s", d.Keywords(), test.input)
+				t.Errorf("test %q failed: expected keywords %v to be found in the input", test.name, d.Keywords())
 				return
 			}
 
 			results, err := d.FromData(context.Background(), false, []byte(test.input))
-			if err != nil {
-				t.Errorf("error = %v", err)
-				return
-			}
+			require.NoError(t, err)
 
 			if len(results) != len(test.want) {
-				if len(results) == 0 {
-					t.Errorf("did not receive result")
-				} else {
-					t.Errorf("expected %d results, only received %d", len(test.want), len(results))
-				}
+				t.Errorf("mismatch in result count: expected %d, got %d", len(test.want), len(results))
 				return
 			}
 
@@ -92,6 +93,7 @@ func TestAha_Pattern(t *testing.T) {
 					actual[string(r.Raw)] = struct{}{}
 				}
 			}
+
 			expected := make(map[string]struct{}, len(test.want))
 			for _, v := range test.want {
 				expected[v] = struct{}{}
