7a26c6f3c57fe0fbcbe51b8bbc5c6681585aaef1
Jeff Rowell <j3ff.rowell@gmail.com>
2025-07-27 06:47:46 -0600

    updated make.com api_token keywords

--- pkg/detectors/make/api_token/api_token.go
+++ pkg/detectors/make/api_token/api_token.go
@@ -29,7 +29,7 @@ var (
 // Keywords are used for efficiently pre-filtering chunks.
 // Use identifiers in the secret preferably, or the provider name.
 func (s Scanner) Keywords() []string {
-	return []string{"make", "token", "api"}
+	return []string{"make"}
 }
 
 // FromData will find and optionally verify Make secrets in a given set of bytes.
