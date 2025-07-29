bf796e1895b7121e4f054961db3a543451c73b20
Aman Ullah <aman.ullah.jalal@trufflesec.com>
2025-07-29 16:57:15 +0500

    Changed verification URL modified status code handling and added comments fixed GCP cred access in integration tests

--- pkg/detectors/rootly/rootly.go
+++ pkg/detectors/rootly/rootly.go
@@ -3,13 +3,14 @@ package rootly
 import (
 	"context"
 	"net/http"
+
 	// "strings"
 	"fmt"
 
-	regexp "github.com/wasilibs/go-re2"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+	regexp "github.com/wasilibs/go-re2"
 )
 
 type Scanner struct{}
@@ -32,52 +33,57 @@ func (s Scanner) Keywords() []string {
 
 // FromData will find and optionally verify Rootly secrets in a given set of bytes.
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
-    dataStr := string(data)
-    uniqueMatches := make(map[string]struct{})
-    for _, match := range keyPat.FindAllStringSubmatch(dataStr, -1) {
-        if len(match) == 1 {
-            uniqueMatches[match[0]] = struct{}{}
-        }
-    }
+	dataStr := string(data)
+	uniqueMatches := make(map[string]struct{})
+	for _, match := range keyPat.FindAllStringSubmatch(dataStr, -1) {
+		if len(match) == 1 {
+			uniqueMatches[match[0]] = struct{}{}
+		}
+	}
 
-    for match := range uniqueMatches {
-        s1 := detectors.Result{
-            DetectorType: detectorspb.DetectorType_Rootly,
-            Raw:          []byte(match),
-        }
+	for match := range uniqueMatches {
+		s1 := detectors.Result{
+			DetectorType: detectorspb.DetectorType_Rootly,
+			Raw:          []byte(match),
+		}
 
-        if verify {
-            isVerified, verificationErr := verifyMatch(ctx, client, match)
-            s1.Verified = isVerified
-            s1.SetVerificationError(verificationErr, match)
-        }
+		if verify {
+			isVerified, verificationErr := verifyMatch(ctx, client, match)
+			s1.Verified = isVerified
+			s1.SetVerificationError(verificationErr, match)
+		}
 
-        results = append(results, s1)
-    }
+		results = append(results, s1)
+	}
 
-    return results, nil
+	return results, nil
 }
 
 func verifyMatch(ctx context.Context, client *http.Client, token string) (bool, error) {
-    req, err := http.NewRequestWithContext(ctx, "GET", "https://api.rootly.com/v1/users/me", nil)
-    if err != nil {
-        return false, err
-    }
-    req.Header.Add("Authorization", "Bearer "+token)
-    res, err := client.Do(req)
-    if err != nil {
-        return false, err
-    }
-    defer res.Body.Close()
 
-    switch res.StatusCode {
-    case http.StatusOK:
-        return true, nil
-    case http.StatusUnauthorized:
-        return false, nil
-    default:
-        return false, fmt.Errorf("unexpected HTTP response status %d", res.StatusCode)
-    }
+	// this endpoint returns 200 if results exist and 404 if incidents do not exist or that token is not authorized
+	// considering both 200 and 404 as positive results i.e. the token is valid
+	// /user/me endpoint does not verify Team and Global API Keys returning 422 error. (There are 3 types of API keys in Rootly, Global, Team and Personal)
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://api.rootly.com/v1/incidents", nil)
+	if err != nil {
+		return false, err
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	res, err := client.Do(req)
+
+	if err != nil {
+		return false, err
+	}
+	defer res.Body.Close()
+
+	switch res.StatusCode {
+	case http.StatusOK, http.StatusNotFound:
+		return true, nil
+	case http.StatusUnauthorized:
+		return false, nil
+	default:
+		return false, fmt.Errorf("unexpected HTTP response status %d", res.StatusCode)
+	}
 }
 
 func (s Scanner) Type() detectorspb.DetectorType {
