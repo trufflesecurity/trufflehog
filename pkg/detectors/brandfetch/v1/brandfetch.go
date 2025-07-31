ca8cb4a3cd654ef9d37a959949f4e4d09b7717fe
Shahzad Haider <shahzadhaider.se@gmail.com>
2025-07-31 20:36:29 +0500

    fixed all issues in v1

--- pkg/detectors/brandfetch/v1/brandfetch.go
+++ pkg/detectors/brandfetch/v1/brandfetch.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
 
 	regexp "github.com/wasilibs/go-re2"
@@ -14,15 +15,17 @@ import (
 	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
 )
 
-type Scanner struct{}
+type Scanner struct {
+	client *http.Client // Optional: allow custom client
+}
 
 func (s Scanner) Version() int { return 1 }
 
-// Ensure the Scanner satisfies the interface at compile time.
-var _ detectors.Detector = (*Scanner)(nil)
-var _ detectors.Versioner = (*Scanner)(nil)
 var (
-	client = common.SaneHttpClient()
+	// Ensure the Scanner satisfies the interface at compile time.
+	_             detectors.Detector  = (*Scanner)(nil)
+	_             detectors.Versioner = (*Scanner)(nil)
+	defaultClient                     = common.SaneHttpClient()
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
 	keyPat = regexp.MustCompile(detectors.PrefixRegex([]string{"brandfetch"}) + `\b([0-9A-Za-z]{40})\b`)
@@ -34,26 +37,42 @@ func (s Scanner) Keywords() []string {
 	return []string{"brandfetch"}
 }
 
+func (s Scanner) getClient() *http.Client {
+	if s.client != nil {
+		return s.client
+	}
+
+	return defaultClient
+}
+
 // FromData will find and optionally verify Brandfetch secrets in a given set of bytes.
 func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (results []detectors.Result, err error) {
 	dataStr := string(data)
-	matches := keyPat.FindAllStringSubmatch(dataStr, -1)
-	for _, match := range matches {
-		resMatch := strings.TrimSpace(match[1])
+
+	uniqueTokenMatches := make(map[string]struct{})
+	for _, match := range keyPat.FindAllStringSubmatch(dataStr, -1) {
+		uniqueTokenMatches[match[1]] = struct{}{}
+	}
+
+	for match := range uniqueTokenMatches {
 		s1 := detectors.Result{
 			DetectorType: detectorspb.DetectorType_Brandfetch,
-			Raw:          []byte(resMatch),
+			Raw:          []byte(match),
+			ExtraData:    map[string]string{"version": strconv.Itoa(s.Version())},
 		}
 
 		if verify {
-			isVerified, verificationErr := verifyBrandFetch(ctx, client, resMatch)
+			isVerified, verificationErr := verifyBrandFetch(ctx, s.getClient(), match)
 			s1.Verified = isVerified
 			s1.SetVerificationError(verificationErr)
 		}
+
 		results = append(results, s1)
 	}
-	return results, nil
+
+	return
 }
+
 func (s Scanner) Type() detectorspb.DetectorType {
 	return detectorspb.DetectorType_Brandfetch
 }
