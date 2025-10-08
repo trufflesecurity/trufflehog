f39a6d8b74749f47c0d1ad49eb47675f2a485d64
Mustansir Muzaffar <mustansir2001@gmail.com>
2025-10-08 17:18:40 +0500

    switch from tabular tests to separate tests

--- pkg/detectors/atlassian/v2/atlassian_test.go
+++ pkg/detectors/atlassian/v2/atlassian_test.go
@@ -2,10 +2,12 @@ package atlassian
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
@@ -83,96 +85,79 @@ func TestAtlassian_Pattern(t *testing.T) {
 	}
 }
 
-func TestAtlassian_AnalysisInfo(t *testing.T) {
+// TestAtlassian_AnalysisInfo_KeyAndOrgId tests if both the key and organization id are populated into AnalysisInfo
+// given that they are present in the input data chunk
+func TestAtlassian_AnalysisInfo_KeyAndOrgId(t *testing.T) {
 	client := common.SaneHttpClient()
 	d := Scanner{client: client}
-	ahoCorasickCore := ahocorasick.NewAhoCorasickCore([]detectors.Detector{d})
-	tests := []struct {
-		name      string
-		input     string
-		gockSetup func()
-		want      []map[string]string
-	}{
-		{
-			name: "key and organization_id both present",
-			input: `
-				[INFO] Sending request to the atlassian API
-				[DEBUG] Using Key=ATCTT3xFfGN0GsZNgOGrQSHSnxiJVi00oHlRicyM0yMNuKCBfw6qOHVcCy4Hm89GnclGb_W-1qAkxqCn5XbuyoX54bNhpK5yFKGFR7ocV6FByvL_P9Sb3tFnbUg3T3I3S_RGCBLMSN7Nsa4GJv8JEJ6bzvDmX-oJ8AnrazMU-zZ5hb-u3t2ERew=366BFE3A
-				[DEBUG] Using Organization ID=123j4567-e89b-12d3-a456-426614174000
-				[INFO] Response received: 200 OK
-				`,
-			gockSetup: func() {
-				gock.New("https://api.atlassian.com").
-					Get("/admin/v1/orgs").
-					MatchHeader("Accept", "application/json").
-					MatchHeader("Authorization", "Bearer ATCTT3xFfGN0GsZNgOGrQSHSnxiJVi00oHlRicyM0yMNuKCBfw6qOHVcCy4Hm89GnclGb_W-1qAkxqCn5XbuyoX54bNhpK5yFKGFR7ocV6FByvL_P9Sb3tFnbUg3T3I3S_RGCBLMSN7Nsa4GJv8JEJ6bzvDmX-oJ8AnrazMU-zZ5hb-u3t2ERew=366BFE3A").
-					Reply(http.StatusOK).
-					JSON(map[string]any{
-						"Data": []map[string]any{},
-					})
-			},
-			want: []map[string]string{
-				{
-					"key":             "ATCTT3xFfGN0GsZNgOGrQSHSnxiJVi00oHlRicyM0yMNuKCBfw6qOHVcCy4Hm89GnclGb_W-1qAkxqCn5XbuyoX54bNhpK5yFKGFR7ocV6FByvL_P9Sb3tFnbUg3T3I3S_RGCBLMSN7Nsa4GJv8JEJ6bzvDmX-oJ8AnrazMU-zZ5hb-u3t2ERew=366BFE3A",
-					"organization_id": "123j4567-e89b-12d3-a456-426614174000",
-				},
-			},
-		},
-		{
-			name: "key only",
-			input: `
-				[INFO] Sending request to the atlassian API
-				[DEBUG] Using Key=ATCTT3xFfGN0GsZNgOGrQSHSnxiJVi00oHlRicyM0yMNuKCBfw6qOHVcCy4Hm89GnclGb_W-1qAkxqCn5XbuyoX54bNhpK5yFKGFR7ocV6FByvL_P9Sb3tFnbUg3T3I3S_RGCBLMSN7Nsa4GJv8JEJ6bzvDmX-oJ8AnrazMU-zZ5hb-u3t2ERew=366BFE3A
-				[INFO] Response received: 200 OK
-				`,
-			gockSetup: func() {
-				gock.New("https://api.atlassian.com").
-					Get("/admin/v1/orgs").
-					MatchHeader("Accept", "application/json").
-					MatchHeader("Authorization", "Bearer ATCTT3xFfGN0GsZNgOGrQSHSnxiJVi00oHlRicyM0yMNuKCBfw6qOHVcCy4Hm89GnclGb_W-1qAkxqCn5XbuyoX54bNhpK5yFKGFR7ocV6FByvL_P9Sb3tFnbUg3T3I3S_RGCBLMSN7Nsa4GJv8JEJ6bzvDmX-oJ8AnrazMU-zZ5hb-u3t2ERew=366BFE3A").
-					Reply(http.StatusOK).
-					JSON(map[string]any{
-						"Data": []map[string]any{},
-					})
-			},
-			want: []map[string]string{
-				{
-					"key": "ATCTT3xFfGN0GsZNgOGrQSHSnxiJVi00oHlRicyM0yMNuKCBfw6qOHVcCy4Hm89GnclGb_W-1qAkxqCn5XbuyoX54bNhpK5yFKGFR7ocV6FByvL_P9Sb3tFnbUg3T3I3S_RGCBLMSN7Nsa4GJv8JEJ6bzvDmX-oJ8AnrazMU-zZ5hb-u3t2ERew=366BFE3A",
-				},
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			defer gock.Off()
-			defer gock.RestoreClient(client)
-			gock.InterceptClient(client)
-			test.gockSetup()
 
-			matchedDetectors := ahoCorasickCore.FindDetectorMatches([]byte(test.input))
-			if len(matchedDetectors) == 0 {
-				t.Errorf("test %q failed: expected keywords %v to be found in the input", test.name, d.Keywords())
-				return
-			}
+	key := "ATCTT3xFfGN0GsZNgOGrQSHSnxiJVi00oHlRicyM0yMNuKCBfw6qOHVcCy4Hm89GnclGb_W-1qAkxqCn5XbuyoX54bNhpK5yFKGFR7ocV6FByvL_P9Sb3tFnbUg3T3I3S_RGCBLMSN7Nsa4GJv8JEJ6bzvDmX-oJ8AnrazMU-zZ5hb-u3t2ERew=366BFE3A"
+	orgId := "123j4567-e89b-12d3-a456-426614174000"
 
-			results, err := d.FromData(context.Background(), true, []byte(test.input))
-			require.NoError(t, err)
+	t.Run("key and organization id both present", func(t *testing.T) {
+		defer gock.Off()
+		defer gock.RestoreClient(client)
+		gock.InterceptClient(client)
+		gock.New("https://api.atlassian.com").
+			Get("/admin/v1/orgs").
+			MatchHeader("Accept", "application/json").
+			MatchHeader("Authorization", fmt.Sprintf("Bearer %s", key)).
+			Reply(http.StatusOK).
+			JSON(map[string]any{
+				"Data": []map[string]any{},
+			})
 
-			if len(results) != len(test.want) {
-				t.Errorf("mismatch in result count: expected %d, got %d", len(test.want), len(results))
-				return
-			}
+		input := fmt.Sprintf(`
+		[INFO] Sending request to the atlassian API
+		[DEBUG] Using Key=%s
+		[DEBUG] Using Organization ID=%s
+		[INFO] Response received: 200 OK
+		`, key, orgId)
 
-			for i, r := range results {
-				if r.AnalysisInfo == nil {
-					t.Errorf("expected analysis info to be populated")
-					return
-				}
+		results, err := d.FromData(context.Background(), true, []byte(input))
+		require.NoError(t, err)
+		require.Len(t, results, 1, "mismatch in result count: expected %d, got %d", 1, len(results))
+		result := results[0]
+		require.NotNil(t, result.AnalysisInfo, "AnalysisInfo is nil")
 
-				if diff := cmp.Diff(test.want[i], r.AnalysisInfo); diff != "" {
-					t.Errorf("%s diff: (-want +got)\n%s", test.name, diff)
-				}
-			}
-		})
-	}
+		assert.Equal(t, key, result.AnalysisInfo["key"], "mismatch in key")
+		assert.Equal(t, orgId, result.AnalysisInfo["organization_id"], "mismatch in organization_id")
+	})
+}
+
+// TestAtlassian_AnalysisInfo_KeyOnly tests if only key is populated into AnalysisInfo
+// given that only the key and no organization_id is present in the input data chunk
+func TestAtlassian_AnalysisInfo_KeyOnly(t *testing.T) {
+	client := common.SaneHttpClient()
+	d := Scanner{client: client}
+
+	key := "ATCTT3xFfGN0GsZNgOGrQSHSnxiJVi00oHlRicyM0yMNuKCBfw6qOHVcCy4Hm89GnclGb_W-1qAkxqCn5XbuyoX54bNhpK5yFKGFR7ocV6FByvL_P9Sb3tFnbUg3T3I3S_RGCBLMSN7Nsa4GJv8JEJ6bzvDmX-oJ8AnrazMU-zZ5hb-u3t2ERew=366BFE3A"
+
+	t.Run("only key present", func(t *testing.T) {
+		defer gock.Off()
+		defer gock.RestoreClient(client)
+		gock.InterceptClient(client)
+		gock.New("https://api.atlassian.com").
+			Get("/admin/v1/orgs").
+			MatchHeader("Accept", "application/json").
+			MatchHeader("Authorization", fmt.Sprintf("Bearer %s", key)).
+			Reply(http.StatusOK).
+			JSON(map[string]any{
+				"Data": []map[string]any{},
+			})
+
+		input := fmt.Sprintf(`
+		[INFO] Sending request to the atlassian API
+		[DEBUG] Using Key=%s
+		[INFO] Response received: 200 OK
+		`, key)
+
+		results, err := d.FromData(context.Background(), true, []byte(input))
+		require.NoError(t, err)
+		require.Len(t, results, 1, "mismatch in result count: expected %d, got %d", 1, len(results))
+		result := results[0]
+		require.NotNil(t, result.AnalysisInfo, "AnalysisInfo is nil")
+
+		assert.Equal(t, key, result.AnalysisInfo["key"], "mismatch in key")
+	})
 }
