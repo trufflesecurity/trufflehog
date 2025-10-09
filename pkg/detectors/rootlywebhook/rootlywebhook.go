824f19bc040ecf49570a8526dc89793bbbefae1f
Dadash Guliyev <64528432+nam3lum@users.noreply.github.com>
2025-10-09 14:32:38 +0300

    Update identifiers to webhook domain

--- pkg/detectors/rootlywebhook/rootlywebhook.go
+++ pkg/detectors/rootlywebhook/rootlywebhook.go
@@ -27,7 +27,7 @@ var (
 // Keywords are used for efficiently pre-filtering chunks.
 // Use identifiers in the secret preferably, or the provider name.
 func (s Scanner) Keywords() []string {
-	return []string{"rootly", "webhook"}
+	return []string{"webhooks.rootly.com"}
 }
 
 // FromData will find and optionally verify RootlyWebhook secrets in a given set of bytes.
