5b735e3096c16733924e597794cc3f9e0fadabef
Dadash Guliyev <64528432+nam3lum@users.noreply.github.com>
2025-10-15 12:03:36 +0300

    Additional keyword to rootly webhook scanner

--- pkg/detectors/rootlywebhook/rootlywebhook.go
+++ pkg/detectors/rootlywebhook/rootlywebhook.go
@@ -27,7 +27,7 @@ var (
 // Keywords are used for efficiently pre-filtering chunks.
 // Use identifiers in the secret preferably, or the provider name.
 func (s Scanner) Keywords() []string {
-	return []string{"webhooks.rootly.com", "rootly", "rootlywebhook", "rootly_webhook", "rootly_token"}
+	return []string{"webhooks.rootly.com", "rootly", "rootlywebhook", "rootly_webhook", "rootly_token", "rootly_webhook_token"}
 }
 
 // FromData will find and optionally verify RootlyWebhook secrets in a given set of bytes.
