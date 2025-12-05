db46e5db8678e1ccbb76379901ff8377ed95f354
Mustansir Muzaffar <mustansir2001@gmail.com>
2025-12-05 17:14:38 +0500

    abort scan if monthly request limit crosses 80%

--- pkg/sources/postman/postman_test.go
+++ pkg/sources/postman/postman_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-errors/errors"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/context"
@@ -687,5 +688,69 @@ func TestSource_HeadersScanning(t *testing.T) {
 		t.Errorf("No chunks were generated from the mock data")
 	} else {
 		t.Logf("Generated %d chunks from the mock data", chunksReceived)
+	}
+}
+
+func TestSource_AbortScanRateLimitCrossedThreshold(t *testing.T) {
+	defer gock.Off()
+	// Mock the API response for getting collection
+	gock.New("https://api.getpostman.com").
+		Get("/collections/1234-abc1").
+		Reply(200).
+		AddHeader("X-RateLimit-Remaining-Month", "19").
+		AddHeader("X-RateLimit-Limit-Month", "100").
+		BodyString(`{"collection":{"info":{"_postman_id":"abc1","name":"test-collection-1","schema":"https://schema.postman.com/json/collection/v2.1.0/collection.json",
+		"updatedAt":"2025-03-21T17:39:25.000Z","createdAt":"2025-03-21T17:37:13.000Z","lastUpdatedBy":"1234","uid":"1234-abc1"},
+		"item":[]}}`)
+
+	ctx := context.Background()
+	s, conn := createTestSource(&sourcespb.Postman{
+		Credential: &sourcespb.Postman_Token{
+			Token: "super-secret-token",
+		},
+	})
+
+	err := s.Init(ctx, "test - postman", 0, 1, false, conn, 1)
+	if err != nil {
+		t.Fatalf("init error: %v", err)
+	}
+	gock.InterceptClient(s.client.HTTPClient)
+	defer gock.RestoreClient(s.client.HTTPClient)
+
+	_, err = s.client.GetCollection(ctx, "1234-abc1")
+	if !errors.Is(err, errAbortScanDueToAPIRateLimit) {
+		t.Errorf("expected abort scan error due to rate limit, got: %v", err)
+	}
+}
+
+func TestSource_AbortScanRateLimitBelowThreshold(t *testing.T) {
+	defer gock.Off()
+	// Mock the API response for getting collection
+	gock.New("https://api.getpostman.com").
+		Get("/collections/1234-abc1").
+		Reply(200).
+		AddHeader("X-RateLimit-Remaining-Month", "90").
+		AddHeader("X-RateLimit-Limit-Month", "100").
+		BodyString(`{"collection":{"info":{"_postman_id":"abc1","name":"test-collection-1","schema":"https://schema.postman.com/json/collection/v2.1.0/collection.json",
+		"updatedAt":"2025-03-21T17:39:25.000Z","createdAt":"2025-03-21T17:37:13.000Z","lastUpdatedBy":"1234","uid":"1234-abc1"},
+		"item":[]}}`)
+
+	ctx := context.Background()
+	s, conn := createTestSource(&sourcespb.Postman{
+		Credential: &sourcespb.Postman_Token{
+			Token: "super-secret-token",
+		},
+	})
+
+	err := s.Init(ctx, "test - postman", 0, 1, false, conn, 1)
+	if err != nil {
+		t.Fatalf("init error: %v", err)
+	}
+	gock.InterceptClient(s.client.HTTPClient)
+	defer gock.RestoreClient(s.client.HTTPClient)
+
+	_, err = s.client.GetCollection(ctx, "1234-abc1")
+	if err != nil {
+		t.Errorf("expected no error, got: %v", err)
 	}
 }
