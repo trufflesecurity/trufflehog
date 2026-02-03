cfd505c0de2821dcaa26ea28e6b28b14adf14e19
Kashif Khan <kashif.khan@trufflesec.com>
2026-02-03 17:01:14 +0500

    Fixed and Improved Gitlab Project Metadata in-memory cache
    Earlier, we used a map to temporarily store GitLab project metadata. While maps work well for small datasets, they don’t scale efficiently for larger ones.
    There was also a bug in the caching logic: when storing entries, we used the GitLab HTTPURLToRepo field as the cache key, but when retrieving entries,
    we used the normalized URL. As a result, cache lookups almost never succeeded, and the cache kept growing without being effectively used.
    
    With this fix, we’ve replaced the map with an LRU cache, which is better suited for this use case.
    The cache now stores up to 15,000 entries for one hour, after which the LRU mechanism automatically evicts old items, keeping memory usage under control.
    We also consistently use the normalized URL for both setting and fetching cache entries.

--- pkg/sources/gitlab/project_cache.go
+++ pkg/sources/gitlab/project_cache.go
@@ -1,29 +1,47 @@
 package gitlab
 
-import "sync"
+import (
+	"sync"
+	"time"
 
+	"github.com/hashicorp/golang-lru/v2/expirable"
+)
+
+// project represents GitLab project metadata.
 type project struct {
 	id    int64
 	name  string
 	owner string
 }
 
-type repoToProjectCache struct {
-	sync.RWMutex
-
-	cache map[string]*project
+// projectMetadataCache stores project metadata with thread-safe access.
+type projectMetadataCache struct {
+	mu    sync.RWMutex
+	cache *expirable.LRU[string, *project]
 }
 
-func (r *repoToProjectCache) get(repo string) (*project, bool) {
-	r.RLock()
-	defer r.RUnlock()
-	proj, ok := r.cache[repo]
-	return proj, ok
+// NewProjectMetadataCache initializes a new project metadata cache
+// with a 30k entry limit and a 60-minute expiration.
+func NewProjectMetadataCache() *projectMetadataCache {
+	return &projectMetadataCache{
+		cache: expirable.NewLRU[string, *project](
+			15000, // upto 15000 entries
+			nil,
+			60*time.Minute, // time-based expiration - 1 hour
+		),
+	}
 }
 
-func (r *repoToProjectCache) set(repo string, proj *project) {
-	r.Lock()
-	defer r.Unlock()
+func (c *projectMetadataCache) get(repo string) (*project, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 
-	r.cache[repo] = proj
+	return c.cache.Get(repo)
+}
+
+func (c *projectMetadataCache) set(repo string, proj *project) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.cache.Add(repo, proj)
 }
