f3eeadf09171e813aca726af78ca2d00240493c5
Miccah Castorina <m.castorina93@gmail.com>
2026-02-20 10:19:36 -0800

    Update doc comments

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -20,16 +20,17 @@ type SyncFunc func() error
 
 type SinkOption func(*sinkConfig)
 
-// New creates a new log object with the provided configurations. If no sinks
-// are provided, a no-op sink will be used. Returns the logger and a cleanup
+// New creates a new log object with the provided sinks. If no sinks are
+// provided, a no-op sink will be used. Returns the logger and a cleanup
 // function that should be executed before the program exits.
 func New(service string, cores ...zapcore.Core) (logr.Logger, SyncFunc) {
 	return NewWithCaller(service, false, cores...)
 }
 
-// NewWithCaller creates a new logger named after the specified service with the provided sink configurations. If
-// addCaller is true, call site information will be attached to each emitted log message. (This behavior can be disabled
-// on a per-sink basis using WithSuppressCaller.)
+// NewWithCaller creates a new logger named after the specified service with
+// the provided sinks. If addCaller is true, call site information will be
+// attached to each emitted log message. (This behavior can be disabled on a
+// per-sink basis using WithSuppressCaller.)
 func NewWithCaller(service string, addCaller bool, cores ...zapcore.Core) (logr.Logger, SyncFunc) {
 	// create logger
 	zapLogger := zap.New(zapcore.NewTee(cores...), zap.WithCaller(addCaller))
