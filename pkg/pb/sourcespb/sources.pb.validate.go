71d3b22a0a1fb6900bfd9aa1bf7adabb7ac5ee16
Kashif Khan <kashif.khan@trufflesec.com>
2026-03-27 15:26:42 +0500

    basic structure for source

--- pkg/pb/sourcespb/sources.pb.validate.go
+++ pkg/pb/sourcespb/sources.pb.validate.go
@@ -7238,3 +7238,107 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = JSONEnumeratorValidationError{}
+
+// Validate checks the field values on Web with the rules defined in the proto
+// definition for this message. If any rules are violated, the first error
+// encountered is returned, or nil if there are no violations.
+func (m *Web) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on Web with the rules defined in the
+// proto definition for this message. If any rules are violated, the result is
+// a list of violation errors wrapped in WebMultiError, or nil if none found.
+func (m *Web) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *Web) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Crawl
+
+	// no validation rules for Depth
+
+	// no validation rules for Delay
+
+	if len(errors) > 0 {
+		return WebMultiError(errors)
+	}
+
+	return nil
+}
+
+// WebMultiError is an error wrapping multiple validation errors returned by
+// Web.ValidateAll() if the designated constraints aren't met.
+type WebMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m WebMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m WebMultiError) AllErrors() []error { return m }
+
+// WebValidationError is the validation error returned by Web.Validate if the
+// designated constraints aren't met.
+type WebValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e WebValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e WebValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e WebValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e WebValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e WebValidationError) ErrorName() string { return "WebValidationError" }
+
+// Error satisfies the builtin error interface
+func (e WebValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sWeb.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = WebValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = WebValidationError{}
