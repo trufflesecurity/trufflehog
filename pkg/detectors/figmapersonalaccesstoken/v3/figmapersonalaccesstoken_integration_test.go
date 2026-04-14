ff9b319652d377f62c6325c75e78ba7910937fc8
Shahzad Haider <shahzadhaider.se@gmail.com>
2026-04-14 14:31:30 +0500

    switched from detectorspb to detector_typepb

--- pkg/detectors/figmapersonalaccesstoken/v3/figmapersonalaccesstoken_integration_test.go
+++ pkg/detectors/figmapersonalaccesstoken/v3/figmapersonalaccesstoken_integration_test.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
-	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detector_typepb"
 )
 
 func TestFigmaPersonalAccessToken_FromChunk(t *testing.T) {
@@ -51,7 +51,7 @@ func TestFigmaPersonalAccessToken_FromChunk(t *testing.T) {
 			},
 			want: []detectors.Result{
 				{
-					DetectorType: detectorspb.DetectorType_FigmaPersonalAccessToken,
+					DetectorType: detector_typepb.DetectorType_FigmaPersonalAccessToken,
 					Verified:     true,
 					ExtraData: map[string]string{
 						"version": "3",
@@ -71,7 +71,7 @@ func TestFigmaPersonalAccessToken_FromChunk(t *testing.T) {
 			},
 			want: []detectors.Result{
 				{
-					DetectorType: detectorspb.DetectorType_FigmaPersonalAccessToken,
+					DetectorType: detector_typepb.DetectorType_FigmaPersonalAccessToken,
 					Verified:     false,
 					ExtraData: map[string]string{
 						"version": "3",
@@ -90,7 +90,7 @@ func TestFigmaPersonalAccessToken_FromChunk(t *testing.T) {
 			},
 			want: []detectors.Result{
 				{
-					DetectorType: detectorspb.DetectorType_FigmaPersonalAccessToken,
+					DetectorType: detector_typepb.DetectorType_FigmaPersonalAccessToken,
 					Verified:     false,
 					ExtraData: map[string]string{
 						"version": "3",
@@ -110,7 +110,7 @@ func TestFigmaPersonalAccessToken_FromChunk(t *testing.T) {
 			},
 			want: []detectors.Result{
 				{
-					DetectorType: detectorspb.DetectorType_FigmaPersonalAccessToken,
+					DetectorType: detector_typepb.DetectorType_FigmaPersonalAccessToken,
 					Verified:     false,
 					ExtraData: map[string]string{
 						"version": "3",
