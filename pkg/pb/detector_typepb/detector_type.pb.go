8e5ad3b1c672195b4b34bfd15060c374a85ff73f
Mustansir Muzaffar <mustansir2001@gmail.com>
2026-04-15 14:39:32 +0500

    fix detector type after merge

--- pkg/pb/detector_typepb/detector_type.pb.go
+++ pkg/pb/detector_typepb/detector_type.pb.go
@@ -7,10 +7,11 @@
 package detector_typepb
 
 import (
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
 	sync "sync"
+
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 const (
