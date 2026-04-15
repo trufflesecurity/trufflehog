8045835f81817e608f60729bd008b56fc8229a62
Mustansir Muzaffar <mustansir2001@gmail.com>
2026-04-15 14:43:32 +0500

    detector type fix after merge

--- pkg/detectors/newreliclicensekey/newreliclicensekey.go
+++ pkg/detectors/newreliclicensekey/newreliclicensekey.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/trufflesecurity/trufflehog/v3/pkg/common"
 	"github.com/trufflesecurity/trufflehog/v3/pkg/detectors"
-	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detectorspb"
+	"github.com/trufflesecurity/trufflehog/v3/pkg/pb/detector_typepb"
 )
 
 type Scanner struct {
@@ -40,8 +40,8 @@ func (s Scanner) getClient() *http.Client {
 // Keywords are used for efficiently pre-filtering chunks.
 func (s Scanner) Keywords() []string { return []string{"ffffnral"} }
 
-func (s Scanner) Type() detectorspb.DetectorType {
-	return detectorspb.DetectorType_NewRelicLicenseKey
+func (s Scanner) Type() detector_typepb.DetectorType {
+	return detector_typepb.DetectorType_NewRelicLicenseKey
 }
 
 func (s Scanner) Description() string {
