90a918d5a1b4ddd5a05fe31dc02a07805ca90031
Shahzad Haider <shahzadhaider.se@gmail.com>
2026-06-02 17:12:03 +0500

    fix failing tests

--- pkg/detectors/figmapersonalaccesstoken/v3/figmapersonalaccesstoken_integration_test.go
+++ pkg/detectors/figmapersonalaccesstoken/v3/figmapersonalaccesstoken_integration_test.go
@@ -147,7 +147,7 @@ func TestFigmaPersonalAccessToken_FromChunk(t *testing.T) {
 					t.Fatalf("wantVerificationError = %v, verification error = %v", tt.wantVerificationErr, got[i].VerificationError())
 				}
 			}
-			ignoreOpts := cmpopts.IgnoreFields(detectors.Result{}, "Raw", "verificationError", "primarySecret")
+			ignoreOpts := cmpopts.IgnoreFields(detectors.Result{}, "Raw", "verificationError", "primarySecret", "chunkOffset", "chunkOffsetSet")
 			if diff := cmp.Diff(got, tt.want, ignoreOpts); diff != "" {
 				t.Errorf("FigmaPersonalAccessToken.FromData() %s diff: (-got +want)\n%s", tt.name, diff)
 			}
