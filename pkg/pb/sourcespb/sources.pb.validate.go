4945fa3cd2a115fa661f9ecbedae3598557996ee
Julien Chaumond <julien@huggingface.co>
2026-06-24 16:40:58 +0200

    huggingface: add bucket scanning (#5017)
    * Fix incorrect doc comments in huggingface engine
    
    * add huggingface bucket scanning
    
    * Update client.go
    
    * use limit=5000 on bucket listing endpoints
    
    * regenerate man page
    
    * escape bucket file path per-segment instead of whole
    
    * escape file path in bucket finding link too
    
    * use separate client without overall timeout for bucket file downloads

--- pkg/pb/sourcespb/sources.pb.validate.go
+++ pkg/pb/sourcespb/sources.pb.validate.go
@@ -3444,6 +3444,8 @@ func (m *Huggingface) validate(all bool) error {
 
 	// no validation rules for IncludePrs
 
+	// no validation rules for SkipAllBuckets
+
 	switch v := m.Credential.(type) {
 	case *Huggingface_Token:
 		if v == nil {
