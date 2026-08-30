86e34e915f9b6dcaefaf60908b6d3fae1b143192
Shahzad Haider <shahzadhaider.se@gmail.com>
2026-08-31 00:28:01 +0500

    detectors/gitlab: redact the token from verification errors

--- pkg/detectors/gitlab/v2/gitlab_v2.go
+++ pkg/detectors/gitlab/v2/gitlab_v2.go
@@ -82,7 +82,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 				s1.Verified = isVerified
 				maps.Copy(s1.ExtraData, extraData)
 
-				s1.SetVerificationError(verificationErr)
+				s1.SetVerificationError(verificationErr, resMatch)
 
 				// for verified keys break out of the endpoint loop to continue to next secret
 				if s1.Verified {
