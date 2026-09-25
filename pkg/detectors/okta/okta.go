35ee6a0867cdd2736810b8d4601e56ccc79ad40b
KevinPham-TruffleSecurity <kevin.pham@trufflesec.com>
2026-09-25 14:39:09 -0700

    remove changes that were used to test github actions

--- pkg/detectors/okta/okta.go
+++ pkg/detectors/okta/okta.go
@@ -18,14 +18,13 @@ type Scanner struct {
 	detectors.DefaultMultiPartCredentialProvider
 }
 
-// testing github action
 // Ensure the Scanner satisfies the interface at compile time.
 var _ detectors.Detector = (*Scanner)(nil)
 var _ detectors.CustomResultsCleaner = (*Scanner)(nil)
 
 var (
 	defaultClient = detectors.DetectorHttpClientWithNoLocalAddresses
-	domainPat     = regexp.MustCompile(`\b[a-z0-9-]{1,40}\.(?:okta(?:preview|-emea|-gov)?\.com|okta\.mil)\b`)
+	domainPat     = regexp.MustCompile(`\b[a-z0-9-]{1,40}\.okta(?:preview|-emea){0,1}\.com\b`)
 	tokenPat      = regexp.MustCompile(`\b00[a-zA-Z0-9_-]{40}\b`)
 
 	// Okta app/client IDs: "0oa" + 17 base62 characters.
